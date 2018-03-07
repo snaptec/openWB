bfc862c6ba15f4f5030b22159eb41a54fb9ab55e
snaptec <info@snaptec.org>
2018-03-07 17:06:49 +0100

    minor changes

--- runs/6.sh
+++ runs/6.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo python /var/www/html/openWB/runs/dac.py 790
+echo "setz ladung auf 6A" >> /var/www/html/openWB/web/lade.log
 echo 1 > /var/www/html/openWB/ramdisk/ladestatus
 echo 6 > /var/www/html/openWB/ramdisk/llsoll
