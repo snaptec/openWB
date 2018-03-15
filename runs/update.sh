fc7cf3245614ed20e6ea3b4c7a7edafb1b58b762
snaptec <info@snaptec.org>
2018-03-15 12:47:15 +0000

    wrong command

--- runs/update.sh
+++ runs/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /var/www/html/
-git clone https://github.com/snaptec/openWB.git
+git pull origin master:master
 sudo chown -R pi:pi openWB 
 chmod +x /var/www/html/openWB/modules/*                     
 chmod +x /var/www/html/openWB/runs/*
