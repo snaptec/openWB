bceb5b78589fdffebd90aef548983bf7a1e9b13e
snaptec <info@snaptec.org>
2018-03-15 12:43:51 +0000

    minor changes

---
+++ web/tools/sofortll.php
@@ -0,0 +1,22 @@
+
+<?php
+if(isset($_POST['sofortll'])) {
+$data = $_POST['sofortll'];
+$config['sofortll'] = $_POST['sofortll'];
+
+$result = '';
+$lines = file('/var/www/html/openWB/openwb.conf');
+foreach($lines as $line) {
+	    if(substr($line, 0, 9) == 'sofortll=') {
+	    $result .= 'sofortll='.$config['sofortll']."\n";
+	    } else {
+	    $result .= $line;
+	    }
+}
+file_put_contents('/var/www/html/openWB/openwb.conf', $result);
+}
+header("Location: ../settings.php");
+?>
+
+
+
