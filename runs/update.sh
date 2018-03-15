ba03b8be0ac7d2e8e3b949abf98c54e7d3e3501b
snaptec <info@snaptec.org>
2018-03-15 12:48:20 +0000

    change directory

--- runs/update.sh
+++ runs/update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /var/www/html/
+cd /var/www/html/openWB
 git pull origin master:master
+cd /var/www/html/
 sudo chown -R pi:pi openWB 
 chmod +x /var/www/html/openWB/modules/*                     
 chmod +x /var/www/html/openWB/runs/*
