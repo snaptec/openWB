0ecfc7deeadbe221246c4d06fe2b371c8a42d151
snaptec <info@snaptec.org>
2018-03-16 15:39:13 +0100

    minor changes

--- runs/update.sh
+++ runs/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /var/www/html/openWB
-git pull origin master:master
+sudo git pull origin master:master
 cd /var/www/html/
 sudo chown -R pi:pi openWB 
-chmod 777 /var/www/html/openWB/openwb.conf
-chmod +x /var/www/html/openWB/modules/*                     
-chmod +x /var/www/html/openWB/runs/*
-chmod 777 /var/www/html/openWB/web/lade.log
+sudo chmod 777 /var/www/html/openWB/openwb.conf
+sudo chmod +x /var/www/html/openWB/modules/*                     
+sudo chmod +x /var/www/html/openWB/runs/*
+sudo chmod 777 /var/www/html/openWB/web/lade.log
 
