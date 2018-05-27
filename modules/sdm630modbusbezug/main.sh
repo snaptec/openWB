70f00ecd6f994d69f5fbe4f48737099c54a99c48
snaptec <info@snaptec.org>
2018-05-27 12:26:21 +0200

    sdmll bugfix

--- modules/sdm630modbusbezug/main.sh
+++ modules/sdm630modbusbezug/main.sh
@@ -19,17 +19,17 @@ while read -r line; do
 	if (( $n == 0 )); then
 		evua1=$(echo "$line" |  cut -c2- )
 		evua1=${evua1%??}
-		printf "%.2f\n" $evua1 > /var/www/html/openWB/ramdisk/bezuga1
+		printf "%.1f\n" $evua1 > /var/www/html/openWB/ramdisk/bezuga1
 	fi
 	if (( $n == 1 )); then
 		evua2=$(echo "$line" |  cut -c2- )
 		evua2=${evua2%??}
-		printf "%.2f\n" $evua2 > /var/www/html/openWB/ramdisk/bezuga2
+		printf "%.1f\n" $evua2 > /var/www/html/openWB/ramdisk/bezuga2
 	fi
 	if (( $n == 2 )); then
 		evua3=$(echo "$line" |  cut -c2- )
 		evua3=${evua3%??}
-		printf "%.2f\n" $evua3 > /var/www/html/openWB/ramdisk/bezuga3
+		printf "%.1f\n" $evua3 > /var/www/html/openWB/ramdisk/bezuga3
 	fi
 	if (( $n == 3 )); then
 		wl1=$(echo "$line" |  cut -c2- |sed 's/\..*$//')
@@ -79,22 +79,22 @@ while read -r line; do
 	if (( $n == 16 )); then
 		evuv1=$(echo "$line" |  cut -c2- )
 		evuv1=${evuv1%??}
-		printf "%.2f\n" $evuv1 > /var/www/html/openWB/ramdisk/evuv1
+		printf "%.1f\n" $evuv1 > /var/www/html/openWB/ramdisk/evuv1
 	fi
 	if (( $n == 17 )); then
 		evuv2=$(echo "$line" |  cut -c2- )
 		evuv2=${evuv2%??}
-		printf "%.2f\n" $evuv2 > /var/www/html/openWB/ramdisk/evuv2
+		printf "%.1f\n" $evuv2 > /var/www/html/openWB/ramdisk/evuv2
 	fi
 	if (( $n == 18 )); then
 		evuv3=$(echo "$line" |  cut -c2- )
 		evuv3=${evuv3%??}
-		printf "%.2f\n" $evuv3 > /var/www/html/openWB/ramdisk/evuv3
+		printf "%.1f\n" $evuv3 > /var/www/html/openWB/ramdisk/evuv3
 	fi
 	if (( $n == 19 )); then
 		evuhz=$(echo "$line" |  cut -c2- )
 		evuhz=${evuhz%??}
-        	printf "%.2f\n" $evuhz > /var/www/html/openWB/ramdisk/evuhz
+        	printf "%.1f\n" $evuhz > /var/www/html/openWB/ramdisk/evuhz
 	fi
 	if (( $n == 20 )); then
 		bezugkwh=$(echo "$line" |  cut -c2- )
