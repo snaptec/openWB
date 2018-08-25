772d2e544d413d66041a3671f661b55fdd262e7b
snaptec <info@snaptec.org>
2018-08-25 08:05:09 +0200

    SoC Leaf Aenderung

--- modules/soc_leaf/soc.py
+++ modules/soc_leaf/soc.py
@@ -30,20 +30,19 @@ if ( leaftimer < 181 ):
 
 
 else:
+    f = open('/var/www/html/openWB/ramdisk/soctimer', 'w')
+    f.write(str(0))
+    f.close()
     from leaf import Leaf
     leaf = Leaf(sys.argv[1], sys.argv[2])
     response = leaf.BatteryStatusCheckRequest()
     time.sleep(15)
-    leaf.BatteryStatusCheckResultRequest(resultKey=response['resultKey'])
-    time.sleep(10)
     socit = leaf.BatteryStatusRecordsRequest()
     justsoc = socit['BatteryStatusRecords']['BatteryStatus']['SOC']['Value']
     f = open('/var/www/html/openWB/ramdisk/soc', 'w')
     f.write(str(justsoc))
     f.close()
-    f = open('/var/www/html/openWB/ramdisk/soctimer', 'w')
-    f.write(str(0))
-    f.close()
+
 
     
     
