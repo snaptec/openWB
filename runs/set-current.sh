19b9482f3b25efe049c2a9fb01016450609776b4
David Meder-Marouelli <David@Meder-Marouelli.de>
2018-11-05 19:27:49 +0100

    add copyright and license notice

--- runs/set-current.sh
+++ runs/set-current.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+#####
+#
+# File: set-current.sh
+#
+# Copyright 2018 David Meder-Marouelli
+#
+#  This file is part of openWB.
+#
+#     openWB is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     openWB is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with openWB.  If not, see <https://www.gnu.org/licenses/>.
+#
+#####
+
 # set charging current in EVSE
 
 . /var/www/html/openWB/openwb.conf
