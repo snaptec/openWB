1d678bb1119e759c521de47ca3bdd991d2cf0762
snaptec <36848507+snaptec@users.noreply.github.com>
2018-11-09 08:40:01 +0100

    Update main.sh

--- modules/wr_kostalpiko/main.sh
+++ modules/wr_kostalpiko/main.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-#Auslesen eines Kostal Piko WR über die integrierte API des WR. Rückgabewerte sind die aktuelle Wattleistung und der Gesamtzählerstand am WR.
-
+#Auslesen eines Kostal Piko WR über die integrierte API des WR. Rückgabewert ist die aktuelle Wattleistung.
 . /var/www/html/openWB/openwb.conf
 
-pvwatttmp=$(curl --connect-timeout 5 -s $wrkostalpikoip/api/dxs.json?dxsEntries=67109120&dxsEntries=251658753)
+pvwatttmp=$(curl --connect-timeout 5 -s $wrkostalpikoip/api/dxs.json?dxsEntries=67109120'&'dxsEntries=251658753)
 
-#Aktuelle Wattleistung, gerundet
-pvwatt=$(echo $pvwatttmp | jq '.dxsEntries[0] | .value | round')
 
-#wenn WR aus bzw. im standby (keine Antwort) ersetze leeren Wert durch eine 0
 re='^[0-9]+$'
 if ! [[ $pvwatt =~ $re ]] ; then
 	   pvwatt="0"
    fi
-   echo $pvwatt
+#aktuelle Ausgangsleistung am WR [W]
+pvwatt=$(echo $pvwatttmp | jq '.dxsEntries[0].value' | awk '{printf "%.0f", $1}')
+#zur weiteren verwendung im webinterface
+echo $pvwatt > /var/www/html/openWB/ramdisk/pvwatt
 
-   #zur weiteren Verwendung im webinterface
-   echo $pvwatt > /var/www/html/openWB/ramdisk/pvwatt
-
-   # Gesamtzählerstand am WR, gerundet
-   pvkwh=$(echo $pvwatttmp | jq '.dxsEntries[1] | .value | round')
-
-   #zur weiteren Verwendung im webinterface
-   echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwh
+#Gesamtzählerstand am WR [kWh]
+pvkwh=$(echo $pvwatttmp | jq '.dxsEntries[1].value' | awk '{printf "%.0f", $1}')
+#zur weiteren verwendung im webinterface
+echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwh
