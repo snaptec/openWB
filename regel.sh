466a804d132639213dd3029d4a56bcae9f31eb74
snaptec <36848507+snaptec@users.noreply.github.com>
2018-11-10 15:42:20 +0100

    Update regel.sh

--- regel.sh
+++ regel.sh
@@ -1268,9 +1268,13 @@ fi
 
 if grep -q 3 "/var/www/html/openWB/ramdisk/lademodus"; then
 	if grep -q 1 "/var/www/html/openWB/ramdisk/ladestatus"; then
-		runs/set-current.sh 0 all
-		exit 0
-	else
-		exit 0
+		runs/set-current.sh 0 m
 	fi
+	if grep -q 1 "/var/www/html/openWB/ramdisk/ladestatuss1"; then
+		runs/set-current.sh 0 s1
+	fi
+	if grep -q 1 "/var/www/html/openWB/ramdisk/ladestatuss2"; then
+		runs/set-current.sh 0 s2
+	fi
+	exit 0
 fi
