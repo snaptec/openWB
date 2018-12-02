ae72983a5318da552805ce9b453414dbcb7667c2
snaptec <info@snaptec.org>
2018-12-02 13:57:21 +0100

    MPM3PM PV Watt Fix

--- web/index.php
+++ web/index.php
@@ -676,7 +676,7 @@
 			<div class="row">
 				<div class="col-xs-4">
 
-				<!-- master -->	Ver 1.02				</div>
+				<!-- master -->	Ver 1.02 nightly				</div>
 
 				<div class="col-xs-4 text-center">
 					<a href="http://openwb.de">www.openwb.de</a>
