67081b568990052a8a6c5fdea4e18e4f350bf687
LocutusB <burkart@gmx.de>
2018-12-27 18:21:43 +0100

    Modul soc_i3 korrigiert
    Da hatten sich irgendwie ein paar Fehler eingeschlichen.

--- modules/soc_i3/main.sh
+++ modules/soc_i3/main.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#Variable aus openWB übergeben. 
-#Verkürzte Intervallzeit in Minuten (bei Geschwindigkeit Regelintervall: Normal) während Ladevogang.
-#Maximalwert = 10
-soci3intervall=5
+. /var/www/html/openWB/openwb.conf
 
 i3timer=$(</var/www/html/openWB/ramdisk/soctimer)
 cd /var/www/html/openWB/modules/soc_i3
@@ -12,7 +9,8 @@ if (( i3timer < 60 )); then
 	echo $i3timer > /var/www/html/openWB/ramdisk/soctimer
 else
 	re='^-?[0-9]+$'
-	soclevel=$(sudo php index.php | jq .chargingLevel)
+	abfrage=$(sudo php index.php | jq '.')
+	soclevel=$(echo $abfrage | jq '.chargingLevel')
 	if  [[ $soclevel =~ $re ]] ; then
 		if (( $soclevel != 0 )) ; then
 			echo $soclevel > /var/www/html/openWB/ramdisk/soc
@@ -21,10 +19,10 @@ else
 
 #Abfrage Ladung aktiv. Setzen des soctimers. 
 	charging=$(echo $abfrage | jq '.chargingActive')
-	if (( $charging != 0 )) ; then
-		soctimer=$((60 * (10 - $i3socintervall) / 10))
+	if [[ $charging != 0 ]] ; then
+		soctimer=$((60 * (10 - $soci3intervall) / 10))
 		echo $soctimer > /var/www/html/openWB/ramdisk/soctimer
 	else
-		echo 0 > /var/www/html/openWB/ramdisk/soctimer
+		echo 1 > /var/www/html/openWB/ramdisk/soctimer
 	fi
 fi
