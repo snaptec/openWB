b4b5e205fdcdd6c103547a244b07eb1ef3718443
LocutusB <burkart@gmx.de>
2018-12-28 16:09:01 +0100

    Änderungen bzgl. Updateintervall auch für soc_i3s1 übernommen.
    Auch an LP2 wird damit das Updateintervall während des Ladens entsprechend der Einstellung für LP1 verkürzt.

--- modules/soc_i3s1/main.sh
+++ modules/soc_i3s1/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. /var/www/html/openWB/openwb.conf
+
 i3timer=$(</var/www/html/openWB/ramdisk/soctimer1)
 cd /var/www/html/openWB/modules/soc_i3s1
 if (( i3timer < 60 )); then
@@ -7,11 +9,20 @@ if (( i3timer < 60 )); then
 	echo $i3timer > /var/www/html/openWB/ramdisk/soctimer1
 else
 	re='^-?[0-9]+$'
-	soclevel=$(sudo php index.php | jq .chargingLevel)
+	abfrage=$(sudo php index.php | jq '.')
+	soclevel=$(echo $abfrage | jq '.chargingLevel')
 	if  [[ $soclevel =~ $re ]] ; then
 		if (( $soclevel != 0 )) ; then
 			echo $soclevel > /var/www/html/openWB/ramdisk/soc1
 		fi
 	fi
-	echo 0 > /var/www/html/openWB/ramdisk/soctimer1
+
+#Abfrage Ladung aktiv. Setzen des soctimers. 
+	charging=$(echo $abfrage | jq '.chargingActive')
+	if [[ $charging != 0 ]] ; then
+		soctimer1=$((60 * (10 - $soci3intervall) / 10))
+		echo $soctimer1 > /var/www/html/openWB/ramdisk/soctimer1
+	else
+		echo 1 > /var/www/html/openWB/ramdisk/soctimer1
+	fi
 fi
