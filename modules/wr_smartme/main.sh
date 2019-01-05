4d7d3722a69f872fc14543cda4e918e8fb430489
LocutusB <burkart@gmx.de>
2019-01-05 11:49:47 +0100

    Modulorder umbenannt in wr_smartme. Kleine Korrekturen.
    Der Ordnername passt jetzt zur übrigen Struktur. Auslesen und Berechnen der Werte optimiert.

--- modules/wr_smartme/main.sh
+++ modules/wr_smartme/main.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 #Variablen über die Weboberfläche abfragen und an das Modul übergeben.
-wr_smartme_user="user"
-wr_smartme_pass="password"
-wr_smartme_url="https://smart-me.com:443/api/Devices/[Device_ID]"
+#wr_smartme_user="user"
+#wr_smartme_pass="pass"
+#wr_smartme_url="https://smart-me.com:443/api/Devices/[ID]"
 
 . /var/www/html/openWB/openwb.conf
 
 #Daten einlesen
 json=$(curl -u $wr_smartme_user:$wr_smartme_pass --connect-timeout 10 -s $wr_smartme_url)
 
-#Aktuelle PV Leistung (W)
+#Aktuelle Leistung (kW --> W)
 wattwr=$(echo $json | jq .ActivePower)
 wattwr=$(echo "scale=3 ; $wattwr * 1000" | bc)
 wattwr=$(echo "$wattwr / 1" | bc)
 
-#Zählerstand PV (kWh)
-pvkwh=$(echo $json | jq .CounterReading)
-pvkwh=$(echo "$pvkwh / -1" | bc)
+#Zählerstand Export (kWh)
+pvkwh=$(echo $json | jq .CounterReadingExport)
+pvkwh=$(echo "$pvkwh / 1" | bc)
 
-#Prüfen ob Werte gültig
+#Prüfen ob Werte gültig sonst letzten gültigen Wert übernehmen
 re='^-?[0-9]+$'
 if ! [[ $wattwr =~ $re ]] ; then
-	   wattwr="0"
+	   wattwr=$(</var/www/html/openWB/ramdisk/pvwatt)
 fi
 if ! [[ $pvkwh =~ $re ]] ; then
-	   pvkwh="0"
+	   pvkwh=$(</var/www/html/openWB/ramdisk/pvkwh)
 fi
 
 #Ausgabe
 echo $wattwr
 echo $wattwr > /var/www/html/openWB/ramdisk/pvwatt
+
 echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwh
 
 pvkwhk=$(echo "scale=3 ; $pvkwh / 1000" | bc)
