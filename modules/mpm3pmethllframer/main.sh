d6f84ad02e9e8bae9cb0a10152116596d23e8898
snaptec <info@snaptec.org>
2019-02-01 15:57:22 +0100

    OpenWB Master zero config added

--- modules/mpm3pmethllframer/main.sh
+++ modules/mpm3pmethllframer/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . /var/www/html/openWB/openwb.conf
 
-sudo python /var/www/html/openWB/modules/mpm3pmiethllframer/readmpm3pm.py
+sudo python /var/www/html/openWB/modules/mpm3pmethllframer/readmpm3pm.py
 
 
 
