f0ab16dedc85daa0be48d496d352aa87a67e1c65
snaptec <info@snaptec.org>
2019-04-18 22:34:57 +0200

    EVU Kit bugfix

--- modules/bezug_ethmpm3pm/readmpm3pm.py
+++ modules/bezug_ethmpm3pm/readmpm3pm.py
@@ -39,7 +39,7 @@ value1 = resp.registers[0]
 value2 = resp.registers[1] 
 all = format(value1, '04x') + format(value2, '04x')
 ikwh = int(struct.unpack('>i', all.decode('hex'))[0]) 
-ikwh = float(ekwh) * 10
+ikwh = float(ikwh) * 10
 f = open('/var/www/html/openWB/ramdisk/bezugkwh', 'w')
 f.write(str(ikwh))
 f.close()
