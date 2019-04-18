f3709390c420fa178628f875579a7edafde0c4d4
snaptec <info@snaptec.org>
2019-04-18 22:29:36 +0200

    EVU Kit bugfix

--- modules/bezug_ethmpm3pm/readmpm3pm.py
+++ modules/bezug_ethmpm3pm/readmpm3pm.py
@@ -34,9 +34,12 @@ f = open('/var/www/html/openWB/ramdisk/evuv3', 'w')
 f.write(str(voltage))
 f.close()
 
-resp = client.read_input_registers(0x0002,2, unit=5)
-ikwh = resp.registers[1]
-ikwh = float(ikwh) * 10
+resp = client.read_input_registers(0x0002,4, unit=5)
+value1 = resp.registers[0] 
+value2 = resp.registers[1] 
+all = format(value1, '04x') + format(value2, '04x')
+ikwh = int(struct.unpack('>i', all.decode('hex'))[0]) 
+ikwh = float(ekwh) * 10
 f = open('/var/www/html/openWB/ramdisk/bezugkwh', 'w')
 f.write(str(ikwh))
 f.close()
