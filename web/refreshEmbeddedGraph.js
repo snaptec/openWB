8cfa88c41500c43a81b2c6dfbdedae53bd520409
Michael Ortenstein <develop@ortenstein.de>
2019-05-28 22:38:04 +0200

    Bugfix

--- web/refreshEmbeddedGraph.js
+++ web/refreshEmbeddedGraph.js
@@ -4,7 +4,11 @@ function getgraph() {
 	var source = "themes/"+themeCookie.read()+"/graph-live.php",
 	timestamp = (new Date()).getTime(),
 	newUrl = source + '?_=' + timestamp;
-	document.getElementById("livegraph").src = newUrl;
+	if (document.getElementById("livegraph")) {
+		document.getElementById("livegraph").src = newUrl;
+	}
 }
 
 doval = setInterval(getgraph, 5000);
+
+getgraph();
