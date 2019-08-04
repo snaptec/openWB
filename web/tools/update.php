33818eea77f082c7b9a5ef363530c9d3d84edb64
snaptec <info@snaptec.org>
2019-08-04 10:41:14 +0200

    update bugfix

--- web/tools/update.php
+++ web/tools/update.php
@@ -1,9 +1,12 @@
+<html>
 
-	<script type="text/javascript">
-	    setTimeout(function() { window.location = "../index.php"; }, 10000);
-	</script>
+
 <?php
 
 	echo "Update wird durchgeführt, bitte nicht vom Strom trennen";
 	exec("/var/www/html/openWB/runs/update.sh &");
 ?>
+	<head>
+		<meta http-equiv="refresh" content="1;url=../index.php" />
+	</head>
+</html>
