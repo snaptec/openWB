971b5e83457e3e6b5bf152ddee838e5463d68060
snaptec <info@snaptec.org>
2019-08-04 10:21:11 +0200

    display ui changes

--- web/tools/update.php
+++ web/tools/update.php
@@ -1,9 +1,9 @@
+
+	<script type="text/javascript">
+	    setTimeout(function() { window.location = "../index.php"; }, 10000);
+	</script>
 <?php
 
 	echo "Update wird durchgeführt, bitte nicht vom Strom trennen";
 	exec("/var/www/html/openWB/runs/update.sh");
 ?>
-	<script type="text/javascript">
-	    setTimeout(function() { window.location = "../index.php"; }, 30000);
-	</script>
-
