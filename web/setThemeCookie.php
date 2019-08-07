4804ac5ffacf57dd5cee7fb69dfe35bd2f91848a
Michael Ortenstein <develop@ortenstein.de>
2019-08-07 21:59:20 +0200

    Bugfix und Änderungen Dark_Gauges_1 Theme. Automatische Skalierung nur noch beim Hausverbrauch, alle anderen Skalierungen werden als Cookie gespeichert, um optisch die Werte schneller erfassen zu können.

--- web/setThemeCookie.php
+++ web/setThemeCookie.php
@@ -1,4 +1,4 @@
 <?php
  	$themeCookie = $_GET['theme'];
-	setcookie('openWBTheme', $themeCookie, time()+(60*60*24*365*2));
+	setcookie('openWBTheme', $themeCookie, time()+(60*60*24*365*2), '/');
 ?>
