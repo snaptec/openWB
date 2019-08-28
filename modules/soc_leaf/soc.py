6135c22cc4f40a9703dc1cf2c1da7667c37ca2e2
snaptec <info@snaptec.org>
2019-08-28 13:46:43 +0200

    Leaf SoC fixed

--- modules/soc_leaf/soc.py
+++ modules/soc_leaf/soc.py
@@ -5,43 +5,27 @@ import time
 
 leaftimer = open('/var/www/html/openWB/ramdisk/soctimer', 'r')
 leaftimer = int(leaftimer.read())
-
+thecommand=str('python3 /var/www/html/openWB/modules/soc_leaf/getsoc.py')
+thestring=str( sys.argv[1] + ' ' + sys.argv[2] + ' &') 
 if ( leaftimer < 181 ):
     leaftimer += 1
     f = open('/var/www/html/openWB/ramdisk/soctimer', 'w')
     f.write(str(leaftimer))
     f.close()
     if ( leaftimer == 10 ):
-        from leaf import Leaf
-        leaf = Leaf(sys.argv[1], sys.argv[2])
-        socit = leaf.BatteryStatusRecordsRequest()
-        justsoc = socit['BatteryStatusRecords']['BatteryStatus']['SOC']['Value']
-        f = open('/var/www/html/openWB/ramdisk/soc', 'w')
-        f.write(str(justsoc))
-        f.close()
+        from subprocess import call
+        exit_code = call(thecommand + ' ' + thestring, shell=True)
     if ( leaftimer == 60 ):
-        from leaf import Leaf
-        leaf = Leaf(sys.argv[1], sys.argv[2])
-        socit = leaf.BatteryStatusRecordsRequest()
-        justsoc = socit['BatteryStatusRecords']['BatteryStatus']['SOC']['Value']
-        f = open('/var/www/html/openWB/ramdisk/soc', 'w')
-        f.write(str(justsoc))
-        f.close()
+        from subprocess import call
+        exit_code = call(thecommand + ' ' + thestring, shell=True)
 
 
 else:
     f = open('/var/www/html/openWB/ramdisk/soctimer', 'w')
     f.write(str(0))
     f.close()
-    from leaf import Leaf
-    leaf = Leaf(sys.argv[1], sys.argv[2])
-    response = leaf.BatteryStatusCheckRequest()
-    time.sleep(15)
-    socit = leaf.BatteryStatusRecordsRequest()
-    justsoc = socit['BatteryStatusRecords']['BatteryStatus']['SOC']['Value']
-    f = open('/var/www/html/openWB/ramdisk/soc', 'w')
-    f.write(str(justsoc))
-    f.close()
+    from subprocess import call
+    exit_code = call(thecommand + ' ' + thestring, shell=True)
 
 
     
