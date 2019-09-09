9e9ec1e7bd695768a23a614e4100856216519191
Christian <git-developer@users.noreply.github.com>
2019-09-09 11:34:33 +0200

    fix: update parsing of previously unknown values

--- modules/bezug_solarview/main.sh
+++ modules/bezug_solarview/main.sh
@@ -34,7 +34,7 @@ request() {
 
   [ "$debug" -ne 0 ] && >&2 echo "Raw response: $response"
   #
-  # Format:    {WR,Tag,Monat,Jahr,Stunde,Minute,KDY,KMT,KYR,KT0,PAC,???,???,UL1,IL1,UL2,IL2,UL3,IL3,TKK},Checksum
+  # Format:    {WR,Tag,Monat,Jahr,Stunde,Minute,KDY,KMT,KYR,KT0,PAC,UDC,IDC,UDCB,IDCB,UDCC,IDCC,UDCD,IDCD,TKK},Checksum
   # Beispiele: {22,09,09,2019,10,37,0001.2,00024,000903,00007817,01365,000,000.0,000,000.0,000,000.0,000,000.0,00},:
   #            {21,09,09,2019,10,37,0002.3,00141,004233,00029525,01365,000,000.0,000,000.0,000,000.0,000,000.0,00},;
   #
@@ -44,9 +44,8 @@ request() {
   #  KYR= Jahresertrag (kWh)
   #  KT0= Gesamtertrag (kWh)
   #  PAC= Generatorleistung in W
-  #  UL1, IL1= Netzspannung, Netzstrom Phase 1
-  #  UL2, IL2= Netzspannung, Netzstrom Phase 2
-  #  UL3, IL3= Netzspannung, Netzstrom Phase 3
+  #  UDC, UDCB, UDCC, UDCD= Generator-Spannungen in Volt pro MPP-Tracker
+  #  IDC, IDCB, IDCC, IDCD= Generator-Ströme in Ampere pro MPP-Tracker
   #  TKK= Temperatur Wechselrichter
 
   # Geschweifte Klammern und Checksumme entfernen
@@ -56,7 +55,7 @@ request() {
   # Werte auslesen und verarbeiten
   local LANG=C
   local IFS=','
-  echo "$values" | while read -r WR Tag Monat Jahr Stunde Minute KDY KMT KYR KT0 PAC UDC IDC UDCB IDCB UDCC IDCC X1 X2 UL1 IL1 UL2 IL2 UL3 IL3 TKK
+  echo "$values" | while read -r WR Tag Monat Jahr Stunde Minute KDY KMT KYR KT0 PAC UDC IDC UDCB IDCB UDCC IDCC UDCD IDCD TKK
   do
 
     # Werte formatiert in Variablen speichern
@@ -70,14 +69,14 @@ request() {
     energy_month=$(printf "%.0f" "$KMT")
     energy_year=$(printf "%.0f" "$KYR")
     energy_total=$(printf "%.0f" "$KT0")
-    x1=$(printf "%.0f" "$X1")
-    x2=$(printf "%.1f" "$X2")
-    grid1_voltage=$(printf "%.0f" "$UL1")
-    grid1_current=$(printf "%.1f" "$IL1")
-    grid2_voltage=$(printf "%.0f" "$UL2")
-    grid2_current=$(printf "%.1f" "$IL2")
-    grid3_voltage=$(printf "%.0f" "$UL3")
-    grid3_current=$(printf "%.1f" "$IL3")
+    mpptracker1_voltage=$(printf "%.0f" "$UDC")
+    mpptracker1_current=$(printf "%.1f" "$IDC")
+    mpptracker2_voltage=$(printf "%.0f" "$UDCB")
+    mpptracker2_current=$(printf "%.1f" "$IDCB")
+    mpptracker3_voltage=$(printf "%.0f" "$UDCC")
+    mpptracker3_current=$(printf "%.1f" "$IDCC")
+    mpptracker4_voltage=$(printf "%.0f" "$UDCD")
+    mpptracker4_current=$(printf "%.1f" "$IDCD")
     temperature=$(printf "%.0f" "$TKK")
 
     if [ "$debug" -ne 0 ]; then
@@ -91,18 +90,18 @@ request() {
       >&2 echo "  Monat:  $energy_month kWh"
       >&2 echo "  Jahr:   $energy_year kWh"
       >&2 echo "  Gesamt: $energy_total kWh"
-      >&2 echo "Unbekannt 1: $x1"
-      >&2 echo "Unbekannt 2: $x2"
-      >&2 echo "Netz:"
-      >&2 echo "  Phase 1:"
-      >&2 echo "    Spannung: $grid1_voltage V"
-      >&2 echo "    Strom:    $grid1_current A"
-      >&2 echo "  Phase 2:"
-      >&2 echo "    Spannung: $grid2_voltage V"
-      >&2 echo "    Strom:    $grid2_current A"
-      >&2 echo "  Phase 3:"
-      >&2 echo "    Spannung: $grid3_voltage V"
-      >&2 echo "    Strom:    $grid3_current A"
+      >&2 echo "Generator-MPP-Tracker-1"
+      >&2 echo "  Spannung: $mpptracker1_voltage V"
+      >&2 echo "  Strom:    $mpptracker1_current A"
+      >&2 echo "Generator-MPP-Tracker-2"
+      >&2 echo "  Spannung: $mpptracker2_voltage V"
+      >&2 echo "  Strom:    $mpptracker2_current A"
+      >&2 echo "Generator-MPP-Tracker-3"
+      >&2 echo "  Spannung: $mpptracker3_voltage V"
+      >&2 echo "  Strom:    $mpptracker3_current A"
+      >&2 echo "Generator-MPP-Tracker-4"
+      >&2 echo "  Spannung: $mpptracker4_voltage V"
+      >&2 echo "  Strom:    $mpptracker4_current A"
     fi
 
     # Werte speichern
--- modules/wr_solarview/main.sh
+++ modules/wr_solarview/main.sh
@@ -7,13 +7,6 @@
 
 . /var/www/html/openWB/openwb.conf
 
-
-# Sende-Kommando (siehe SolarView-Dokumentation); Beispiele:
-# '00*': Gesamte Anlage
-# '01*': Wechselrichter 1
-# '02*': Wechselrichter 2
-command="${1:-00*}"
-
 # Checks
 if [ -z "$solarview_hostname" ]; then
   >&2 echo "Missing required variable 'solarview_hostname'"
@@ -41,7 +34,7 @@ request() {
 
   [ "$debug" -ne 0 ] && >&2 echo "Raw response: $response"
   #
-  # Format:   {WR,Tag,Monat,Jahr,Stunde,Minute,KDY,KMT,KYR,KT0,PAC,UDC,IDC,UDCB,IDCB,UDCC,IDCC,???,???,UL1,IL1,UL2,IL2,UL3,IL3,TKK},Checksum
+  # Format:   {WR,Tag,Monat,Jahr,Stunde,Minute,KDY,KMT,KYR,KT0,PAC,UDC,IDC,UDCB,IDCB,UDCC,IDCC,UDCD,IDCD,UL1,IL1,UL2,IL2,UL3,IL3,TKK},Checksum
   # Beispiel: {01,09,09,2019,08,18,0000.0,00082,002617,00018691,00104,451,000.2,000,000.0,000,000.0,000,000.0,226,000.4,000,000.0,000,000.0,00},▒
   #
   # Bedeutung (siehe SolarView-Dokumentation):
@@ -50,8 +43,8 @@ request() {
   #  KYR= Jahresertrag (kWh)
   #  KT0= Gesamtertrag (kWh)
   #  PAC= Generatorleistung in W
-  #  UDC, UDCB, UDCC= Generator-Spannungen in Volt pro MPP-Tracker
-  #  IDC, IDCB, IDCC= Generator-Ströme in Ampere pro MPP-Tracker
+  #  UDC, UDCB, UDCC, UDCD= Generator-Spannungen in Volt pro MPP-Tracker
+  #  IDC, IDCB, IDCC, IDCD= Generator-Ströme in Ampere pro MPP-Tracker
   #  UL1, IL1= Netzspannung, Netzstrom Phase 1
   #  UL2, IL2= Netzspannung, Netzstrom Phase 2
   #  UL3, IL3= Netzspannung, Netzstrom Phase 3
@@ -64,7 +57,7 @@ request() {
   # Werte auslesen und verarbeiten
   local LANG=C
   local IFS=','
-  echo "$values" | while read -r WR Tag Monat Jahr Stunde Minute KDY KMT KYR KT0 PAC UDC IDC UDCB IDCB UDCC IDCC X1 X2 UL1 IL1 UL2 IL2 UL3 IL3 TKK
+  echo "$values" | while read -r WR Tag Monat Jahr Stunde Minute KDY KMT KYR KT0 PAC UDC IDC UDCB IDCB UDCC IDCC UDCD IDCD UL1 IL1 UL2 IL2 UL3 IL3 TKK
   do
 
     # Werte formatiert in Variablen speichern
@@ -76,14 +69,14 @@ request() {
     energy_month=$(printf "%.0f" "$KMT")
     energy_year=$(printf "%.0f" "$KYR")
     energy_total=$(printf "%.0f" "$KT0")
-    x1=$(printf "%.0f" "$X1")
-    x2=$(printf "%.1f" "$X2")
     mpptracker1_voltage=$(printf "%.0f" "$UDC")
     mpptracker1_current=$(printf "%.1f" "$IDC")
     mpptracker2_voltage=$(printf "%.0f" "$UDCB")
     mpptracker2_current=$(printf "%.1f" "$IDCB")
     mpptracker3_voltage=$(printf "%.0f" "$UDCC")
     mpptracker3_current=$(printf "%.1f" "$IDCC")
+    mpptracker4_voltage=$(printf "%.0f" "$UDCD")
+    mpptracker4_current=$(printf "%.1f" "$IDCD")
     grid1_voltage=$(printf "%.0f" "$UL1")
     grid1_current=$(printf "%.1f" "$IL1")
     grid2_voltage=$(printf "%.0f" "$UL2")
@@ -103,8 +96,6 @@ request() {
       >&2 echo "  Monat:  $energy_month kWh"
       >&2 echo "  Jahr:   $energy_year kWh"
       >&2 echo "  Gesamt: $energy_total kWh"
-      >&2 echo "Unbekannt 1: $x1"
-      >&2 echo "Unbekannt 2: $x2"
       >&2 echo "Generator-MPP-Tracker-1"
       >&2 echo "  Spannung: $mpptracker1_voltage V"
       >&2 echo "  Strom:    $mpptracker1_current A"
@@ -114,6 +105,9 @@ request() {
       >&2 echo "Generator-MPP-Tracker-3"
       >&2 echo "  Spannung: $mpptracker3_voltage V"
       >&2 echo "  Strom:    $mpptracker3_current A"
+      >&2 echo "Generator-MPP-Tracker-4"
+      >&2 echo "  Spannung: $mpptracker4_voltage V"
+      >&2 echo "  Strom:    $mpptracker4_current A"
       >&2 echo "Netz:"
       >&2 echo "  Phase 1:"
       >&2 echo "    Spannung: $grid1_voltage V"
@@ -137,6 +131,12 @@ request() {
     echo "$power"
   done
 }
+
+# Sende-Kommando (siehe SolarView-Dokumentation); Beispiele:
+# '00*': Gesamte Anlage
+# '01*': Wechselrichter 1
+# '02*': Wechselrichter 2
+command="${1:-00*}"
 
 power=$(request "$command")
 echo "$power"
