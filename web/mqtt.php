ae089d6a6d8fca046c5d859dbee1ba5bf1c77e6d
Kurt <dj3mu@darc.de>
2019-12-01 16:25:54 +0100

    Modified for discussion results.
    Last version in English language. UI will now be translated to German.

--- web/mqtt.php
+++ web/mqtt.php
@@ -70,25 +70,29 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
 	</div>
 
     <?php
-        $files = glob('/etc/mosquitto/conf.d/*-bridge-*.conf');
+        $files = glob('/etc/mosquitto/conf.d/*-bridge-*.conf*');
+        array_push($files, "");
         $firstLoopDone = false;
         foreach($files as $currentFile)
         {
-            $currentBridge = preg_replace('/^.*-bridge-(.+)\.conf$/', '${1}', $currentFile);
+            $currentBridge = preg_replace('/^99-bridge-(.+)\.conf/', '${1}', $currentFile);
 
             $bridgeLines = file($currentFile);
-            $connectionName = "<not found>";
-            $remoteAddressAndPort = "<not found>:8883";
+            $connectionName = "unique-connection-name";
+            $remoteAddressAndPort = "your.remote.mqtt.host:8883";
             $remotePrefix = NULL;
-            $remoteUser = "<not found>";
+            $remoteUser = "remote-mqtt-host-user-name";
             $remotePassword = "";
-            $remoteClientId = "<not found>";
+            $remoteClientId = "unique-mqtt-client-id-used-with-remote";
             $mqttProtocol = "mqttv31";
             $exportGlobal = false;
             $exportEvu = false;
             $exportPv = false;
             $exportAllLps = false;
             $subscribeChargeMode = false;
+            $exportGraph = false;
+            $tlsVersion = "tlsv1.2";
+            $bridgeEnabled = preg_match('/.*\.conf$/', $currentFile) === 1;
             foreach($bridgeLines as $bridgeLine) {
                 //echo "line '$bridgeLine'<br/>";
                 if(is_null($remotePrefix) && preg_match('/^\s*topic\s+([^\s]+?)\s+([^\s]+?)\s+([^\s]+?)\s+([^\s]+?)\s+([^\s]+?)\s+/', $bridgeLine, $matches) === 1) {
@@ -110,6 +114,9 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
                 else if(preg_match('/^\s*bridge_protocol_version\s+(.+)/', $bridgeLine, $matches) === 1) {
                     $mqttProtocol = trim($matches[1]);
                 }
+                else if(preg_match('/^\s*bridge_tls_version\s+(.+)/', $bridgeLine, $matches) === 1) {
+                    $tlsVersion = trim($matches[1]);
+                }
                 
                 if(preg_match('/^\s*topic\s+openWB\/global\/#/', $bridgeLine) === 1) {
                     $exportGlobal = true;
@@ -119,6 +126,9 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
                 }
                 if(preg_match('/^\s*topic\s+openWB\/pv\/#/', $bridgeLine) === 1) {
                     $exportPv = true;
+                }
+                if(preg_match('/^\s*topic\s+openWB\/graph\/#/', $bridgeLine) === 1) {
+                    $exportGraph = true;
                 }
                 if(preg_match('/^\s*topic\s+openWB\/lp\/#/', $bridgeLine) === 1) {
                     $exportAllLps = true;
@@ -135,25 +145,25 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
     ?>
             <form action="./tools/savemqtt.php?bridge=<?php echo urlencode($connectionName); ?>" method="POST">
                 <div class="row">
-                    <b><label>Bridge <input type="text" name="ConnectionName" id="ConnectionName" value="<?php echo $connectionName; ?>" /></label></b><br/>
+                    <input type="checkbox" name="bridgeEnabled" value="bridgeEnabled" <?php echo $bridgeEnabled ? "checked=\"checked\"" : "" ?>><?php echo $bridgeEnabled ? "": "&nbsp;&nbsp;<em>! Disabled !</em>" ?><b><label>&nbsp;&nbsp;Bridge&nbsp;&nbsp;<input type="text" size="35" name="ConnectionName" id="ConnectionName" value="<?php echo $connectionName; ?>" /></label></b><br/>
                     <?php if($debugold >= 1) echo "<small>in file '$currentFile'</small>"; ?><br/>
                     <span style="color: red; font-weight: bold; font-size:small;"><u>ATTENTION</u>: Configuring an MQTT might allow all users with access to the remote MQTT server to see your openWB data !<br/>
                     It's highly recommended to enable only for private MQTT servers with strong transport encryption, personal login and strict access control, at least for topics starting with the "Remote Prefix" configured below !</span>
                 </div>
                 <div style="margin-top: 15px;">
-                    Remote Address and Port:&nbsp;<input type="text" name="RemoteAddress" id="RemoteAddress" value="<?php echo $remoteAddressAndPort; ?>" /><br/>
+                    Remote Address and Port:&nbsp;<input type="text" size="50" name="RemoteAddress" id="RemoteAddress" value="<?php echo $remoteAddressAndPort; ?>" /><br/>
                     <small>Remote MQTT server address and port number. Default port is 8883 for TLS protected connections.</small>
                 </div>
                 <div style="margin-top: 15px;">
-                    Remote User:&nbsp;<input type="text" name="RemoteAddress" id="RemoteAddress" value="<?php echo $remoteUser; ?>" /><br/>
+                    Remote User:&nbsp;<input type="text" size="35" name="RemoteUser" id="RemoteUser" value="<?php echo $remoteUser; ?>" /><br/>
                     <small>User name to log in on the remote MQTT server.</small>
                 </div>
                 <div style="margin-top: 15px;">
-                    Remote Password:&nbsp;<input type="password" name="RemoteAddress" id="RemoteAddress" value="<?php echo $remotePassword; ?>" /><br/>
+                    Remote Password:&nbsp;<input type="password" size="35" name="RemotePass" id="RemotePass" value="<?php echo $remotePassword; ?>" /><br/>
                     <small>Password to log in on the remote MQTT server. White spaces at beginning or end of the password are NOT supported.</small>
                 </div>
                 <div style="margin-top: 15px;">
-                    Remote Prefix:&nbsp;<input type="text" name="RemotePrefix" id="RemotePrefix" value="<?php echo $remotePrefix; ?>" /><br/>
+                    Remote Prefix:&nbsp;<input type="text" size="55" name="RemotePrefix" id="RemotePrefix" value="<?php echo $remotePrefix; ?>" /><br/>
                     <small>The topic prefix to prepend to the 'openWB/...' topics.<br/>
                     Example: If this field is 'pfx' then the topic publish and subscribed to remote will be 'pfx/openWB/...' </small>
                 </div>
@@ -170,6 +180,20 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
                     </fieldset>
                 </div>
                 <div style="margin-top: 15px;">
+                    TLS Protocol:<br/>
+                    <small>The TLS protocol version to use for encrypting the traffic to the remote MQTT server.</small>
+                    <fieldset>
+                        <table>
+                        <tr>
+                            <td style="text-align: left; vertical-align: center; padding: 10px;"><input type="radio" name="tlsProtocol" value="tlsv1.3" disabled <?php echo $tlsVersion == "tlsv1.3" ? "checked=\"checked\"" : "" ?>>&nbsp;TLSv1.3<br/><span style="color: darkgreen; font-size:small;">Recommended<br/>But not supported on<br/>Debian Stretch</span></td>
+                            <td style="text-align: left; vertical-align: center; padding: 10px;"><input type="radio" name="tlsProtocol" value="tlsv1.2" <?php echo $tlsVersion == "tlsv1.2" ? "checked=\"checked\"" : "" ?>>&nbsp;TLSv1.2<br/><span style="color: lightgreen; font-size:small;">Recommended</span></td>
+                            <!-- td style="text-align: left; vertical-align: center; padding: 10px;"><input type="radio" name="tlsProtocol" value="tlsv1.1" <?php echo $tlsVersion == "tlsv1.1" ? "checked=\"checked\"" : "" ?>>&nbsp;TLSv1.1<br/><span style="color: darkred; font-size:small;">Can be broken.</span><td/ -->
+                            <td style="text-align: left; vertical-align: center; padding: 10px;"><input type="radio" name="tlsProtocol" value="none" <?php echo $tlsVersion == "none" ? "checked=\"checked\"" : "" ?>>&nbsp;No encryption<br/><span style="color: red; font-size:small;">Highly unsafe !<br/>Transmits plain-text passwords.<br/>Use only in protected environments!</span></td>
+                        </tr>
+                        </table>
+                    </fieldset>
+                </div>
+                <div style="margin-top: 15px;">
                     <b>Publish to remote:</b><br/>
                     <small>The data that shall be sent to the remote MQTT server.</small>
                     <fieldset>
@@ -180,6 +204,7 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
                             <td style="text-align: left; vertical-align: center; padding: 10px;"><input type="checkbox" name="pvData" value="pvData" <?php echo $exportPv ? "checked=\"checked\"" : "" ?>>&nbsp;PV Data</td>
                         <tr>
                             <td style="text-align: left; vertical-align: center; padding: 10px;"><input type="checkbox" name="allLps" value="allLps" <?php echo $exportAllLps ? "checked=\"checked\"" : "" ?>>&nbsp;All Charging Points</td>
+                            <td style="text-align: left; vertical-align: center; padding: 10px;"><input type="checkbox" name="graph" value="graph" <?php echo $exportGraph ? "checked=\"checked\"" : "" ?>>&nbsp;Graphing Data</td>
                         </tr>
                         </table>
                     </fieldset>
@@ -198,6 +223,9 @@ $settingspwsold = str_replace( "'", "", $settingspwold);
                         </tr>
                         </table>
                     </fieldset>
+                </div>
+                <div>
+                    <button type="submit" name="action" value="saveBridge">Save bridge '<?php echo urlencode($connectionName); ?>'</button>
                 </div>
             </form>
     <?php
