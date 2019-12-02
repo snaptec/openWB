bfc5c12aed91eb1a76f0e23568a3fcfd8c3c957d
Kurt <dj3mu@darc.de>
2019-12-02 16:54:46 +0100

    Extended sent debug data for MQTT bridge configs

--- runs/senddebug.sh
+++ runs/senddebug.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 sed -i 's/debug.*/debug=1/' /var/www/html/openWB/openwb.conf
-sleep 60
-echo "$(tail -1000 /var/www/html/openWB/ramdisk/openWB.log)" > /var/www/html/openWB/ramdisk/debug.log
-echo "############################ mqtt ##############" >> /var/www/html/openWB/ramdisk/debug.log
-echo "$(tail -200 /var/www/html/openWB/ramdisk/mqtt.log)" >> /var/www/html/openWB/ramdisk/debug.log
-echo "############################ config ##############" >> /var/www/html/openWB/ramdisk/debug.log
+sleep 1
 
-grep -F -v -e leaf -e i3 -e zoe -e tesla -e carnet /var/www/html/openWB/openwb.conf >> /var/www/html/openWB/ramdisk/debug.log
-cat /var/www/html/openWB/ramdisk/debuguser >> /var/www/html/openWB/ramdisk/debug.log
-echo "############################ version ##############" >> /var/www/html/openWB/ramdisk/debug.log
+debugFile=/var/www/html/openWB/ramdisk/debug.log
 
-echo "Version" >> /var/www/html/openWB/ramdisk/debug.log
-cat /var/www/html/openWB/web/version >> /var/www/html/openWB/ramdisk/debug.log
-curl --upload /var/www/html/openWB/ramdisk/debug.log https://openwb.de/tools/debug.php
+echo "$(tail -1000 /var/www/html/openWB/ramdisk/openWB.log)" > $debugFile
+
+echo "############################ mqtt ##############" >> $debugFile
+echo "$(tail -200 /var/www/html/openWB/ramdisk/mqtt.log)" >> $debugFile
+
+for currentConfig in /etc/mosquitto/conf.d/99-bridge-*; do
+    if [ -f "$currentConfig" ]; then
+        echo "############################ mqtt bridge '$currentConfig' ######" >> $debugFile
+        sudo grep -F -v -e password "$currentConfig" | sed '/^#/ d'>> $debugFile
+    fi
+done
+
+echo "############################ config ##############" >> $debugFile
+grep -F -v -e leaf -e i3 -e zoe -e tesla -e carnet /var/www/html/openWB/openwb.conf >> $debugFile
+cat /var/www/html/openWB/ramdisk/debuguser >> $debugFile
+
+echo "############################ version ##############" >> $debugFile
+echo "Version" >> $debugFile
+cat /var/www/html/openWB/web/version >> $debugFile
+
+#curl --upload $debugFile https://openwb.de/tools/debug.php
 
 sed -i 's/debug.*/debug=0/' /var/www/html/openWB/openwb.conf
 rm /var/www/html/openWB/ramdisk/debuguser
-
