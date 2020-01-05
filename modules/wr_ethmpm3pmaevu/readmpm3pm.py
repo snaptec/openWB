390e78a5afa27fe6890d3b9b452accfe6a374961
lackylacky <57707146+lackylacky@users.noreply.github.com>
2020-01-05 16:11:08 +0100

    Update readmpm3pm.py
    hast natürlich recht ;)

--- modules/wr_ethmpm3pmaevu/readmpm3pm.py
+++ modules/wr_ethmpm3pmaevu/readmpm3pm.py
@@ -27,12 +27,7 @@ f = open('/var/www/html/openWB/ramdisk/pvkwh', 'w')
 f.write(str(ikwh))
 f.close()
 
-resp = client.read_input_registers(0x0004,4, unit=8)
-value1 = resp.registers[0] 
-value2 = resp.registers[1] 
-all = format(value1, '04x') + format(value2, '04x')
-ikwhk = int(struct.unpack('>i', all.decode('hex'))[0]) 
-ikwhk = float(ikwhk) / 100
+ikwhk = float(ikwh) / 1000
 f = open('/var/www/html/openWB/ramdisk/pvkwhk', 'w')
 f.write(str(ikwhk))
 f.close()
