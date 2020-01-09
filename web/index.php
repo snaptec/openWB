c1c4db4d433e116882ec30a8e690944a37b96446
Michael <michael@ortenstein.de>
2020-01-09 16:49:32 +0100

    changes to neta tag

--- web/index.php
+++ web/index.php
@@ -22,7 +22,7 @@
 		var chartjsSelected = <?php echo $graphliveamold ?>;
 	</script>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1,user-scalable=0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
          <meta name="apple-mobile-web-app-capable" content="yes">
          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
          <meta name="apple-mobile-web-app-title" content="openWB">
