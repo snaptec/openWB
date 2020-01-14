2bceeeb3cd039f15ce58b25fc1ecbbb5f93f2360
snaptec <info@snaptec.org>
2020-01-14 07:19:44 +0100

    Bugfix SMA SHM

--- modules/bezug_smashm/main.sh
+++ modules/bezug_smashm/main.sh
@@ -2,6 +2,6 @@
 
 . /var/www/html/openWB/openwb.conf
 
-timeout 3 python3 /var/www/html/openWB/modules/bezug_smashm/sma-em-measurement.py || true
+timeout 3 python3 /var/www/html/openWB/modules/bezug_smashm/sma-em-measurement.py 
 wattbezug=$(</var/www/html/openWB/ramdisk/wattbezug)
 echo $wattbezug
