98db7302c72d08696ab7a6d250eb1faac25a85b1
Michael <michael@ortenstein.de>
2020-01-18 11:16:51 +0100

    process hidden theme first

--- web/index.php
+++ web/index.php
@@ -1,20 +1,4 @@
 <?php
-	// check if theme cookie exists
-	// then expand period of validity
-	// else set standard theme
-	if ( !(isset($_COOKIE['openWBTheme'] ) === true)) {
-		setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
-		$_COOKIE['openWBTheme'] = 'standard';
-	} else {
-		$themeCookie = $_COOKIE['openWBTheme'];
-		setcookie('openWBTheme', $themeCookie, time()+(60*60*24*365*2));
-	}
-	// check if theme exists
-	// if not, set standard theme
-	if ( is_dir('themes/'.$_COOKIE['openWBTheme']) == 0 ) {
-		setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
-		$_COOKIE['openWBTheme'] = 'standard';
-	}
 	// check if forced theme is activated in config file
 	$simplemodeold = '';
 	$lines = file('/var/www/html/openWB/openwb.conf');
@@ -28,6 +12,22 @@
 		include 'themes/hidden/simplemode.php';
 	} else {
 		// theme set by cookie
+		// check if theme cookie exists
+		// then expand period of validity
+		// else set standard theme
+		if ( !(isset($_COOKIE['openWBTheme'] ) === true)) {
+			setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
+			$_COOKIE['openWBTheme'] = 'standard';
+		} else {
+			$themeCookie = $_COOKIE['openWBTheme'];
+			setcookie('openWBTheme', $themeCookie, time()+(60*60*24*365*2));
+		}
+		// check if theme exists
+		// if not, set standard theme
+		if ( is_dir('themes/'.$_COOKIE['openWBTheme']) == 0 ) {
+			setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
+			$_COOKIE['openWBTheme'] = 'standard';
+		}
 		include 'themes/'.$_COOKIE['openWBTheme'].'/theme.html';
 	}
 ?>
