97cfda02ff36af38d4c39c73665caf9a44bf938a
Michael <michael@ortenstein.de>
2020-01-18 11:09:49 +0100

    set standard theme if configured was deleted/renamed

--- web/index.php
+++ web/index.php
@@ -1,27 +1,33 @@
 <?php
-// check if theme cookie exists
-// then expand period of validity
-// else set standard theme
-if(!(isset($_COOKIE['openWBTheme']) === true)) {
-	setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
-	$_COOKIE['openWBTheme'] = 'standard';
-} else {
-	$themeCookie = $_COOKIE['openWBTheme'];
-	setcookie('openWBTheme', $themeCookie, time()+(60*60*24*365*2));
-}
-// check if forced theme is activated in config file
-$simplemodeold = '';
-$lines = file('/var/www/html/openWB/openwb.conf');
-foreach($lines as $line) {
-	if(strpos($line, "simplemode=") !== false) {
-		list(, $simplemodeold) = explode("=", $line);
+	// check if theme cookie exists
+	// then expand period of validity
+	// else set standard theme
+	if ( !(isset($_COOKIE['openWBTheme'] ) === true)) {
+		setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
+		$_COOKIE['openWBTheme'] = 'standard';
+	} else {
+		$themeCookie = $_COOKIE['openWBTheme'];
+		setcookie('openWBTheme', $themeCookie, time()+(60*60*24*365*2));
 	}
-}
-if ( $simplemodeold == 1 ) {
-	// force hidden theme
-	include 'themes/hidden/simplemode.php';
-} else {
-	// theme set by cookie
-	include 'themes/'.$_COOKIE['openWBTheme'].'/theme.html';
-}
+	// check if theme exists
+	// if not, set standard theme
+	if ( is_dir('themes/'.$_COOKIE['openWBTheme']) == 0 ) {
+		setcookie('openWBTheme', 'standard', time()+(60*60*24*365*2));
+		$_COOKIE['openWBTheme'] = 'standard';
+	}
+	// check if forced theme is activated in config file
+	$simplemodeold = '';
+	$lines = file('/var/www/html/openWB/openwb.conf');
+	foreach( $lines as $line ) {
+		if( strpos($line, "simplemode=") !== false ) {
+			list(, $simplemodeold) = explode("=", $line);
+		}
+	}
+	if ( $simplemodeold == 1 ) {
+		// force hidden theme
+		include 'themes/hidden/simplemode.php';
+	} else {
+		// theme set by cookie
+		include 'themes/'.$_COOKIE['openWBTheme'].'/theme.html';
+	}
 ?>
