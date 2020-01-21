0bbd7b2aa0936512c9edcd89093c2726c2c3477b
snaptec <info@snaptec.org>
2020-01-21 21:14:40 +0100

    Fix timing

--- web/tools/update.php
+++ web/tools/update.php
@@ -1,13 +1,13 @@
 <html>
 	<head>
-		<meta http-equiv="refresh" content="240;url=../index.php" />
+		<meta http-equiv="refresh" content="20;url=../index.php" />
 	</head>
 <?php
 	echo "Update wird durchgeführt, bitte nicht vom Strom trennen";
 	exec("/var/www/html/openWB/runs/update.sh > /dev/null &");
-	header( "refresh:240;url=../index.php" );
+	header( "refresh:20;url=../index.php" );
 ?>
 <script type="text/javascript">
-   setTimeout(function() { window.location.href = "../index.php"; }, 240000);
+   setTimeout(function() { window.location.href = "../index.php"; }, 20000);
 </script>
 </html>
