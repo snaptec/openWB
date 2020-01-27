0fa0faaa83bdd1603317ec16267852d0aa675bf0
snaptec <info@snaptec.org>
2020-01-27 20:01:27 +0100

    Long Live Graph mit Chart.js hinzugefügt

--- graphing.sh
+++ graphing.sh
@@ -82,61 +82,14 @@ mosquitto_pub -t openWB/graph/8alllivevalues -r -m "$([ ${#all8livevalues} -ge 1
 
 #Long Time Graphing
 if (( graphtimer == 1 )); then
-	echo $((pvwatt * -1)) >> /var/www/html/openWB/ramdisk/pv.graph
-	echo $wattbezugint >> /var/www/html/openWB/ramdisk/evu.graph
-	echo $soc >> /var/www/html/openWB/ramdisk/soc.graph
-	echo $ladeleistung >> /var/www/html/openWB/ramdisk/ev.graph
-	if (( speichervorhanden == 1 )); then
-		echo $speicherleistung >> /var/www/html/openWB/ramdisk/speicher.graph
-		echo $speichersoc >> /var/www/html/openWB/ramdisk/speichersoc.graph
-	fi
-	if [[ socmodul1 != "none" ]]; then
-		echo $soc1 >> /var/www/html/openWB/ramdisk/soc1.graph
-	fi
-	echo $ladeleistunglp1 >> /var/www/html/openWB/ramdisk/ev1.graph
-	echo $hausverbrauch >> /var/www/html/openWB/ramdisk/hausverbrauch.graph
-	if (( lastmanagement == 1 )); then
-		echo $ladeleistunglp2 >> /var/www/html/openWB/ramdisk/ev2.graph
-	fi
-	if (( verbraucher1_aktiv == 1 )); then
-		echo $verbraucher1_watt >> /var/www/html/openWB/ramdisk/verbraucher1.graph
-	fi
-	if (( verbraucher2_aktiv == 1 )); then
-		echo $verbraucher2_watt >> /var/www/html/openWB/ramdisk/verbraucher2.graph
-	fi
 	if (( dpseed == "3" )); then
 		livegraphtime="240"
 	else
 		livegraphtime="720"
 	fi
-	date +%H:%M >> /var/www/html/openWB/ramdisk/time.graph
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/pv.graph)" > /var/www/html/openWB/ramdisk/pv.graph
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/evu.graph)" > /var/www/html/openWB/ramdisk/evu.graph
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/soc.graph)" > /var/www/html/openWB/ramdisk/soc.graph
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/ev.graph)" > /var/www/html/openWB/ramdisk/ev.graph 
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/ev1.graph)" > /var/www/html/openWB/ramdisk/ev1.graph
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/hausverbrauch.graph)" > /var/www/html/openWB/ramdisk/hausverbrauch.graph
-	if (( verbraucher1_aktiv == 1 )); then
-		echo "$(tail -$livegraph /var/www/html/openWB/ramdisk/verbraucher1.graph)" > /var/www/html/openWB/ramdisk/verbraucher1.graph 
-	fi
-	if (( verbraucher2_aktiv == 1 )); then
-		echo "$(tail -$livegraph /var/www/html/openWB/ramdisk/verbraucher2.graph)" > /var/www/html/openWB/ramdisk/verbraucher2.graph 
-	fi
-	if (( lastmanagement == 1 )); then	
-		echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/ev2.graph)" > /var/www/html/openWB/ramdisk/ev2.graph 
-	fi
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/time.graph)" > /var/www/html/openWB/ramdisk/time.graph
-	if ((speichervorhanden == 1 )); then
-		echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/speicher.graph)" > /var/www/html/openWB/ramdisk/speicher.graph
-		echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/speichersoc.graph)" > /var/www/html/openWB/ramdisk/speichersoc.graph
-	fi
-	if [[ socmodul1 != "none" ]]; then
-		echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/soc1.graph)" > /var/www/html/openWB/ramdisk/soc1.graph
-	fi
-	#beta testing
-	echo $(date +%H:%M:%S),$wattbezugint,$ladeleistung,$pvgraph,$ladeleistunglp1,$ladeleistunglp2,$ladeleistung,$speicherleistung,$speichersoc,$soc,$soc1,$hausverbrauch,$verbraucher1_watt,$verbraucher2_watt >> /var/www/html/openWB/ramdisk/all.graph
-	echo "$(tail -$livegraphtime /var/www/html/openWB/ramdisk/all.graph)" > /var/www/html/openWB/ramdisk/all.graph
-	#end beta testing
+	longlivetime=$((livegraphtime*2))
+	echo $(date +%H:%M:%S),$wattbezugint,$ladeleistung,$pvgraph,$ladeleistunglp1,$ladeleistunglp2,$ladeleistunglp3,$ladeleistunglp4,$ladeleistunglp5,$ladeleistunglp6,$ladeleistunglp7,$ladeleistunglp8,$speicherleistung,$speichersoc,$soc,$soc1,$hausverbrauch,$verbraucher1_watt,$verbraucher2_watt >> /var/www/html/openWB/ramdisk/all.graph
+	echo "$(tail -$longlivetime /var/www/html/openWB/ramdisk/all.graph)" > /var/www/html/openWB/ramdisk/all.graph
 
 
 fi
