655b6286980e517241ae49b6d2f1827d0cb527d9
Michael <michael@ortenstein.de>
2020-02-02 19:35:19 +0100

    new update page

--- web/tools/update.php
+++ web/tools/update.php
@@ -1,13 +1,73 @@
-<html>
+<!DOCTYPE html>
+<html lang="de">
+
 	<head>
-		<meta http-equiv="refresh" content="20;url=../index.php" />
+		<base href="/openWB/web/">
+
+		<meta charset="UTF-8">
+		<title>openWB update</title>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-4.4.1/bootstrap.min.css">
+		<!-- Normalize -->
+		<link rel="stylesheet" type="text/css" href="css/normalize-8.0.1.css">
+		<!-- include reboot-style -->
+		<link rel="stylesheet" type="text/css" href="tools/reboot.css">
+
+		<!-- important scripts to be loaded -->
+		<script src="js/jquery-3.4.1.min.js"></script>
+		<script src="js/bootstrap-4.4.1/bootstrap.bundle.min.js"></script>
 	</head>
-<?php
-	echo "Update wird durchgeführt, bitte nicht vom Strom trennen";
-	exec("/var/www/html/openWB/runs/update.sh > /dev/null &");
-	header( "refresh:20;url=../index.php" );
-?>
-<script type="text/javascript">
-   setTimeout(function() { window.location.href = "../index.php"; }, 20000);
-</script>
+
+	<body>
+
+		<header>
+		    <!-- Fixed navbar -->
+		    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+		        <div class="navbar-brand">
+		            openWB
+		        </div>
+		    </nav>
+		</header>
+
+		<div role="main" class="container" style="margin-top:20px">
+			<div class="row">
+				<div class="col text-center">
+					<h1>Update</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col text-center">
+					<p id="infoText"></p>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="cssload-loader text-center">
+					<div class="cssload-inner cssload-one"></div>
+					<div class="cssload-inner cssload-two"></div>
+					<div class="cssload-inner cssload-three"></div>
+				</div>
+			</div>
+		</div>  <!-- container -->
+
+		<footer class="footer bg-dark text-light font-small">
+		  <div class="container text-center">
+		  </div>
+		</footer>
+
+		<script type="text/javascript">
+
+			$(document).ready(function(){
+
+				$("#infoText").text("Update der openWB angefordert...");
+				$.get("./tools/updatePerformNow.php").done(function() {
+					setTimeout(function() { window.location.href = "./index.php"; }, 20000);
+				})
+
+			});
+
+		</script>
+
+	</body>
 </html>
