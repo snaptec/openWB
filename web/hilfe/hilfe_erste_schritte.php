9c6ede9e8f666b4f94488811bb0893aacb87591c
Michael Ortenstein <develop@ortenstein.de>
2020-02-09 13:38:57 +0100

    new help layout

---
+++ web/hilfe/hilfe_erste_schritte.php
@@ -0,0 +1,41 @@
+<div id="bedienung">
+	<h3>Bedienung</h3>
+		<p> Lademodus</p><br>
+			Der Unterschied zwischen "Standby" und "Stop" ist, dass bei "Stop" keine Ladung erlaubt ist.<br>
+		Im Semistop Modus kann die Ladung durch Nachtladen und Zielladen aktiviert werden<br>
+		<hr>
+   <br>
+	Etwas unklar in der Bedienung?<br>
+	Passende Erklärung parat?<br>
+	<br> Mithilfe erwünscht. Fragen oder Hilfetexte bitte im <a href="https://openwb.de/forum/">Forum</a> posten oder an info@snaptec.de
+</div>
+
+<div id="ladeeinstellung">
+	<h3>Ladeeinstellung</h3>
+	<p>Meldung "Lastmanagement aktiv, Ladeleistung reduziert"</p>
+	<br> Wurde innerhalb der letzten 2 Minuten das Lastmanagement aktiv, wird diese Meldung auf der Hauptseite unterhalb des Graphen angezeigt.<br>
+   	Sie dient rein der Information, dass das Lastmanagement von OpenWB aktiv ist und die gewünschte bzw. eingestellte Ladeleistung derzeit nicht möglich ist.<br>
+   	Es besteht kein Handlungsbedarf. Wenn mehr Leistung zur Verfügung steht, regelt OpenWB automatisch auf den gewünschten Wert hoch.<br>
+   	<br><br><br>
+	Etwas unklar bei den Ladeeinstellungen?<br>
+	Passende Erklärung parat?<br>
+   <br> Mithilfe erwünscht. Fragen oder Hilfetexte bitte im <a href="https://openwb.de/forum/">Forum</a> posten oder an info@snaptec.de
+</div>
+
+
+<div id="misc">
+	<h3>Misc</h3>
+	<p> EVSE Modbus Test zu finden unter Status.<br></p>
+	Der Test dient zur Hilfe bei der Erstkonfiguration.<br>Er prüft die korrekte Kommunikation zur EVSE.<br> Bei Verwendung eines Modbus/LanKonverters baut der Test zunächst die dafür benötigte Verbindung auf.<br>
+	Nutzung:<br><br>
+	Vor Test den Lademodus auf der Hauptseite auf Stop stellen.<br>
+	Für den gewünschten Ladepunkt auf Testen klicken<br>
+	<br>Die Seite lädt nun neu und man erhält nach 3-10 Sekunden automatisch ein Ergebnis:<br>
+	<br> erfolgreich = Kommunikation funktioniert
+	<br> Fehler = Kommunikation mit der EVSE nicht möglich. Korrekte Konfiguration bzw. Verkabelung prüfen
+	<br> Platzhalter konfiguriert = es ist an diesem Ladepunkt keine Modbus EVSE konfiguriert
+	<hr>
+	Sonstige Hilfe die hier stehen sollte?<br>
+	Passende Erklärung parat?<br>
+   <br> Mithilfe erwünscht. Fragen oder Hilfetexte bitte im <a href="https://openwb.de/forum/">Forum</a> posten oder an info@snaptec.de
+</div>
---
+++ web/hilfe/hilfe_index.php
@@ -0,0 +1,3 @@
+<div>
+	Inhalt noch nicht fertig
+</div>
