ef4b948bf51043c28ba9261256d0356b6f93f66b
snaptec <info@snaptec.org>
2020-02-13 15:11:30 +0100

    Unterstützung für openWB EVU Kit V2 hinzugefügt

--- modules/bezug_ethmpm3pm/main.sh
+++ modules/bezug_ethmpm3pm/main.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 . /var/www/html/openWB/openwb.conf
-
-sudo python /var/www/html/openWB/modules/bezug_ethmpm3pm/readmpm3pm.py 
+if (( evukitversion == 1 )); then
+	sudo python /var/www/html/openWB/modules/bezug_ethmpm3pm/readlovato.py
+else
+	sudo python /var/www/html/openWB/modules/bezug_ethmpm3pm/readmpm3pm.py 
+fi
 wattbezug=$(</var/www/html/openWB/ramdisk/wattbezug)
 echo $wattbezug
 
--- runs/atreboot.sh
+++ runs/atreboot.sh
@@ -2321,6 +2321,10 @@ then
 	echo "myrenault_locationlp2=de_DE" >> /var/www/html/openWB/openwb.conf
 	echo "myrenault_countrylp2=DE" >> /var/www/html/openWB/openwb.conf
 fi
+if ! grep -Fq "evukitversion=" /var/www/html/openWB/openwb.conf
+then
+	echo "evukitversion=0" >> /var/www/html/openWB/openwb.conf
+fi
 sudo kill $(ps aux |grep '[m]qttsub.py' | awk '{print $2}')
 if ps ax |grep -v grep |grep "python3 /var/www/html/openWB/runs/mqttsub.py" > /dev/null
 then
