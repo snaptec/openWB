cb33600d4ad1bf2237b43b985746065b33925b8d
Lutz Bender <github@lutz-bender.de>
2020-03-10 11:39:57 +0100

    More cleanup

--- web/ladelog.php
+++ web/ladelog.php
@@ -429,106 +429,108 @@ fclose($extractf);
 $avgladel = round($avgladel / $count, 3);
 $ladezeit = intval(round($ladezeit / $count, 2));
 ?>
-		<hr>
-		<div class="row">
-			<div class="col-xs-12 text-center">
-				<div class="col-xs-2 text-center" style="font-size: 1vw">
-					Startzeit
-				</div>
-				<div class="col-xs-2 text-center" style="font-size: 1vw">
-					Endzeit
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-					Geladene km gesamt
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-					Geladene kWh gesamt
-				</div>
-				<div class="col-xs-2 text-center" style="font-size: 1vw">
-					Durchschnittliche Ladeleistung kW
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-					Durchschnittliche Ladedauer
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-					Ladepunkt
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-					Lademodus
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-					RFID
+			<hr>
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<div class="col-xs-2 text-center" style="font-size: 1vw">
+						Startzeit
+					</div>
+					<div class="col-xs-2 text-center" style="font-size: 1vw">
+						Endzeit
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+						Geladene km gesamt
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+						Geladene kWh gesamt
+					</div>
+					<div class="col-xs-2 text-center" style="font-size: 1vw">
+						Durchschnittliche Ladeleistung kW
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+						Durchschnittliche Ladedauer
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+						Ladepunkt
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+						Lademodus
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+						RFID
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12 text-center">
-				<div class="col-xs-2 text-center" style="font-size: 1vw">
-				</div>
-				<div class="col-xs-2 text-center" style="font-size: 1vw">
-								</div>
-				<div class="col-xs-1 text-center" style="font-size: 1.5vw">
-						<?php print($sumgelkm); ?>	</div>
-				<div class="col-xs-1 text-center" style="font-size: 1.5vw">
-					<?php print($sumkwh); ?>
-				</div>
-				<div class="col-xs-2 text-center" style="font-size: 1.5vw">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<div class="col-xs-2 text-center" style="font-size: 1vw">
+					</div>
+					<div class="col-xs-2 text-center" style="font-size: 1vw">
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1.5vw">
+						<?php print($sumgelkm); ?>
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1.5vw">
+						<?php print($sumkwh); ?>
+					</div>
+					<div class="col-xs-2 text-center" style="font-size: 1.5vw">
 						<?php print($avgladel); ?>
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1.5vw">
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1.5vw">
 						<?php print($ladezeit); ?> Min
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
-				</div>
-				<div class="col-xs-1 text-center" style="font-size: 1vw">
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+					</div>
+					<div class="col-xs-1 text-center" style="font-size: 1vw">
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="row">
-			<div class="col-xs-1">
+			<div class="row">
+				<div class="col-xs-1">
+				</div>
+				<div class="col-xs-7">
+					<form name="limitlines" id="limitlines" action="ladelog.php" method="GET">
+						<label for="lines">Anzahl berücksichtiger Ladungen</label>
+						<input id="lines" name="lines" type="number" min="0" value="<?php print $limit ?>" required="required" /><br>
+						<label for="zeitakt">Filter aktiv:</label>
+						<input id="zeitakt" name="zeitakt" type="checkbox" <?php if (isset($_GET['zeitakt'])){ if ( $_GET['zeitakt'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="von">Startdatum:</label>
+						<input id="von" name="von" type="date" min="2018-01-01" value="<?php print date("Y-m-d", $wahlstart) ?>" required="required" />
+						<label for="bis">Enddatum:</label>
+						<input id="bis" name="bis" type="date" min="2018-01-01" value="<?php print date("Y-m-d", $wahlstop) ?>" required="required" /><br>
+						<label for="lp1akt">LP1:</label>
+						<input id="lp1akt" name="lp1akt" type="checkbox" <?php if (isset($_GET['lp1akt'])){ if ( $_GET['lp1akt'] == "on"){ echo "checked"; }} ?> >
+						<label for="lp2akt">LP2:</label>
+						<input id="lp2akt" name="lp2akt" type="checkbox" <?php if (isset($_GET['lp2akt'])){ if ( $_GET['lp2akt'] == "on"){ echo "checked"; }} ?> >
+						<label for="lp3akt">LP3:</label>
+						<input id="lp3akt" name="lp3akt" type="checkbox" <?php if (isset($_GET['lp3akt'])){ if ( $_GET['lp3akt'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="sofort">Sofortlademodus:</label>
+						<input id="sofort" name="sofort" type="checkbox" <?php if (isset($_GET['sofort'])){ if ( $_GET['sofort'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="minpv">Min+PV Lademodus:</label>
+						<input id="minpv" name="minpv" type="checkbox" <?php if (isset($_GET['minpv'])){ if ( $_GET['minpv'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="nurpv">NurPV Lademodus:</label>
+						<input id="nurpv" name="nurpv" type="checkbox" <?php if (isset($_GET['nurpv'])){ if ( $_GET['nurpv'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="standby">Standby Lademodus:</label>
+						<input id="standby" name="standby" type="checkbox" <?php if (isset($_GET['standby'])){ if ( $_GET['standby'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="nachtladenlp1">Nachtladen LP1:</label>
+						<input id="nachtladenlp1" name="nachtladenlp1" type="checkbox" <?php if (isset($_GET['nachtladenlp1'])){ if ( $_GET['nachtladenlp1'] == "on"){ echo "checked"; }} ?> ><br>
+						<label for="rfidakt">RFID:</label>
+						<input id="rfidakt" name="rfidakt" type="checkbox" <?php if (isset($_GET['rfidakt'])){ if ( $_GET['rfidakt'] == "on"){ echo "checked"; }} ?> >
+						<label for="rfid">TAG:</label>
+						<input id="rfid" name="rfid" type="text" value="<?php if (isset($_GET['rfid'])){  echo $_GET['rfid']; } ?>" ><br>
+						<button class="btn btn-primary btn-green" type="submit">Go</button>
+					</form>
+				</div>
+				<div class="col-xs-4">
+				</div>
 			</div>
-			<div class="col-xs-7">
-				<form name="limitlines" id="limitlines" action="ladelog.php" method="GET">
-					<label for="lines">Anzahl berücksichtiger Ladungen</label>
-					<input id="lines" name="lines" type="number" min="0" value="<?php print $limit ?>" required="required" /><br>
-					<label for="zeitakt">Filter aktiv:</label>
-					<input id="zeitakt" name="zeitakt" type="checkbox" <?php if (isset($_GET['zeitakt'])){ if ( $_GET['zeitakt'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="von">Startdatum:</label>
-					<input id="von" name="von" type="date" min="2018-01-01" value="<?php print date("Y-m-d", $wahlstart) ?>" required="required" />
-					<label for="bis">Enddatum:</label>
-					<input id="bis" name="bis" type="date" min="2018-01-01" value="<?php print date("Y-m-d", $wahlstop) ?>" required="required" /><br>
-					<label for="lp1akt">LP1:</label>
-					<input id="lp1akt" name="lp1akt" type="checkbox" <?php if (isset($_GET['lp1akt'])){ if ( $_GET['lp1akt'] == "on"){ echo "checked"; }} ?> >
-					<label for="lp2akt">LP2:</label>
-					<input id="lp2akt" name="lp2akt" type="checkbox" <?php if (isset($_GET['lp2akt'])){ if ( $_GET['lp2akt'] == "on"){ echo "checked"; }} ?> >
-					<label for="lp3akt">LP3:</label>
-					<input id="lp3akt" name="lp3akt" type="checkbox" <?php if (isset($_GET['lp3akt'])){ if ( $_GET['lp3akt'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="sofort">Sofortlademodus:</label>
-					<input id="sofort" name="sofort" type="checkbox" <?php if (isset($_GET['sofort'])){ if ( $_GET['sofort'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="minpv">Min+PV Lademodus:</label>
-					<input id="minpv" name="minpv" type="checkbox" <?php if (isset($_GET['minpv'])){ if ( $_GET['minpv'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="nurpv">NurPV Lademodus:</label>
-					<input id="nurpv" name="nurpv" type="checkbox" <?php if (isset($_GET['nurpv'])){ if ( $_GET['nurpv'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="standby">Standby Lademodus:</label>
-					<input id="standby" name="standby" type="checkbox" <?php if (isset($_GET['standby'])){ if ( $_GET['standby'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="nachtladenlp1">Nachtladen LP1:</label>
-					<input id="nachtladenlp1" name="nachtladenlp1" type="checkbox" <?php if (isset($_GET['nachtladenlp1'])){ if ( $_GET['nachtladenlp1'] == "on"){ echo "checked"; }} ?> ><br>
-					<label for="rfidakt">RFID:</label>
-					<input id="rfidakt" name="rfidakt" type="checkbox" <?php if (isset($_GET['rfidakt'])){ if ( $_GET['rfidakt'] == "on"){ echo "checked"; }} ?> >
-					<label for="rfid">TAG:</label>
-					<input id="rfid" name="rfid" type="text" value="<?php if (isset($_GET['rfid'])){  echo $_GET['rfid']; } ?>" ><br>
-					<button class="btn btn-primary btn-green" type="submit">Go</button>
+			<div class="row">
+				<button onclick="window.location.href='./index.php'" class="btn btn-primary btn-blue">Zurück</button>
+				<form method="get" action="../ramdisk/ladelog.csv">
+					<button class="btn btn-primary btn-green">Download csv</button>
 				</form>
 			</div>
-			<div class="col-xs-4">
-			</div>
-		</div>
-		<div class="row">
-			<button onclick="window.location.href='./index.php'" class="btn btn-primary btn-blue">Zurück</button>
-			<form method="get" action="../ramdisk/ladelog.csv">
-				<button class="btn btn-primary btn-green">Download csv</button>
-			</form>
 		</div>
 	</section>
 
