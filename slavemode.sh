2dba399ed12ec94babd95d982149ed77c9dd0902
Kurt <dj3mu@darc.de>
2020-03-22 09:01:15 +0100

    Integration changes

--- slavemode.sh
+++ slavemode.sh
@@ -4,18 +4,41 @@ openwbisslave() {
 	AllowedTotalCurrentPerPhase=$(<ramdisk/AllowedTotalCurrentPerPhase)
 	TotalCurrentConsumptionOnL1=$(<ramdisk/TotalCurrentConsumptionOnL1)
 	ChargingVehiclesOnL1=$(<ramdisk/ChargingVehiclesOnL1)
-	lldiff=$(( (AllowedTotalCurrentPerPhase - TotalCurrentConsumptionOnL1) / ChargingVehiclesOnL1))
-	llneu=$(( llalt - lldiff))
+
+	# if no EV's are charging at all it would be a div/0
+	# but also from logical point of view: we have to assume at least ourself as "charging"
+	if [ "$ChargingVehiclesOnL1" -eq "0" ]; then
+		ChargingVehiclesOnL1=1
+	fi
+
+	# compute difference in floats for not to loose too much precision
+	lldiff=$(echo "scale=3; ($AllowedTotalCurrentPerPhase - $TotalCurrentConsumptionOnL1) / $ChargingVehiclesOnL1" | bc)
+
+	# new charge current in int but always rounded to the next (even for negative value numerically) _lower_ integer
+	if [[ "$lldiff" =~ ^\s*- ]]; then
+		llneu=$(echo "scale=0; ($llalt + $lldiff - 1.0)/1" | bc)
+	else
+		llneu=$(echo "scale=0; ($llalt + $lldiff)/1" | bc)
+	fi
+
+	if (( debug == 2 )); then
+		echo "Slave Mode Aktiv, AllowedTotalCurrentPerPhase=$AllowedTotalCurrentPerPhase, TotalCurrentConsumptionOnL1=$TotalCurrentConsumptionOnL1, ChargingVehiclesOnL1=$ChargingVehiclesOnL1, llalt=$llalt, lldiff=$lldiff, llneu=$llneu"
+	fi
+
 	if (( llneu < minimalstromstaerke )); then
 		llneu=0
 	fi
 	if (( llneu > maximalstromstaerke )); then
 		llneu=$maximalstromstaerke
 	fi
+
 	runs/set-current.sh $llneu all
+
 	if (( llalt != llneu )); then
 		echo "$date Ändere Ladeleistung auf $llneu Ampere" >> ramdisk/ladestatus.log
 	fi
+
 	echo "Slave Mode Aktiv, openWB NUR fernsteuerbar" > ramdisk/lastregelungaktiv
+
 	exit 0
 }
