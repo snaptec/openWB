9971248df94cf7839ece4a6f1be5e3b8f74ea7c4
snaptec <info@snaptec.org>
2020-03-27 09:57:52 +0100

    Bugfix fems speicher

--- modules/speicher_fems/main.sh
+++ modules/speicher_fems/main.sh
@@ -2,9 +2,8 @@
 . /var/www/html/openWB/openwb.conf
 soc=$(curl -s "http://x:user@$femsip:8084/rest/channel/ess0/Soc" | jq .value)
 leistung=$(curl -s "http://x:user@$femsip:8084/rest/channel/ess0/ActivePower" | jq .value)
-leistung=$(( leistung * -1 ))
-speicheriwh=$(curl -s "http://x:user@$femsip:8084/rest/channel/ess0/ActiveChargeEnergy" | jq .value)
-speicherewh=$(curl -s "http://x:user@$femsip:8084/rest/channel/ess0/ActiveDischargeEnergy" | jq .value)
+speicherewh=$(curl -s "http://x:user@$femsip:8084/rest/channel/ess0/ActiveChargeEnergy" | jq .value)
+speicheriwh=$(curl -s "http://x:user@$femsip:8084/rest/channel/ess0/ActiveDischargeEnergy" | jq .value)
 
 
 ra='^[-+]?[0-9]+\.?[0-9]*$'
