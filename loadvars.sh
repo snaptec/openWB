2ab522afc057c822d7b384458a28802322d7a49f
snaptec <info@snaptec.org>
2020-04-02 05:51:43 +0200

    Berechnung PV durch openWB wenn kostal DC Speicher vorhanden

--- loadvars.sh
+++ loadvars.sh
@@ -694,7 +694,7 @@ if [[ $wattbezugmodul == "bezug_e3dc" ]] || [[ $wattbezugmodul == "bezug_kostalp
 	fi
 	# sim bezug end
 fi
-if [[ $pvwattmodul == "none" ]] && [[ $speichermodul == "speicher_e3dc" ]]; then
+if [[ $pvwattmodul == "none" ]] && [[ $speichermodul == "speicher_e3dc" ]] || [[ $speichermodul == "speicher_kostalplenticore" ]] && [[ $pvwattmodul == "wr_plenticore" ]]; then
 	ra='^-?[0-9]+$'
 	watt3=$(</var/www/html/openWB/ramdisk/pvwatt)
 	if [[ -e /var/www/html/openWB/ramdisk/pvwatt0pos ]]; then
--- modules/wr_plenticore/main.sh
+++ modules/wr_plenticore/main.sh
@@ -13,7 +13,11 @@
 #########################################################
 
 # Aufruf der Leseroutine mit den IP WR 1 und ggf. WR 2
-sudo python /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2
+if [[ $speichermodul == "speicher_kostalplenticore" ]]; then
+	sudo python /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2 1
+else
+	sudo python /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2 0
+fi
 
 # Rückgabe des Wertes Gesamt-PV-Leistung
 pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt)
