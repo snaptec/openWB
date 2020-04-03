19527ebfec912bf2374a36a213725a63f512fd07
Kurt <dj3mu@darc.de>
2020-04-03 18:06:04 +0200

    Proper handling of simultaneous unplug of multiple CPs
    Cleanup of string quoting. All strings should be quoted now as some are
    assembled dynamically from variables.

--- rfidtag.sh
+++ rfidtag.sh
@@ -17,7 +17,7 @@ rfid() {
 
 	NowItIs=$(date +%s)
 
-	lasttag=$(<ramdisk/readtag)
+	lasttag=$(<"ramdisk/readtag")
 
 	if [[ $lasttag != "0" ]]; then
 		if [ $lasttag == $rfidlp1c1 ] || [ $lasttag == $rfidlp1c2 ]  || [ $lasttag == $rfidlp1c3 ] ; then
@@ -57,8 +57,8 @@ rfid() {
 			checkTagValidAndSetStartScanData $currentCp
 		done
 
-		echo $lasttag > ramdisk/rfidlasttag
-		echo 0 > ramdisk/readtag
+		echo $lasttag > "ramdisk/rfidlasttag"
+		echo 0 > "ramdisk/readtag"
 	fi
 
 
@@ -67,59 +67,59 @@ rfid() {
 	#
 	if (( slavemode == 1 )); then
 
-	setLpPlugChangeState
+		setLpPlugChangeState
 
-	# handle plugin only if we have valid un-assigned start data (i.e. an RFID-scan that has not yet been assigned to a CP)
-	if [ -f ramdisk/startRfidScanData ]; then
+		# handle plugin only if we have valid un-assigned start data (i.e. an RFID-scan that has not yet been assigned to a CP)
+		if [ -f "ramdisk/startRfidScanData" ]; then
 
-		# extract fragments of start data
-		startData=$(<ramdisk/startRfidScanData)
-		IFS=',' read -r -a startDataSegments <<< "$startData"
+			# extract fragments of start data
+			startData=$(<"ramdisk/startRfidScanData")
+			IFS=',' read -r -a startDataSegments <<< "$startData"
 
-		if (( pluggedLp > 0 )); then
+			if (( pluggedLp > 0 )); then
 
-			startDataForPluggedLp="${startData}"
-			echo "$NowItIs: Charge point #${pluggedLp} has been plugged in - recording accounting start data"
-			echo "${startDataForPluggedLp}" > "ramdisk/startRfidScanDataLp${pluggedLp}"
-			rm ramdisk/startRfidScanData
-		else
-			secondsSinceRfidScan=$(( NowItIs - startDataSegments[0] ))
+				startDataForPluggedLp="${startData}"
+				echo "$NowItIs: Charge point #${pluggedLp} has been plugged in - recording accounting start data"
+				echo "${startDataForPluggedLp}" > "ramdisk/startRfidScanDataLp${pluggedLp}"
+				rm -f "ramdisk/startRfidScanData"
+			else
+				secondsSinceRfidScan=$(( NowItIs - startDataSegments[0] ))
 
-			$dbgWrite "$NowItIs: Not yet plugged in any CP ${secondsSinceRfidScan} seconds after RFID scan. Further waiting for plugin for at most ${MaximumSecondsAfterRfidScanToAssignCp} seconds"
+				$dbgWrite "$NowItIs: Not yet plugged in any CP ${secondsSinceRfidScan} seconds after RFID scan. Further waiting for plugin for at most ${MaximumSecondsAfterRfidScanToAssignCp} seconds"
 
-			# check for timeout of start data
-			if (( secondsSinceRfidScan > MaximumSecondsAfterRfidScanToAssignCp )); then
-				echo "$NowItIs: Timeout (${secondsSinceRfidScan} > ${MaximumSecondsAfterRfidScanToAssignCp} seconds) waiting for plugin after RFID scan. Disabling the CP and deleting accounting data."
+				# check for timeout of start data
+				if (( secondsSinceRfidScan > MaximumSecondsAfterRfidScanToAssignCp )); then
+					echo "$NowItIs: Timeout (${secondsSinceRfidScan} > ${MaximumSecondsAfterRfidScanToAssignCp} seconds) waiting for plugin after RFID scan. Disabling the CP and deleting accounting data."
 
-				for ((currentCp=1; currentCp<=NumberOfSupportedChargePoints; currentCp++)); do
-					if [[ "${pluggedLps[$currentCp]}" -eq "0" ]]; then
-						echo "$NowItIs: Disabling CP #${currentCp} as it's unplugged and delay after RFID tag scan has been exceeded"
-						mosquitto_pub -r -q 2 -t "openWB/set/lp${currentCp}/ChargePointEnabled" -m "0"
-					fi
-				done
+					# in case of timeout we disable all CPs that are NOT plugged in right now
+					for ((currentCp=1; currentCp<=NumberOfSupportedChargePoints; currentCp++)); do
+						if [[ "${lpsPlugStat[$currentCp]}" -eq "0" ]]; then
+							echo "$NowItIs: Disabling CP #${currentCp} as it's still unplugged after timeout of RFID tag scan has been exceeded"
+							mosquitto_pub -r -q 2 -t "openWB/set/lp${currentCp}/ChargePointEnabled" -m "0"
+						fi
+					done
 
-				rm ramdisk/startRfidScanData
+					rm -f "ramdisk/startRfidScanData"
+				fi
 			fi
 		fi
+
+		# handle un-plug
+		for ((currentCp=1; currentCp<=NumberOfSupportedChargePoints; currentCp++)); do
+			if (( unpluggedLps[$currentCp] > 0 )); then
+				echo "$NowItIs: Charge point #${currentCp} has been UNplugged - stop sending accounting data after one final transmission"
+
+				# one final transmission of accounting data ...
+				sendAccounting $currentCp
+
+				# ... before we disabled it by removing the start info
+				rm -f "ramdisk/startRfidScanDataLp${currentCp}"
+			fi
+
+			# finally actually transmit the accounting data
+			sendAccounting $currentCp
+		done
 	fi
-
-	# handle un-plug
-	if (( unpluggedLp > 0 )); then
-		echo "$NowItIs: Charge point #${unpluggedLp} has been UNplugged - stop sending accounting data after one final transmission"
-
-		# one final transmission of accounting data ...
-		sendAccounting $unpluggedLp
-
-		# ... before we disabled it
-		rm "ramdisk/startRfidScanDataLp${unpluggedLp}"
-	fi
-
-	fi
-
-	# finally actually transmit the accounting data
-	for ((currentCp=1; currentCp<=NumberOfSupportedChargePoints; currentCp++)); do
-		sendAccounting $currentCp
-	done
 }
 
 
@@ -141,8 +141,8 @@ sendAccounting() {
 # if it has NOT, pluggedLp will be set to 0
 setLpPlugChangeState() {
 
-	oplugstat=$(<ramdisk/accPlugstatChangeDetectLp1)
-	oplugstats1=$(<ramdisk/accPlugstatChangeDetectLp2)
+	oplugstat=$(<"ramdisk/accPlugstatChangeDetectLp1")
+	oplugstats1=$(<"ramdisk/accPlugstatChangeDetectLp2")
 
 	if [ -f "ramdisk/mockedPlugstat" ]; then
 		plugstat=$(<"ramdisk/mockedPlugstat")
@@ -153,27 +153,30 @@ setLpPlugChangeState() {
 	fi
 
 	pluggedLp=0
-	pluggedLp=0
-	pluggedLps=($plugstat $plugstats1)
+	lpsPlugStat=($plugstat $plugstats1)
+	unpluggedLps=(0 0 0)
 
-	if (( plugstat == 1 )) && (( oplugstat == 0 )); then
-		echo "$NowItIs: LP 1 plugged in"
-		pluggedLp=1
-	elif (( plugstat == 0 )) && (( oplugstat == 1 )); then
-		echo "$NowItIs: LP 1 un-plugged"
-		unpluggedLp=1
-	fi
-
+	# first check LP2 as the last one will win for plugin and it seems more logical to let LP1 win
 	if (( plugstats1 == 1 )) && (( oplugstats1 == 0 )); then
 		echo "$NowItIs: LP 2 plugged in"
 		pluggedLp=2
 	elif (( plugstats1 == 0 )) && (( oplugstats1 == 1 )); then
 		echo "$NowItIs: LP 2 un-plugged"
-		unpluggedLp=2
+		unpluggedLps[2]=1
 	fi
 
-	echo $plugstat > ramdisk/accPlugstatChangeDetectLp1
-	echo $plugstats1 > ramdisk/accPlugstatChangeDetectLp2
+	echo $plugstats1 > "ramdisk/accPlugstatChangeDetectLp2"
+
+	# finally check LP1 so it wins
+	if (( plugstat == 1 )) && (( oplugstat == 0 )); then
+		echo "$NowItIs: LP 1 plugged in"
+		pluggedLp=1
+	elif (( plugstat == 0 )) && (( oplugstat == 1 )); then
+		echo "$NowItIs: LP 1 un-plugged"
+		unpluggedLps[1]=1
+	fi
+
+	echo $plugstat > "ramdisk/accPlugstatChangeDetectLp1"
 }
 
 
