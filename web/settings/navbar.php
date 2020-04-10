65af3bae4c125af9039be3e6b7b207681fac96f5
Remo Gloor <remo.gloor@gmail.com>
2020-04-11 00:01:36 +0200

    Closed vurnability that passwords can be accessed through triggering backup.

--- web/settings/navbar.php
+++ web/settings/navbar.php
@@ -82,7 +82,7 @@
 			</div>
 			<!-- modal footer -->
 			<div class="modal-footer d-flex justify-content-center">
-				<button type="button" class="btn btn-green" data-dismiss="modal" onclick="window.location.href='./tools/bckredirect.html'">Backup</button>
+				<button type="button" class="btn btn-green" data-dismiss="modal" onclick="window.location.href='./settings/bckredirect.html'">Backup</button>
 				<button type="button" class="btn btn-red" data-dismiss="modal">Abbruch</button>
 			</div>
 		</div>
