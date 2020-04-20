0b6c1305d1c15a6065095678690108c576f729a5
snaptec <info@snaptec.org>
2020-04-20 17:22:53 +0200

    Api Anpassung Discovergy PV modul

--- modules/wr_discovergy/main.sh
+++ modules/wr_discovergy/main.sh
@@ -2,7 +2,7 @@
 . /var/www/html/openWB/openwb.conf
 output=$(curl --connect-timeout 5 -s -u $discovergyuser:$discovergypass "https://api.discovergy.com/public/v1/last_reading?meterId=$discovergypvid")
 
-pvwh=$(echo $output | jq .values.energy)
+pvwh=$(echo $output | jq .values.energyOut)
 pvwh=$(( bezugwh / 10000000 ))
 echo $bezugwh > /var/www/html/openWB/ramdisk/pvkwh
 
