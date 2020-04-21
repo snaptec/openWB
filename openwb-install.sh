6937936f2dcad4dcf8da31e9001612e97311dbee
premultiply <ulrich.peters@posteo.de>
2020-04-21 16:20:06 +0200

    PHP 7.3
    Raspbian Buster benutzt nun PHP 7.3
    Fixes #478

--- openwb-install.sh
+++ openwb-install.sh
@@ -37,11 +37,11 @@ if ! [ -x "$(command -v apachectl)" ]; then
 	sleep 1
 	apt-get -qq install -y php-gd
 	sleep 1
-	apt-get -qq install -y php7.0-xml
+	apt-get -qq install -y php7.3-xml
 	sleep 2
         apt-get -qq install -y php-curl
 	sleep 1	
-	apt-get -qq install -y libapache2-mod-php7.0
+	apt-get -qq install -y libapache2-mod-php7.3
 	sleep 2
 	apt-get -qq install -y jq
 	sleep 2
@@ -145,7 +145,7 @@ then
 else
 	echo "EXTRA_OPTS="-L 0"" >> /etc/default/cron
 fi
-sudo /bin/su -c "echo 'upload_max_filesize = 30M' > /etc/php/7.0/apache2/conf.d/20-uploadlimit.ini"
+sudo /bin/su -c "echo 'upload_max_filesize = 30M' > /etc/php/7.3/apache2/conf.d/20-uploadlimit.ini"
 sudo apt-get -qq install -y python-pip
 sudo pip install  -U pymodbus
 echo "www-data ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd
