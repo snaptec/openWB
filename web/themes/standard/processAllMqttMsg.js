b1291310a502616059a4329678754ca285424c18
tpd-opitz <14850347+tpd-opitz@users.noreply.github.com>
2020-04-30 18:25:10 +0200

    Phasenanzeige für das Standard-Theme

--- web/themes/standard/processAllMqttMsg.js
+++ web/themes/standard/processAllMqttMsg.js
@@ -995,13 +995,25 @@ function processLpMessages(mqttmsg, mqttpayload, mqtttopic, htmldiv) {
 		//console.log('mqttmsg-W: '+index+'   load='+mqttpayload);
 		$("#restzeitlp"+index+"div").html(mqttpayload);
 	}
+        else if ( mqttmsg.match( /^openwb\/lp\/[1-9][0-9]*\/countphasesinuse/i ) ) {
+                 var index = mqttmsg.match(/\d/g)[0];  // extract first match = number from mqttmsg
+                 var parent = $('.chargePointInfoLp[lp="' + index + '"]');  // get parent row element for charge point
+                 var element = $(parent).find('.phasesInUse');  // now get parents respective child element
+                 var phasesInUse = parseInt(mqttpayload, 10);
+                 if ( isNaN(phasesInUse) || phasesInUse < 1 || phasesInUse > 3 ) {
+                         $(element).text('');
+                 } else {
+                         var phaseSymbols = ['&#x2460','&#x2461','&#x2462'];
+                         $(element).text(" / " + phaseSymbols[ phasesInUse ] );
+                 }
+         }
 	else if ( mqttmsg.match( /^openwb\/lp\/[1-9][0-9]*\/aconfigured$/i ) ) {
 		// target current value at charge point
 		// matches to all messages containing "openwb/lp/#/aconfigured"
 		// where # is an integer > 0
 		// search is case insensitive
 		var index = mqttmsg.match(/\d/g)[0];  // extract first match = number from mqttmsg
-		var targetCurrent = " / " + parseInt(mqttpayload, 10) + " A";
+		var targetCurrent = " * " + parseInt(mqttpayload, 10) + " A";
 		$("#targetCurrentLp"+index+"div").html(targetCurrent);
 	}
 	else if ( mqttmsg.match( /^openwb\/lp\/[1-9][0-9]*\/boolplugstat$/i ) ) {
