070ff8cd30f0df20c7993cd42f796d7e5613e180
Michael <michael@ortenstein.de>
2020-05-02 16:16:10 +0200

    Sofortladen-Settings und Fix PV-Laden

--- web/settings/topicsToSubscribe_sofortconfig.js
+++ web/settings/topicsToSubscribe_sofortconfig.js
@@ -9,5 +9,14 @@
 // line[1] = load counter (if needed)
 
 var topicsToSubscribe = [
+	["openWB/config/get/global/minEVSECurrentAllowed", 0],
 	
+	["openWB/config/get/sofort/lp/1/current", 0],
+	["openWB/config/get/sofort/lp/2/current", 0],
+	["openWB/config/get/sofort/lp/3/current", 0],
+	["openWB/config/get/sofort/lp/4/current", 0],
+	["openWB/config/get/sofort/lp/5/current", 0],
+	["openWB/config/get/sofort/lp/6/current", 0],
+	["openWB/config/get/sofort/lp/7/current", 0],
+	["openWB/config/get/sofort/lp/8/current", 0]
 ];
