b3a434bd2948007b2597e0771714e2ab4d35cec8
snaptec <info@snaptec.org>
2020-05-06 13:46:51 +0200

    typo

--- runs/mqttsub.py
+++ runs/mqttsub.py
@@ -411,7 +411,7 @@ def on_message(client, userdata, msg):
             sendcommand = ["/var/www/html/openWB/runs/replaceinconfig.sh", "nurpv70dynact=", msg.payload.decode("utf-8")]
             subprocess.Popen(sendcommand)
             client.publish("openWB/config/get/pv/nurpv70dynact", msg.payload.decode("utf-8"), qos=0, retain=True)
-            client.publish("openWB/config/set/pv/nurpv70dynact", msg.payload.decode("utf-8"), qos=0, retain=True)
+            client.publish("openWB/config/set/pv/nurpv70dynact", "", qos=0, retain=True)
     if (msg.topic == "openWB/config/set/pv/nurpv70dynw"):
         if (int(msg.payload) >= 2000 and int(msg.payload) <= 50000):
             sendcommand = ["/var/www/html/openWB/runs/replaceinconfig.sh", "nurpv70dynw=", msg.payload.decode("utf-8")]
