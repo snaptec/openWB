0e7481b3f40a7bc7afe5dbb8287f18d1125b9dc7
snaptec <info@snaptec.org>
2020-05-12 16:14:42 +0200

    Max Soc Handling geändert

--- nurpv.sh
+++ nurpv.sh
@@ -23,20 +23,23 @@ if [[ $schieflastaktiv == "1" ]]; then
 	fi
 fi
 if (( stopchargeafterdisclp1 == 0 )); then
-	if (( soc > stopchargepvpercentagelp1 )); then
-		if [[ $lp1enabled == "1" ]]; then
-			mosquitto_pub -r -t "openWB/set/lp/1/ChargePointEnabled" -m "0"
-			echo "$date LP1, Lademodus NurPV. Schalte Ladepunkt auf gesperrt da $soc % SoC erreicht, Ziel $stopchargepvpercentagelp1 %" >> ramdisk/ladestatus.log
-		fi
-	else
-		if [[ $lp1enabled == "0" ]]; then
-			mosquitto_pub -r -t "openWB/set/lp/1/ChargePointEnabled" -m "1"
-			echo "$date LP1, Lademodus NurPV. Schalte Ladepunkt frei da $soc % SoC noch nicht erreicht, Ziel $stopchargepvpercentagelp1 %" >> ramdisk/ladestatus.log
+	if [[ $stopchargepvatpercentlp1 == "1" ]]; then
+		if (( soc > stopchargepvpercentagelp1 )); then
+			if [[ $lp1enabled == "1" ]]; then
+				mosquitto_pub -r -t "openWB/set/lp/1/ChargePointEnabled" -m "0"
+				echo "$date LP1, Lademodus NurPV. Schalte Ladepunkt auf gesperrt da $soc % SoC erreicht, Ziel $stopchargepvpercentagelp1 %" >> ramdisk/ladestatus.log
+			fi
+		else
+			if [[ $lp1enabled == "0" ]]; then
+				mosquitto_pub -r -t "openWB/set/lp/1/ChargePointEnabled" -m "1"
+				echo "$date LP1, Lademodus NurPV. Schalte Ladepunkt frei da $soc % SoC noch nicht erreicht, Ziel $stopchargepvpercentagelp1 %" >> ramdisk/ladestatus.log
 
+			fi
 		fi
 	fi
 fi
 if (( stopchargeafterdisclp2 == 0 )); then
+	if [[ $stopchargepvatpercentlp2 == "1" ]]; then
 		if (( soc1 > stopchargepvpercentagelp2 )); then
 			if [[ $lp2enabled == "1" ]]; then
 				mosquitto_pub -r -t "openWB/set/lp/2/ChargePointEnabled" -m "0"
@@ -50,6 +53,7 @@ if (( stopchargeafterdisclp2 == 0 )); then
 
 			fi
 		fi
+	fi
 fi
 
 if [[ $lastmanagement == "0" ]]; then
