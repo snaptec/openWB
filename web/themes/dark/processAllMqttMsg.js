0b9c4611faba5071a18fc4319e3c43bfa2b32147
snaptec <info@snaptec.org>
2020-05-15 12:30:08 +0200

    Bugfix Ui

--- web/themes/dark/processAllMqttMsg.js
+++ web/themes/dark/processAllMqttMsg.js
@@ -224,6 +224,7 @@ function processGraphMessages(mqttmsg, mqttpayload) {
 		if (initialread == 0) {
 			window['all'+index+'p'] = mqttpayload;
 			window['all'+index] = 1;
+			console.log(mqttpayload);
 			putgraphtogether();
 		}
 	}
