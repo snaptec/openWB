e2cdc154b086d35796a31754e4e04d63f9159e2a
snaptec <info@snaptec.org>
2020-05-30 08:28:58 +0200

    Bugfix Maximalstromstärke Sofort Laden bei einem Ladepunkt

--- sofortlademodus.sh
+++ sofortlademodus.sh
@@ -127,7 +127,7 @@ if [[ $lastmanagement == "0" ]]; then
 						exit 0
 					fi
 					if (( llalt > maximalstromstaerke )); then
-						llneu=$((llalt - 1 ))
+						llneu=$maximalstromstaerke
 						runs/set-current.sh "$llneu" m
 						echo "$date LP1, Lademodus Sofort. Ladung geändert auf $llneu Ampere" >> ramdisk/ladestatus.log
 						if [[ $debug == "1" ]]; then
@@ -167,11 +167,15 @@ if [[ $lastmanagement == "0" ]]; then
 						if (( llneu > sofortll )); then
 							llneu=$sofortll
 						fi
+						if (( llneu > maximalstromstaerke )); then
+							llneu=$maximalstromstaerke
+						fi
+
 						if (( llneu < sofortll )); then
 							echo "Lastmanagement aktiv, Ladeleistung reduziert" > ramdisk/lastregelungaktiv
 						fi 
 						if (( llalt > maximalstromstaerke )); then
-							llneu=$((llalt - 1 ))
+							llneu=$maximalstromstaerke
 							runs/set-current.sh "$llneu" m
 							echo "$date LP1, Lademodus Sofort. Ladung geändert auf $llneu Ampere" >> ramdisk/ladestatus.log
 							if [[ $debug == "1" ]]; then
@@ -189,7 +193,7 @@ if [[ $lastmanagement == "0" ]]; then
 					if (( llalt > sofortll)); then
 						llneu=$sofortll
 						if (( llalt > maximalstromstaerke )); then
-							llneu=$((llalt - 1 ))
+							llneu=$maximalstromstaerke
 							runs/set-current.sh "$llneu" m
 							echo "$date LP1, Lademodus Sofort. Ladung geändert auf $llneu Ampere" >> ramdisk/ladestatus.log
 							if [[ $debug == "1" ]]; then
