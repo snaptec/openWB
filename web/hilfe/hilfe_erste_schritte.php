439d73ab866df4899e1424edd4beb8caf862b17a
snaptec <info@snaptec.org>
2020-06-05 10:22:21 +0200

    Text angepasst

--- web/hilfe/hilfe_erste_schritte.php
+++ web/hilfe/hilfe_erste_schritte.php
@@ -3,7 +3,9 @@
 <h4> Lademodus</h4>
 <p>
 	Der Unterschied zwischen "Standby" und "Stop" ist, dass bei "Stop" keine Ladung erlaubt ist.<br>
-	Im Standby  Modus kann die Ladung durch Nachtladen und Zielladen aktiviert werden.
+	Im Standby  Modus kann die Ladung durch Nachtladen und Zielladen aktiviert werden.<br>
+	Im Lademodus Min + PV wird immer mit der eingestellten Minimalstromstärke geladen und bei vorhandenem Überschuss hochgeregelt<br>
+	Der Lademodus PV startet die Ladung erst wenn genügend PV Überschuss vorhanden ist. Lässt dieser nach wird die Ladung auch wieder gestoppt
 </p>
 <hr>
 <h4>Ladeeinstellung</h4>
@@ -12,6 +14,12 @@
 	Wurde innerhalb der letzten 2 Minuten das Lastmanagement aktiv, wird diese Meldung auf der Hauptseite unterhalb des Graphen angezeigt.<br>
 	Sie dient rein der Information, dass das Lastmanagement von openWB aktiv ist und die gewünschte bzw. eingestellte Ladeleistung derzeit nicht möglich ist.<br>
 	Es besteht kein Handlungsbedarf. Wenn mehr Leistung zur Verfügung steht, regelt openWB automatisch auf den gewünschten Wert hoch.
+</p>
+<hr>
+<h4>Ladepunktsteuerung</h4>
+<p>
+	Ein grüner Ladepunkt zeigt an das dieser grundsätzlich freigeschaltet ist. Je nach Lademodus ist eine Ladung möglich oder auch nicht<br>
+	Ein roter durchgestrichener Ladepunkt zeigt einen deaktivierten Ladepunkt an. Durch anklicken ist der Ladepunkt manuell (de-)aktivierbar
 </p>
 <hr>
 <!-- <<< Erste Schritte -->
