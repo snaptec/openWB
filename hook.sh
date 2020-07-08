4c59f11ee3218b887424b58b53e3db51faf5d19d
manuel <info@rc-portal.com>
2020-07-08 19:08:19 +0200

    removed sudo

--- hook.sh
+++ hook.sh
@@ -195,7 +195,7 @@ if (( verbraucher1_aktiv == "1")); then
 		fi
 	fi
 	if [[ $verbraucher1_typ == "abb-b23" ]]; then
-			sudo python modules/verbraucher/abb-23remote.py 1 $verbraucher1_source $verbraucher1_id &
+			python modules/verbraucher/abb-23remote.py 1 $verbraucher1_source $verbraucher1_id &
 			verbraucher1_watt=$(cat /var/www/html/openWB/ramdisk/verbraucher1_watt)
 		fi
 	fi
@@ -253,7 +253,7 @@ if (( verbraucher2_aktiv == "1")); then
 		fi
 	fi
 	if [[ $verbraucher2_typ == "abb-b23" ]]; then
-			sudo python modules/verbraucher/abb-23remote.py 2 $verbraucher2_source $verbraucher2_id &
+			python modules/verbraucher/abb-23remote.py 2 $verbraucher2_source $verbraucher2_id &
 			verbraucher2_watt=$(cat /var/www/html/openWB/ramdisk/verbraucher2_watt)
 		fi
 	fi
