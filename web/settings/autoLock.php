01c3692f266ac2604943eed8530e6d128f26b9dd
Lutz Bender <github@lutz-bender.de>
2020-07-31 12:03:44 +0200

    enable theme support on recent settings pages

--- web/settings/autoLock.php
+++ web/settings/autoLock.php
@@ -45,6 +45,28 @@
 		<script>
 			var oldClockpickerTime;  // holds old value of clockpicker during changing the time
 		</script>
+		<script>
+			function getCookie(cname) {
+				var name = cname + '=';
+				var decodedCookie = decodeURIComponent(document.cookie);
+				var ca = decodedCookie.split(';');
+				for(var i = 0; i <ca.length; i++) {
+					var c = ca[i];
+					while (c.charAt(0) == ' ') {
+						c = c.substring(1);
+					}
+					if (c.indexOf(name) == 0) {
+						return c.substring(name.length, c.length);
+					}
+				}
+				return '';
+			}
+			var themeCookie = getCookie('openWBTheme');
+			// include special Theme style
+			if( '' != themeCookie ){
+				$('head').append('<link rel="stylesheet" href="themes/' + themeCookie + '/settings.css?v=20200801">');
+			}
+		</script>
 	</head>
 
 	<body>
