ef4f3838361499aaab8ba0259c29ac881b69bec9
snaptec <info@snaptec.org>
2020-07-31 14:22:35 +0200

    Bugfix Typo

--- modules/wr2_ethlovatoaevu/main.sh
+++ modules/wr2_ethlovatoaevu/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 	sudo python /var/www/html/openWB/modules/wr2_ethlovatoaevu/readlovato.py 
-pvwatt2=$(</var/www/html/openWB/ramdisk/pvwatt2)
+pvwatt2=$(</var/www/html/openWB/ramdisk/pv2watt)
 echo $pvwatt2
 
 
