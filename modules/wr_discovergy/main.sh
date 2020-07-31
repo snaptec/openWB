cc15d948670b27d8471170eff8401762c520fea1
snaptec <info@snaptec.org>
2020-07-31 15:48:41 +0200

    Bugfix Typo

--- modules/wr_discovergy/main.sh
+++ modules/wr_discovergy/main.sh
@@ -2,7 +2,7 @@
 output=$(curl --connect-timeout 5 -s -u $discovergyuser:$discovergypass "https://api.discovergy.com/public/v1/last_reading?meterId=$discovergypvid")
 
 pvwh=$(echo $output | jq .values.energyOut)
-pvwh=$(( bezugwh / 10000000 ))
+pvwh=$(( pvwh / 10000000 ))
 echo $pvwh > /var/www/html/openWB/ramdisk/pvkwh
 
 watt=$(echo $output | jq .values.power)
