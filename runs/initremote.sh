5fb71df0593a1ff514f5cadc3677b027d4b90e76
snaptec <info@snaptec.org>
2020-08-10 06:11:26 +0200

    Remotesupport enhanced

--- runs/initremote.sh
+++ runs/initremote.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 token=$(</var/www/html/openWB/ramdisk/remotetoken)
-IFS=';' read -r token port <<< "$token"
-/var/www/html/openWB/runs/startremotesupport.sh $token $port &>/dev/null & disown;
+IFS=';' read -r token port user <<< "$token"
+if [ "${#user}" -gt 5 ]; then
+	user=$user
+else
+	user=getsupport
+fi
+
+/var/www/html/openWB/runs/startremotesupport.sh $token $port $user &>/dev/null & disown;
 
