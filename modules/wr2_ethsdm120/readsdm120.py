21a83e4625b75f004cded1a7de42847b78a96d78
snaptec <info@snaptec.org>
2020-09-18 10:40:04 +0200

    bugfix sdm120 pv2 modul

--- modules/wr2_ethsdm120/readsdm120.py
+++ modules/wr2_ethsdm120/readsdm120.py
@@ -17,6 +17,8 @@ sdmid = int(sys.argv[2])
 resp = client.read_input_registers(0x000C,2, unit=sdmid)
 watt = struct.unpack('>f',struct.pack('>HH',*resp.registers))
 watt = int(watt[0])
+if watt > 0:
+        watt=watt*-1
 f = open("/var/www/html/openWB/ramdisk/pv2watt", 'w')
 f.write(str(watt))
 f.close()
@@ -25,16 +27,18 @@ f.close()
 resp = client.read_input_registers(0x004a,2, unit=sdmid)
 vwh = struct.unpack('>f',struct.pack('>HH',*resp.registers))
 vwh1 = float("%.3f" % vwh[0])
-vwhk = str(vwh1)
-f = open("/var/www/html/openWB/ramdisk/pv2kwhk", 'w')
-f.write(str(vwhk))
-f.close()
+resp = client.read_input_registers(0x0048,2, unit=sdmid)
+v1wh = struct.unpack('>f',struct.pack('>HH',*resp.registers))
+v1wh1 = float("%.3f" % v1wh[0])
 
-vwh2 = float(vwh1) * int(1000)
-vwh3 = str(vwh2)
-f = open("/var/www/html/openWB/ramdisk/pv2kwh", 'w')
-f.write(str(vwh3))
-f.close()
-
+if vwh1 > v1wh1:
+    finalwh=vwh1
+else:
+    finalwh=v1wh1
+    vwh2 = float(finalwh) * int(1000)
+    vwh3 = str(vwh2)
+    f = open("/var/www/html/openWB/ramdisk/pv2kwh", 'w')
+    f.write(str(vwh3))
+    f.close()
 
 
