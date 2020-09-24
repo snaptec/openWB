084d623924337430b6546d8061f081e493085f6b
snaptec <info@snaptec.org>
2020-09-24 21:22:32 +0200

    Kostal Piko AC Werte wenn ohne speicher

--- modules/wr_kostalpiko/main.sh
+++ modules/wr_kostalpiko/main.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #Auslesen eines Kostal Piko WR über die integrierte API des WR. Rückgabewert ist die aktuelle Wattleistung.
+if [[ "$speichermodul" != "none" ]]; then
+	pvwatttmp=$(curl --connect-timeout 5 -s $wrkostalpikoip/api/dxs.json?dxsEntries=33556736'&'dxsEntries=251658753)
+else
+	pvwatttmp=$(curl --connect-timeout 5 -s $wrkostalpikoip/api/dxs.json?dxsEntries=67109120'&'dxsEntries=251658753)
+fi
 
-pvwatttmp=$(curl --connect-timeout 5 -s $wrkostalpikoip/api/dxs.json?dxsEntries=33556736'&'dxsEntries=251658753)
 
 #aktuelle Ausgangsleistung am WR [W]
 pvwatt=$(echo $pvwatttmp | jq '.dxsEntries[0].value' | sed 's/\..*$//')
