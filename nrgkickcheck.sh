04b51f4c99a886fb5b362745e450a65d77fc4294
snaptec <info@snaptec.org>
2020-10-29 10:48:25 +0100

    Bugfix NRGKick

--- nrgkickcheck.sh
+++ nrgkickcheck.sh
@@ -25,4 +25,27 @@ if [[ $evsecon == "nrgkick" ]]; then
 		fi
 	fi
 fi
+if [[ $evsecons1 == "nrgkick" ]]; then
+        output=$(curl --connect-timeout 3 -s http://$nrgkickiplp2/api/settings/$nrgkickmaclp2)
+        current=$(</var/www/html/openWB/ramdisk/llsolls1)
+        if [[ $? == "0" ]] ; then
+                state=$(echo $output | jq -r '.Values.ChargingStatus.Charging')
+                if grep -q 1 "/var/www/html/openWB/ramdisk/ladestatuss1"; then
+                        if [[ $state == "false" ]] ; then
+                                curl --connect-timeout 2 -s -X PUT -H "Content-Type: application/json" --data "{ "Values": {"ChargingStatus": { "Charging": true }, "ChargingCurrent": { "Value": $current }, "DeviceMetadata":{"Password": $nrgkickpwlp2}}}" $nrgkickiplp2/api/settings/$nrgkickmaclp2 > /dev/null
+                        fi
+                fi
+                if grep -q 0 "/var/www/html/openWB/ramdisk/ladestatus"; then
+                        if [[ $state == "true" ]] ; then
+                        curl --connect-timeout 2 -s -X PUT -H "Content-Type: application/json" --data "{ "Values": {"ChargingStatus": { "Charging": false }, "ChargingCurrent": { "Value": "6"}, "DeviceMetadata":{"Password": $nrgkickpwlp2}}}" $nrgkickiplp2/api/settings/$nrgkickmaclp2 > /dev/null
+                        fi
+                fi
+                oldcurrent=$(echo $output | jq -r '.Values.ChargingCurrent.Value')
+                current=$(</var/www/html/openWB/ramdisk/llsolls1)
+                if (( oldcurrent != $current )) ; then
+                        curl --silent --connect-timeout $nrgkicktimeoutlp2 -s -X PUT -H "Content-Type: application/json" --data "{ "Values": {"ChargingStatus": { "Charging": true }, "ChargingCurrent": { "Value": $current}, "DeviceMetadata":{"Password": $nrgkickpwlp2}}}" $nrgkickiplp2/api/settings/$nrgkickmaclp2 > /dev/null
+ > /dev/null
+                fi
+        fi
+fi
 }
