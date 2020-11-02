2898aa9755769ce329197e880fc9c27766dc4d1c
snaptec <info@snaptec.org>
2020-11-02 15:31:30 +0100

    Solaredge Auslesung optimiert

--- modules/wr_solaredge/main.sh
+++ modules/wr_solaredge/main.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
+#!/bin/bash
+
 Solaredgebatwr="0"
 if [[ $solaredgespeicherip == $solaredgepvip ]]  ; then
-	 Solaredgebatwr="1"  
+	Solaredgebatwr="1"  
 fi
-if [[ $solaredgepvslave3 != "none" ]] && [[ $solaredgepvslave4 != "none" ]]&& [[ $solaredgepvslave2 != "none" ]]; then
-	sudo python /var/www/html/openWB/modules/wr_solaredge/solaredge4.py $solaredgepvip $solaredgepvslave1 $solaredgepvslave2 $solaredgepvslave3 $Solaredgebatwr $solaredgepvslave4 $wr1extprod
+if [[ $solaredgewr2ip != "none" ]]; then
+	python /var/www/html/openWB/modules/wr_solaredge/solaredge2wr.py $solaredgepvip $solaredgepvslave1 $Solaredgebatwr $solaredgewr2ip $wr1extprod
 else
-	if [[ $solaredgepvslave3 != "none" ]] && [[ $solaredgepvslave2 != "none" ]]; then
-		sudo python /var/www/html/openWB/modules/wr_solaredge/solaredge3.py $solaredgepvip $solaredgepvslave1 $solaredgepvslave2 $solaredgepvslave3 $Solaredgebatwr $wr1extprod
-	else
-		if (( $solaredgepvslave2 != "none" )); then
-			sudo python /var/www/html/openWB/modules/wr_solaredge/solaredge2.py $solaredgepvip $solaredgepvslave1 $solaredgepvslave2 $Solaredgebatwr $wr1extprod
-		else
-			if [[ $solaredgewr2ip != "none" ]]; then
-				sudo python /var/www/html/openWB/modules/wr_solaredge/solaredge2wr.py $solaredgepvip $solaredgepvslave1 $Solaredgebatwr $solaredgewr2ip	$wr1extprod
-			else
-				sudo python /var/www/html/openWB/modules/wr_solaredge/solaredge.py $solaredgepvip $solaredgepvslave1 $Solaredgebatwr $wr1extprod
-			fi
-		fi
-	fi
+	python /var/www/html/openWB/modules/wr_solaredge/solaredgeall.py $solaredgepvip $solaredgepvslave1 $solaredgepvslave2 $solaredgepvslave3 $solaredgepvslave4 $Solaredgebatwr $wr1extprod
 fi
 
 pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt)
