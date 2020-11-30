c592a08ad45d206f4aac7ff975537ee91e42e9c3
okaegi <okaegi@pinball4you.ch>
2020-11-30 11:35:54 +0100

    soc für peugeot e208
    Credentials werden in
    $soc_1_username
    $soc_1_password
    $soc_1_clientid
    $soc_1_clientsecret
    und
    $soc_2_username
    $soc_2_password
    $soc_2_clientid
    $soc_2_clientsecret
    erwartet

---
+++ modules/soc_mypeugeotlp2/main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+timer=$(</var/www/html/openWB/ramdisk/soctimer1)
+if (( timer < 60 )); then
+	timer=$((timer+1))
+	echo $timer > /var/www/html/openWB/ramdisk/soctimer1
+else
+ sudo python /var/www/html/openWB/modules/soc_mypeugeotlp2/peugeotsoc.py $soc_2_username $soc_2_password $soc_2_clientid $soc_2_clientsecret
+ echo 0 > /var/www/html/openWB/ramdisk/soctimer1
