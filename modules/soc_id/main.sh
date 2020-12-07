ce4325d90be6583dffa7ad4cfd01dcb179330e66
snaptec <info@snaptec.org>
2020-12-07 11:55:10 +0100

    SoC Timer ID Modul

--- modules/soc_id/main.sh
+++ modules/soc_id/main.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 soctimer=$(</var/www/html/openWB/ramdisk/soctimer)
+ladeleistung=$(</var/www/html/openWB/ramdisk/llaktuell)
 
 
-
-tmpintervall=$(( 720 * 6 ))
+tmpintervall=$(( 480 * 6 ))
 
 if (( soctimer < tmpintervall )); then
 	soctimer=$((soctimer+1))
+	if (( ladeleistung > 500 ));then
+		soctimer=$((soctimer+47))
+	fi
 	echo $soctimer > /var/www/html/openWB/ramdisk/soctimer
 else
 	/var/www/html/openWB/modules/evcc-soc id --user "$soc_id_username" --password "$soc_id_passwort" --vin "$soc_id_vin" > /var/www/html/openWB/ramdisk/soc &
