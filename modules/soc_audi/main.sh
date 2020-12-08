4ce7be6c78de3dae3813082f04d2f0f8920b9b9e
snaptec <info@snaptec.org>
2020-12-08 13:58:16 +0100

    Fix Audi SoC

--- modules/soc_audi/main.sh
+++ modules/soc_audi/main.sh
@@ -9,17 +9,6 @@ if (( auditimer < 180 )); then
 	fi
 	echo $auditimer > /var/www/html/openWB/ramdisk/soctimer
 else
-	answer=$(sudo python3 /var/www/html/openWB/modules/soc_audi/Run.py $soc_audi_username $soc_audi_passwort)
-	vin=$(echo $answer  | sed 's/.*: //' | head -c-3 | cut -c 2-)
-	acctoken=$(cat token.json | jq -r .access_token)
-	battsoc=$(curl -s --header "Accept: application/json" --header "X-App-Name: eRemote" --header "X-App-Version: 1.0.0" --header "User-Agent: okhttp/2.3.0" --header "Authorization: AudiAuth 1 $acctoken" https://msg.audi.de/fs-car/bs/batterycharge/v1/Audi/DE/vehicles/$vin/charger)
-	soclevel=$(echo $battsoc | jq .charger.status.batteryStatusData.stateOfCharge.content)
-
-	re='^-?[0-9]+$'
-	if  [[ $soclevel =~ $re ]] ; then
-		if (( $soclevel != 0 )) ; then
-			echo $soclevel > /var/www/html/openWB/ramdisk/soc
-		fi
-	fi
+	/var/www/html/openWB/modules/evcc-soc audi --user "$soc_audi_username" --password "$soc_audi_passwort" > /var/www/html/openWB/ramdisk/soc &
 	echo 1 > /var/www/html/openWB/ramdisk/soctimer
 fi
