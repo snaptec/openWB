cf05ed58e4e9baf7aca60982dc5da89d15014257
Lutz Bender <github@lutz-bender.de>
2020-12-15 10:52:30 +0100

    soc_tesla: changed logfile handling

--- modules/soc_tesla/main.sh
+++ modules/soc_tesla/main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 TOKENPASSWORD='#TokenInUse#'
 response=''
 
@@ -7,12 +6,12 @@ OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 MODULEDIR=$(cd `dirname $0` && pwd)
 CONFIGFILE="$OPENWBBASEDIR/openwb.conf"
-LOGFILE="$RAMDISKDIR/soc-tesla.log"
+LOGFILE="$RAMDISKDIR/soc.log"
 CHARGEPOINT=$1
 
-socTeslaDebug=$debug
+socDebug=$debug
 # for developement only
-socTeslaDebug=1
+socDebug=1
 
 case $CHARGEPOINT in
 	2)
@@ -45,13 +44,11 @@ esac
 
 password="${!passwordConfigText}"
 
-socTeslaLog(){
-	if (( $socTeslaDebug > 0 )); then
+socDebugLog(){
+	if (( socDebug > 0 )); then
 		timestamp=`date --rfc-3339=seconds`
 		echo "$timestamp: Lp$CHARGEPOINT: $@" >> $LOGFILE
 	fi
-	# limit logfile to 500 lines
-	mv $LOGFILE $LOGFILE.old; tail -n 500 $LOGFILE.old > $LOGFILE; rm $LOGFILE.old
 }
 
 getAndWriteSoc(){
@@ -59,9 +56,9 @@ getAndWriteSoc(){
 	response=$(python $MODULEDIR/teslajson.py --email="$username" --tokens_file="$tokensfile" --vid="$carnumber" --json get data)
 	# current state of car
 	state=$(echo $response | jq .response.state)
-	socTeslaLog "State: $state"
+	socDebugLog "State: $state"
 	soclevel=$(echo $response | jq .response.charge_state.battery_level)
-	socTeslaLog "SoC: $soclevel"
+	socDebugLog "SoC: $soclevel"
 
 	if  [[ $soclevel =~ $re ]] ; then
 		if (( $soclevel != 0 )) ; then
@@ -76,12 +73,12 @@ incrementTimer(){
 }
 
 clearPassword(){
-	socTeslaLog "Removing password from config."
+	socDebugLog "Removing password from config."
 	sed -i "s/$passwordConfigText=.*/$passwordConfigText=''/" $CONFIGFILE
 }
 
 setTokenPassword(){
-	socTeslaLog "Writing token password to config."
+	socDebugLog "Writing token password to config."
 	sed -i "s/$passwordConfigText=.*/$passwordConfigText='$TOKENPASSWORD'/" $CONFIGFILE
 }
 
@@ -91,60 +88,60 @@ checkToken(){
 		'')
 			# empty password tells us to remove a possible saved token
 			if [ -f $tokensfile ]; then
-				socTeslaLog "Empty password set: removing tokensfile."
+				socDebugLog "Empty password set: removing tokensfile."
 				rm $tokensfile
 			fi
-			socTeslaLog "Empty Password - nothing to do."
+			socDebugLog "Empty Password - nothing to do."
 			returnValue=1
 			;;
 		$TOKENPASSWORD)
 			# check if token is present
 			if [ ! -f $tokensfile ]; then
-				socTeslaLog "Tokenpassword set but no token found: clearing password in config."
+				socDebugLog "Tokenpassword set but no token found: clearing password in config."
 				clearPassword
-				socTeslaLog "Tokenpassword without token - nothing to do."
+				socDebugLog "Tokenpassword without token - nothing to do."
 				returnValue=2
 			fi
 			;;
 		*)
 			# new password entered
 			if [ -f $tokensfile ]; then
-				socTeslaLog "New password set: removing tokensfile."
+				socDebugLog "New password set: removing tokensfile."
 				rm $tokensfile
 			fi
 			# Request new token with user/pass.
-			socTeslaLog "Requesting new token..."
+			socDebugLog "Requesting new token..."
 			response=$(python $MODULEDIR/teslajson.py --email="$username" --password="$password" --tokens_file="$tokensfile" --json)
 			# password in response, so do not log it!
 			if [ -f $tokensfile ]; then
-				socTeslaLog "...all done, removing password from config file."
+				socDebugLog "...all done, removing password from config file."
 				setTokenPassword
 			else
-				socTeslaLog "ERROR: Auth with user/pass failed!"
+				socDebugLog "ERROR: Auth with user/pass failed!"
 				echo "Fehler: Anmeldung bei Tesla gescheitert!" > $RAMDISKDIR/lastregelungaktiv
 				returnValue=3
 			fi
 			;;
 	esac
-	socTeslaLog "CheckToken returnValue: $returnValue"
+	socDebugLog "CheckToken returnValue: $returnValue"
 	return "$returnValue"
 }
 
 wakeUpCar(){
-	socTeslaLog "Waking up car."
+	socDebugLog "Waking up car."
 	response=$(python $MODULEDIR/teslajson.py --email="$username" --tokens_file="$tokensfile" --vid="$carnumber" --json do wake_up)
 	state=$(echo $response | jq .response.state)
-	socTeslaLog "Car state after wakeup: $state"
+	socDebugLog "Car state after wakeup: $state"
 }
 
 soctimer=$(<$soctimerfile)
 if (( ladeleistung > 1000 )); then
-	# car is charging
+	# socDebugLog "Car is charging"
 	if (( soctimer < socintervallladen )); then
-		# waiting
+		# socDebugLog "Nothing to do yet. Incrementing timer."
 		incrementTimer
 	else
-		# reset timer
+		socDebugLog "Requesting SoC"
 		echo 0 > $soctimerfile
 		checkToken
 		checkResult=$?
@@ -154,12 +151,12 @@ if (( ladeleistung > 1000 )); then
 		fi
 	fi
 else
-	# car is not charging
+	# socDebugLog "Car is not charging"
 	if (( soctimer < socintervall )); then
-		# waiting
+		# socDebugLog "Nothing to do yet. Incrementing timer."
 		incrementTimer
 	else
-		# reset timer
+		socDebugLog "Requesting SoC"
 		echo 0 > $soctimerfile
 		checkToken
 		checkResult=$?
