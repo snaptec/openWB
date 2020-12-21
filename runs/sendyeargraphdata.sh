a2132e7d6c4077f77dece1dadb2d897e5b6e9ef4
Lutz Bender <github@lutz-bender.de>
2020-12-21 09:27:07 +0100

    fix datepicker in yearly logging

--- runs/sendyeargraphdata.sh
+++ runs/sendyeargraphdata.sh
@@ -21,83 +21,49 @@ echo "$(head -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"12.csv")"
 
 grep . /var/www/html/openWB/ramdisk/yearlydata > /var/www/html/openWB/ramdisk/yearlydatas
 
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${nextyear}01.csv ] 
-then
-#new year found
+if [ -s /var/www/html/openWB/web/logging/data/monthly/${nextyear}01.csv ]; then
+    #new year found
     echo "$(head -n 1 /var/www/html/openWB/web/logging/data/monthly/$nextyear"01.csv")" > /var/www/html/openWB/ramdisk/yearlydata1
 else
-#get the latest record 12
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}12.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"12.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 11
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}11.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"11.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 10
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}10.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"10.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 09
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}09.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"09.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 08
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}08.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"08.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 07
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}07.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"07.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 06
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}06.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"06.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 05
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}05.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"05.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 04
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}04.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"04.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 03
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}03.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"03.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 02
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}02.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"02.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-else
-#get the latest record 01
-if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}01.csv ] 
-then
-   echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"01.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-#   copy this found record to the new year
+    if [ -s /var/www/html/openWB/web/logging/data/monthly/${input}12.csv ]; then
+        #get the latest record 12
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"12.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}11.csv ]; then
+        #get the latest record 11
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"11.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}10.csv ]; then
+        #get the latest record 10
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"10.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}09.csv ]; then
+        #get the latest record 09
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"09.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}08.csv ]; then
+        #get the latest record 08
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"08.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}07.csv ]; then
+        #get the latest record 07
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"07.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}06.csv ]; then
+        #get the latest record 06
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"06.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}05.csv ]; then
+        #get the latest record 05
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"05.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}04.csv ]; then
+        #get the latest record 04
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"04.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}03.csv ]; then
+        #get the latest record 03
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"03.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}02.csv ]; then
+        #get the latest record 02
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"02.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    elif [ -s /var/www/html/openWB/web/logging/data/monthly/${input}01.csv ]; then
+        #get the latest record 01
+        echo "$(tail -n 1 /var/www/html/openWB/web/logging/data/monthly/$input"01.csv")" > /var/www/html/openWB/ramdisk/yearlydata00
+    fi
+
+    # copy this found record to the new year
     cut --complement -f 1 -d, /var/www/html/openWB/ramdisk/yearlydata00 > /var/www/html/openWB/ramdisk/yearlydata02 
     echo "${nextyear}0101" > /var/www/html/openWB/ramdisk/yearlydata01
     paste -d , /var/www/html/openWB/ramdisk/yearlydata01 /var/www/html/openWB/ramdisk/yearlydata02 > /var/www/html/openWB/ramdisk/yearlydata1
