82a12905f56a516ba42383f9eb78a72489d2b49e
DetMoerk <76627235+DetMoerk@users.noreply.github.com>
2020-12-28 14:24:57 +0100

    Comment Bugs fixed
    Copy Paste Errors in Comments fixed

--- modules/soc_eq/callback_lp2.php
+++ modules/soc_eq/callback_lp2.php
@@ -1,5 +1,5 @@
 <?php
-   //Callback procedure for mercedes SoC API LP1 
+   //Callback procedure for mercedes SoC API LP2 
    if( $_GET["code"] ) {
       system( "/var/www/html/openWB/modules/soc_eq/auth.py 2 " . $_GET['code']) ;
    }
--- modules/soc_eqlp2/main.sh
+++ modules/soc_eqlp2/main.sh
@@ -3,6 +3,6 @@
 OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 
 # for backward compatibility only
-# functionality is in soc_myopel
+# functionality is in soc_eq
 $OPENWBBASEDIR/modules/soc_eq/main.sh 2
 exit 0
