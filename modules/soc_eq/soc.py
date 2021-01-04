9b4984e3982dd5cb2c1a674ac9333017c3a03033
DetMoerk <76627235+DetMoerk@users.noreply.github.com>
2021-01-04 16:06:12 +0100

    Logging unf Bug Fixing
    Bug in callback_lp1 fixed
    added Logging to soc.log in soc.py

--- modules/soc_eq/soc.py
+++ modules/soc_eq/soc.py
@@ -1,38 +1,61 @@
 #!/usr/bin/python3
 
-import os, requests, json, time, sys
+import os, requests, json, time, sys, os
+from datetime import datetime, timezone
 
 ramdiskdir = '/var/www/html/openWB/ramdisk/'
 moduledir = '/var/www/html/openWB/modules/soc_eq/'
+
+req_timeout=15 #Timeout for requests in seconds
 
 client_id     = str(sys.argv[1])
 client_secret = str(sys.argv[2])
 VIN           = str(sys.argv[3])
 soc_file      = str(sys.argv[4])
 ChargePoint   = str(sys.argv[5])
+Debug=0
+
+#set the correct permissions as Apache and rest uses different users
+#os.chmod( moduledir + 'acc_tok_lp' + str(ChargePoint),0o646)
+#os.chmod( moduledir + 'ref_tok_lp' + str(ChargePoint),0o646)
+#os.chmod( moduledir + 'expires_lp' + str(ChargePoint),0o646)
+
+
+def socDebugLog(message):
+    local_time = datetime.now(timezone.utc).astimezone()
+#    print(local_time.isoformat() +": Lp" +ChargePoint + ": " + message)
+    print(local_time.strftime(format = "%Y-%m-%d %H:%M:%S%z") +" : Lp" +ChargePoint + ": " + message)
+
+if Debug == 1:
+    socDebugLog("client: " + client_id)
 
 tok_url   = "https://id.mercedes-benz.com/as/token.oauth2"
 #soc_url   = "https://api.mercedes-benz.com/vehicledata/v2/vehicles/"+VIN+"/resources/soc"
 soc_url   = "https://api.mercedes-benz.com/vehicledata/v2/vehicles/"+VIN+"/containers/electricvehicle"
 #range_url = "https://api.mercedes-benz.com/vehicledata/v2/vehicles/"+VIN+"/resources/rangeelectric"
-print("SOC URL: " + soc_url)
+if Debug == 1:
+    socDebugLog("SOC URL: " + soc_url)
 #Get Access token expiry from file
 fd = open(moduledir + 'expires_lp' + str(ChargePoint),'r')
 expires_in = fd.read().rstrip()
 fd.close()
 
-
+#socDebugLog("Expire in: " str((int(expires_in)-int(time.time())))
 if int(expires_in) < int(time.time()):
   #Access Token is exired
-  print("Acc Token Expired")
+  socDebugLog("Acc Token Expired")
   fd = open(moduledir + 'ref_tok_lp' + str(ChargePoint),'r')
   refresh_token = fd.read().rstrip()
   fd.close()
   
   #get new Access Token with referesh token
   data = {'grant_type': 'refresh_token', 'refresh_token': refresh_token }
-  ref = requests.post(tok_url, data=data, verify=True, allow_redirects=False, auth=(client_id, client_secret))
-  print("Refresh Token Call:" + str(ref.status_code))
+  ref = requests.post(tok_url, data=data, verify=True, allow_redirects=False, auth=(client_id, client_secret), timeout=req_timeout)
+  if Debug == 1:
+      socDebugLog("Refresh Token Call:" + str(ref.status_code))
+      socDebugLog("Refresh Token Text:" + str(ref.text))
+
+
   #write HTTP reponse code to file
   try:
     fd = open(ramdiskdir + 'soc_eq_lastresp','w')
@@ -62,8 +85,42 @@ if int(expires_in) < int(time.time()):
     fd = open(moduledir + 'expires_lp' + str(ChargePoint),'w')
     fd.write(str(expires_in))
     fd.close()
+
+  elif ref.status_code == 400:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (Bad Request)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 401:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (Invalid or missing authorization in header)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 402:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (Payment required)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 403:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (Forbidden)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 404:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (The requested resource was not found, e.g.: the selected vehicle could not be found)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 429:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (The service received too many requests in a given amount of time)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 500:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (The service received too many requests in a given amount of time)")
+    socDebugLog(ref.text)
+    exit(1)
+  elif ref.status_code == 503:
+    socDebugLog("Refresh Fehlgeschlagen Code: " + str(ref.status_code) + " (The server is unable to service the request due to a temporary unavailability condition)")
+    socDebugLog(ref.text)
+    exit(1)
   else:
-    print("Error getting Acc after Refresh: " + str(ref.status_code))
+    socDebugLog("Refresh Fehlgeschlagen unbekannter Code: " + str(ref.status_code))
+    socDebugLog(ref.text)
     exit(1)
 
 #get access token from file	
@@ -74,8 +131,10 @@ fd.close()
 #call API for SoC
 header = {'authorization': 'Bearer ' + access_token}
 req_soc = requests.get(soc_url, headers=header, verify=True)
-print("SOC Request: " + str(req_soc.status_code))
-print("SOC Response: " + req_soc.text)
+#req_soc = requests.get(soc_url, headers=header, verify=True, timeout=req_timeout)
+if Debug == 1:
+    socDebugLog("SOC Request: " + str(req_soc.status_code))
+    socDebugLog("SOC Response: " + req_soc.text)
 
 #write HTTP reponse code to file
 try:
@@ -89,18 +148,57 @@ if req_soc.status_code == 200:
   #valid Response
   res = json.loads(req_soc.text)
   #Extract SoC value and write to file
-  #for entry in res:
-    #print(entry)
-    #if entry == 'soc':
-     #soc = entry['soc']['value']
-    #elif entry == 'rangeelectric':
-     #range = entry['rangeelectric']['value']
+  for entry in res:
+      for values in entry:
+          if values == "soc":
+              soc = entry[values]['value']
+          elif values == "rangeelectric":
+              range = entry[values]['value']
+          else:
+              socDebugLog("unknown entry: " + entry)
 
-  soc = res[0]['soc']['value']
-  range = res[1]['rangeelectric']['value']
-  print("SOC: " + soc + " RANGE: " + range)
+  #soc = res[0]['soc']['value']
+  #range = res[1]['rangeelectric']['value']
+  socDebugLog("SOC: " + soc + " RANGE: " + range)
   fd = open(soc_file,'w')
   fd.write(str(soc))
   fd.close()
+  
+elif req_soc.status_code == 400:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (Bad Request)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 401:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (Invalid or missing authorization in header)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 402:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (Payment required)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 403:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (Forbidden)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 404:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (The requested resource was not found, e.g.: the selected vehicle could not be found)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 429:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (The service received too many requests in a given amount of time)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 500:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (The service received too many requests in a given amount of time)")
+  socDebugLog(req_soc.text)
+  exit(1)
+elif req_soc.status_code == 503:
+  socDebugLog("SoC Request Fehlgeschlagen Code: " + str(req_soc.status_code) + " (The server is unable to service the request due to a temporary unavailability condition)")
+  socDebugLog(req_soc.text)
+  exit(1)
+else:
+  socDebugLog("SoC Request Fehlgeschlagen unbekannter Code: " + str(req_soc.status_code))
+  socDebugLog(req_soc.text)
+  exit(1)
 
 exit(0)
