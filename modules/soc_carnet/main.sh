a4a2375ae5a6148079c98a221646f4d9d4ecb2d1
LKuemmel <lena.kuemmel@openwb.de>
2021-01-05 14:40:01 +0100

    Carnet-SoC-Modul überarbeitet

--- modules/soc_carnet/main.sh
+++ modules/soc_carnet/main.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
+CHARGEPOINT=$1
 
-vwtimer=$(</var/www/html/openWB/ramdisk/soctimer)
+case $CHARGEPOINT in
+	2)
+		# second charge point
+		soctimerfile="/var/www/html/openWB/ramdisk/soctimer1"
+		socfile="/var/www/html/openWB/ramdisk/soc1"
+		intervall=$soccarnetlp2intervall
+		;;
+	*)
+		# defaults to first charge point for backward compatibility
+		soctimerfile="/var/www/html/openWB/ramdisk/soctimer"
+		socfile="/var/www/html/openWB/ramdisk/soc"
+		intervall=$soccarnetintervall
+		;;
+esac
+
+vwtimer=$(<$soctimerfile)
 if (( vwtimer < 60 )); then
 	vwtimer=$((vwtimer+1))
-	echo $vwtimer > /var/www/html/openWB/ramdisk/soctimer
+	echo $vwtimer > $soctimerfile
 else
-	/var/www/html/openWB/modules/soc_carnet/soc.sh $carnetuser $carnetpass &
 	#Abfrage Ladung aktiv. Setzen des soctimers. 
-	
 	if (( ladeleistung > 800 )) ; then
 		vwtimer=$((60 * (10 - $soccarnetintervall) / 10))
-		echo $vwtimer > /var/www/html/openWB/ramdisk/soctimer
+		echo $vwtimer > $soctimerfile
 	else
-		echo 1 > /var/www/html/openWB/ramdisk/soctimer
+		echo 1 > $soctimerfile
 	fi
+	
+	/var/www/html/openWB/modules/soc_carnet/soc.sh $carnetuser $carnetpass $socfile
 fi
--- modules/soc_carnet/soc.sh
+++ modules/soc_carnet/soc.sh
@@ -3,7 +3,7 @@
 	soclevel=$(sudo PYTHONIOENCODING=UTF-8 python /var/www/html/openWB/modules/soc_carnet/we_connect_client.py -u $1 -p $2 | grep batteryPercentage | jq -r .EManager.rbc.status.batteryPercentage)
 	if  [[ $soclevel =~ $re ]] ; then
 		if (( $soclevel != 0 )) ; then
-			echo $soclevel > /var/www/html/openWB/ramdisk/soc
+			echo $soclevel > $3
 		fi
 	fi
 
