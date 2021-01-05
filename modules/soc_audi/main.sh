68c9206893c1092a2233cfd97ef09754d29c79f2
LKuemmel <lena.kuemmel@openwb.de>
2021-01-05 12:44:50 +0100

    Audi-SoC-Modul überarbeitet

--- modules/soc_audi/main.sh
+++ modules/soc_audi/main.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+CHARGEPOINT=$1
 
-auditimer=$(</var/www/html/openWB/ramdisk/soctimer)
+case $CHARGEPOINT in
+	2) 
+		# second charge point
+		soctimerfile="/var/www/html/openWB/ramdisk/soctimer1"
+		socfile="/var/www/html/openWB/ramdisk/soc1"
+		username=$soc2user
+		password=$soc2pass
+		;;
+	*)
+		# defaults to first charge point for backward compatibility
+		# set CHARGEPOINT in case it is empty (needed for logging)
+		CHARGEPOINT=1
+		soctimerfile="/var/www/html/openWB/ramdisk/soctimer"
+		socfile="/var/www/html/openWB/ramdisk/soc"
+		username=$soc_audi_username
+		password=$soc_audi_passwort
+		;;
+esac
+
+auditimer=$(<$soctimerfile)
 if (( auditimer < 180 )); then
 	auditimer=$((auditimer+1))
 	if ((ladeleistung > 800 )); then
 		auditimer=$((auditimer+2))
 	fi
-	echo $auditimer > /var/www/html/openWB/ramdisk/soctimer
+	echo $auditimer > $soctimerfile
 else
-	/var/www/html/openWB/modules/evcc-soc audi --user "$soc_audi_username" --password "$soc_audi_passwort" > /var/www/html/openWB/ramdisk/soc &
-	echo 1 > /var/www/html/openWB/ramdisk/soctimer
+	/var/www/html/openWB/modules/evcc-soc audi --user "$username" --password "$passsword" > $socfile &
+	echo 1 > $soctimerfile
 fi
--- modules/soc_audilp2/main.sh
+++ modules/soc_audilp2/main.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 
-auditimer=$(</var/www/html/openWB/ramdisk/soctimer1)
-if (( auditimer < 180 )); then
-	auditimer=$((auditimer+1))
-	if ((ladeleistung > 800 )); then
-		auditimer=$((auditimer+2))
-	fi
-	echo $auditimer > /var/www/html/openWB/ramdisk/soctimer1
-else
-	/var/www/html/openWB/modules/evcc-soc audi --user "$soc2user" --password "$soc2pass" > /var/www/html/openWB/ramdisk/soc1 &
-
-	echo 1 > /var/www/html/openWB/ramdisk/soctimer1
-fi
+# for backward compatibility only
+# functionality is in soc_audi
+$OPENWBBASEDIR/modules/soc_audi/main.sh 2
+exit 0
