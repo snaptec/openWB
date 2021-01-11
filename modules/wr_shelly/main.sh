3a298d383ebc89948add55fc9a58393c47ac78ce
snaptec <info@snaptec.org>
2021-01-11 15:16:05 +0100

    Bugfix Shelly WR

--- modules/wr_shelly/main.sh
+++ modules/wr_shelly/main.sh
@@ -2,9 +2,10 @@
 
 pv_out=$pv_out=$(curl --connect-timeout 3 -s $pv1_ipa/status )
 pv_watt=$(echo $pv_out |jq '.meters[0].power' | sed 's/\..*$//')
-if (( $pv_watt > 0 )); then
-	pv_watt=$(echo "$pv_watt*-1" |bc)
-fi
+#if (( $pv_watt > 0 )); then
+#	pv_watt=$(echo "$pv_watt*-1" |bc)
+#fi
+pv_watt=$(echo "$pv_watt * -1" | bc)
 re='^-?[0-9]+$'
 if ! [[ $pv_watt =~ $re ]] ; then
    pv_watt="0"
