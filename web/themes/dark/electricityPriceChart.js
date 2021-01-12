c1b99963ea998ee0719c66ca38b07d30d7c26986
Michael <michael@ortenstein.de>
2021-01-12 22:56:49 +0100

    Plugin Tetsts

--- web/themes/dark/electricityPriceChart.js
+++ web/themes/dark/electricityPriceChart.js
@@ -2,15 +2,14 @@ function loadElectricityPriceChart() {
 	var electricityPriceChartData = {
 		labels: electricityPriceTimeline,
 		datasets: [{
-			label: 'Strompreis Cent/kWh',
+			yAxisID: 'y-axis-0',
+			data: electricityPriceChartline,
 			borderColor: "rgba(255, 155, 155, 0.9)",
 			backgroundColor: "rgba(0, 0, 255, 0.7)",
 			borderWidth: 2,
 			fill: false,
-			data: electricityPriceChartline,
-			yAxisID: 'y-axis-1',
 			steppedLine: true
-		} ]
+		}]
 	}
 
 	var ctxa = $('#electricityPriceChartCanvas')[0].getContext('2d');
@@ -46,30 +45,38 @@ function loadElectricityPriceChart() {
 							fontColor: "rgba(153, 153, 153, 1)"
 					}
 				}],
-				yAxes: [
-					{
-						// horizontal line for values displayed on the left side (power)
-						position: 'left',
-						id: 'y-axis-1',
-						type: 'linear',
+				yAxes: [{
+					// values price
+					position: 'left',
+					id: 'y-axis-0',
+					type: 'linear',
+					display: true,
+					scaleLabel: {
 						display: true,
-						scaleLabel: {
-						display: true,
-						labelString: 'Preis Cent/kWh',
-							// middle grey, opacy = 100% (visible)
-							fontColor: "rgba(153, 153, 153, 1)"
+						labelString: 'Strompreis [Cent/kWh]',
+						fontColor: "rgba(153, 153, 153, 1)"
 					},
-						gridLines: {
-							// light grey, opacy = 100% (visible)
-							color: "rgba(204, 204, 204, 1)",
-						},
-						ticks: {
-							// middle grey, opacy = 100% (visible)
-							fontColor: "rgba(153, 153, 153, 1)"
-						}
-
-					}]
+					gridLines: {
+						color: "rgba(204, 204, 204, 1)",
+					},
+					ticks: {
+						fontColor: "rgba(153, 153, 153, 1)"
+					}
+				}]
+			},
+			plugins:{
+						annotation: {
+			        annotations: [{
+			            type: 'line',
+			            mode: 'horizontal',
+			            scaleID: 'x-axis-1',
+			            value: 26,
+			            borderColor: 'tomato',
+			            borderWidth: 4
+			        }],
+			        drawTime: "afterDraw" // (default)
+			    }
 			}
 		}
 	});
-}  // end loadgraph
+}
