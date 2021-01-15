0cea3ae065eccb06e08da1398a856137298b762d
Michael <michael@ortenstein.de>
2021-01-15 10:47:47 +0100

    commit to wrong branch reversed

--- web/themes/dark/electricityPriceChart.js
+++ web/themes/dark/electricityPriceChart.js
@@ -63,34 +63,7 @@ function loadElectricityPriceChart() {
 						fontColor: "rgba(153, 153, 153, 1)"
 					}
 				}]
-			},
-			annotation: {
-		        annotations: [{
-		            type: 'line',
-					id: 'maxPrice',
-		            mode: 'horizontal',
-		            scaleID: 'y-axis-left',
-		            value: 32,
-		            borderColor: 'rgba(73, 238, 73, 0.7)',
-		            borderWidth: 2,
-					label: {
-				        backgroundColor: 'rgba(73, 238, 73, 0.6)',
-				        font: {
-				            // Font style of text, default below
-				            style: "bold",
-				            // Font color of text, default below
-				            color: "rgba(20, 122, 20, 1)",
-				        },
-				        // Whether the label is enabled and should be displayed
-				        enabled: true,
-				        // Text to display in label - default is null. Provide an array to display values on a new line
-				        content: "preiyslabel",
-				        // Rotation of label, in degrees, or 'auto' to use the degrees of the line, default is 0
-				        rotation: "auto"
-				    }
-		        }],
-		        drawTime: "afterDatasetsDraw" // (default)
-		    }
+			}
 		}
 	});
 }
