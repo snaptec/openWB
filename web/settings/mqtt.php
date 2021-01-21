52a4a15afd1c30fc25d04343693f414bdcb68659
LKuemmel <lena.kuemmel@openwb.de>
2021-01-21 11:07:11 +0100

    Textkorrektur, Patterns

--- web/settings/mqtt.php
+++ web/settings/mqtt.php
@@ -128,23 +128,25 @@
 
 					if ($firstLoopDone) echo "<hr>";
 			?>
-			<form action="./tools/savemqtt.php?bridge=<?php echo urlencode($connectionName); ?>" method="POST">
+			<form action="./tools/savemqtt.php" method="POST">
+				<!-- previous bridge name, needed for renaming a bridge -->
+				<input type="hidden" readonly="readonly" name="bridge" value="<?php echo($connectionName); ?>">
 
 				<!-- Konfiguration -->
 				<div class="card border-secondary">
 					<div class="card-header bg-secondary">
-						Konfiguration der MQTT-Br&uuml;cke
+						Konfiguration der MQTT-Brücke
 					</div>
 					<div class="card-body">
 						<div class="card-text alert alert-danger">
-						<u>ACHTUNG</u>: Die Konfiguration einer MQTT-Br&uuml;cke erlaubt allen Nutzern mit Zugang zum entfernten MQTT-Server alle weitergeleiteten Daten dieser openWB einzusehen!<br/>
-						Es wird dringend empfohlen, dies nur f&uuml;r nicht-&ouml;ffentliche MQTT-Server unter Verwendung starker Transport-Verschl&uuml;sselung (TLS)  mit personfiziertem Login und 
-						strenger Zugriffskontrolle (zumindest f&uuml;r die MQTT-Thema unterhalb von &quot;Entfernter Pr&auml;fix&quot;) zu aktivieren!
+						<u>ACHTUNG</u>: Die Konfiguration einer MQTT-Brücke erlaubt allen Nutzern mit Zugang zum entfernten MQTT-Server alle weitergeleiteten Daten dieser openWB einzusehen!<br/>
+						Es wird dringend empfohlen, dies nur für nicht-öffentliche MQTT-Server unter Verwendung starker Transport-Verschlüsselung (TLS)  mit personfiziertem Login und 
+						strenger Zugriffskontrolle (zumindest für die MQTT-Thema unterhalb von "Entfernter Präfix") zu aktivieren!
 						</div>
 						<div class="form-group">
 							<div class="form-row mb-1">
 								<div class="col-md-4">
-									<label class="col-form-label">MQTT-Br&uuml;cke</label>
+									<label class="col-form-label">MQTT-Brücke</label>
 								</div>
 								<div class="col">
 									<div class="btn-group btn-block btn-group-toggle" data-toggle="buttons">
@@ -159,38 +161,38 @@
 							</div>
 						</div>
 						<div class="form-row mb-1">
-							<label for="ConnectionName" class="col-md-4 col-form-label">Name der Br&uuml;cke</label>
+							<label for="ConnectionName" class="col-md-4 col-form-label">Name der Brücke</label>
 							<div class="col">
-								<input class="form-control" type="text" size="35" name="ConnectionName" id="ConnectionName" value="<?php echo $connectionName; ?>"><br/>
+								<input class="form-control" type="text" size="35" name="ConnectionName" id="ConnectionName" pattern="^[a-zA-Z0-9]+$" value="<?php echo $connectionName; ?>"><br/>
 								<?php if($debugold >= 1) echo "<small>in Datei '$currentFile'</small>"; ?>
 							</div>
 						</div>
 						<div class="form-row mb-1">
 							<label for="RemoteAddress" class="col-md-4 col-form-label">Addresse und Portnummer des entfernten MQTT-Servers</label>
 							<div class="col">
-								<input class="form-control" type="text" size="50" name="RemoteAddress" id="RemoteAddress" value="<?php echo $remoteAddressAndPort; ?>">
-								<span class="form-text small">Entfernter MQTT-Server und Port-Nummer. Standard Port ist 8883 f&uuml;r eine TLS-gesch&uuml;tzte Verbindung.</span>
+								<input class="form-control" type="text" size="50" name="RemoteAddress" id="RemoteAddress" pattern="^([a-zA-Z0-9][a-zA-Z0-9.-]+):{0,1}([0-9]{0,5})$" value="<?php echo $remoteAddressAndPort; ?>">
+								<span class="form-text small">Entfernter MQTT-Server und Port-Nummer. Standard Port ist 8883 für eine TLS-geschützte Verbindung.</span>
 							</div>
 						</div>
 						<div class="form-row mb-1">
 							<label for="RemoteUser" class="col-md-4 col-form-label">Benutzer</label>
 							<div class="col">
-								<input class="form-control" type="text" size="35" name="RemoteUser" id="RemoteUser" value="<?php echo $remoteUser; ?>">
-								<span class="form-text small">Benutzername f&uuml;r den Login auf dem entfernten MQTT-Server.</span>
+								<input class="form-control" type="text" size="35" name="RemoteUser" id="RemoteUser" pattern="^([a-zA-Z0-9_\-+.]+)$" value="<?php echo $remoteUser; ?>">
+								<span class="form-text small">Benutzername für den Login auf dem entfernten MQTT-Server.</span>
 							</div>
 						</div>
 						<div class="form-row mb-1">
 							<label for="RemotePass" class="col-md-4 col-form-label">Passwort</label>
 							<div class="col">
-								<input class="form-control" ype="password" size="35" name="RemotePass" id="RemotePass" value="<?php echo $remotePassword; ?>">
-								<span class="form-text small">Passwort f&uuml;r den Login auf dem entfernten MQTT-Server. Leerzeichen am Anfang und Ende des Passworts werden nicht unterst&uuml;tzt.</span>
+								<input class="form-control" ype="password" size="35" name="RemotePass" id="RemotePass" pattern="^\S.\S+$" value="<?php echo $remotePassword; ?>">
+								<span class="form-text small">Passwort für den Login auf dem entfernten MQTT-Server. Leerzeichen am Anfang und Ende des Passworts werden nicht unterstützt.</span>
 							</div>
 						</div>
 						<div class="form-row mb-1">
-							<label for="RemotePrefix" class="col-md-4 col-form-label">Entfernter Pr&auml;fix</label>
+							<label for="RemotePrefix" class="col-md-4 col-form-label">Entfernter Präfix</label>
 							<div class="col">
-								<input class="form-control" ype="text" size="55" name="RemotePrefix" id="RemotePrefix" value="<?php echo $remotePrefix; ?>">
-								<span class="form-text small">MQTT-Thema Pr&auml;fix, welches dem 'openWB/...' vorangestellt wird.<br/>
+								<input class="form-control" ype="text" size="55" name="RemotePrefix" id="RemotePrefix" pattern="^[a-zA-Z0-9_\-\/]+[/]$" value="<?php echo $remotePrefix; ?>">
+								<span class="form-text small">MQTT-Thema Präfix, welches dem 'openWB/...' vorangestellt wird.<br/>
 									Beispiel: Wenn in diesem Feld 'pfx/' eingetragen wird, werden alle Weiterleitungen und Registrierungen auf der entfernten Seite mit 'pfx/openWB/...' benannt.</span>
 							</div>
 						</div>
@@ -221,17 +223,16 @@
 											<td style="text-align: left; vertical-align: center; padding: 10px;">
 												<input type="radio" name="tlsProtocol" value="tlsv1.3" <?php if (!$tlsv13Supported) echo "disabled"; ?> 
 												<?php echo $tlsVersion == "tlsv1.3" ? "checked=\"checked\"" : "" ?>>&nbsp;TLSv1.3
-												<span style="color: green; font-size:small;"><?php echo $tlsv13Supported ? "Bevorzugt" : "<br/>Auf Debian Versionen kleiner<br/>'Buster' nicht unterst&uuml;tzt"; ?></span>
+												<span style="color: green; font-size:small;"><?php echo $tlsv13Supported ? "Bevorzugt" : "<br/>Auf Debian Versionen kleiner<br/>'Buster' nicht unterstützt"; ?></span>
 											</td>
 											<td style="text-align: left; vertical-align: center; padding: 10px;">
 												<input type="radio" name="tlsProtocol" value="tlsv1.2" <?php echo $tlsVersion == "tlsv1.2" ? "checked=\"checked\"" : "" ?>>&nbsp;TLSv1.2
-												<span style="color: green; font-size:small;"><?php if (!$tlsv13Supported) echo "<br/>Empfohlen"; ?></span><
+												<span style="color: green; font-size:small;"><?php if (!$tlsv13Supported) echo "<br/>Empfohlen"; ?></span>
 											</td>
-											<!-- td style="text-align: left; vertical-align: center; padding: 10px;"><input type="radio" name="tlsProtocol" value="tlsv1.1" <?php echo $tlsVersion == "tlsv1.1" ? "checked=\"checked\"" : "" ?>>&nbsp;TLSv1.1<br/><span style="color: darkred; font-size:small;">Can be broken.</span><td/ -->
 										</tr>
 									</table>
 								</fieldset>
-								<span class="form-text small">Version des TLS Protokolls, welches zur Verschl&uuml;sselung der Kommunikation mit dem entfernten Server verwendet wird.</span>
+								<span class="form-text small">Version des TLS Protokolls, welches zur Verschlüsselung der Kommunikation mit dem entfernten Server verwendet wird.</span>
 							</div>
 						</div>
 					</div>
@@ -267,7 +268,7 @@
 						<div class="form-group">
 							<div class="form-row mb-1">
 								<div class="col-md-4">
-									<label class="col-form-label">Datenserien f&uuml;r Diagramme</label>
+									<label class="col-form-label">Datenserien für Diagramme</label>
 								</div>
 								<div class="col">
 									<div class="btn-group btn-block btn-group-toggle" data-toggle="buttons">
@@ -292,14 +293,14 @@
 					<div class="card-body">
 						<div class="card-text alert alert-danger">
 							<u>ACHTUNG</u>: Dies erlaubt jedem Nutzer des entfernten MQTT-Servers mit Zugriff auf die entsprechenden Themen, diese openWB fern zu steuern!<br/>
-							Es wird schwer empfohlen, dies nur f&uuml;r nicht-&ouml;ffentliche MQTT-Server unter Verwendung starker Transport-Verschl&uuml;sselung (TLS) mit 
+							Es wird schwer empfohlen, dies nur für nicht-öffentliche MQTT-Server unter Verwendung starker Transport-Verschlüsselung (TLS) mit 
 							personfiziertem Login und strenger Zugriffskontrolle zu aktivieren!<br/>
-							KEINESFALLS AUF <u>&Ouml;FFENTLICH ZUG&Auml;NGLICHEN</u> MQTT-SERVERN AKTIVEREN!!!
+							KEINESFALLS AUF <u>ÖFFENTLICH ZUGÄNGLICHEN</u> MQTT-SERVERN AKTIVEREN!!!
 						</div>
 						<div class="form-group">
 							<div class="form-row mb-1">
 								<div class="col-md-4">
-									<label class="col-form-label">Fernkonfiguration erm&ouml;glichen (gef&auml;hrlich)</label>
+									<label class="col-form-label">Fernkonfiguration ermöglichen (gefährlich)</label>
 								</div>
 								<div class="col">
 									<div class="btn-group btn-block btn-group-toggle" data-toggle="buttons">
@@ -319,11 +320,11 @@
 				<div class="row justify-content-center py-1">
 					<div class="col text-center">
 						Das Anwenden der Änderungen dauert <?php echo $refreshDuration ?> Sekunden. Bitte die openWB in dieser Zeit nicht vom Strom trennen!<br><br>
-						<button type="submit" class="btn btn-green" name="action" value="saveBridge">Einstellungen f&uuml;r Br&uuml;cke '<?php echo urlencode($connectionName); ?>' speichern</button>
+						<button type="submit" class="btn btn-green" name="action" value="saveBridge">Einstellungen für Brücke '<?php echo urlencode($connectionName); ?>' speichern</button>
 					</div>
 				</div>
 				<div class="row justify-content-center py-1">
-					<button type="submit" class="btn btn-green" name="action" value="deleteBridge">Br&uuml;cke '<?php echo urlencode($connectionName); ?>' l&ouml;schen</button>
+					<button type="submit" class="btn btn-green" name="action" value="deleteBridge">Brücke '<?php echo urlencode($connectionName); ?>' löschen</button>
 				</div>
 
 			</form>
