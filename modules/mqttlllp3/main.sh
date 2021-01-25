dcdb2d2db099ba4fe46232ab226ad0a90794cb3e
Lutz Bender <github@lutz-bender.de>
2021-01-25 14:13:04 +0100

    fixes to update and reboot

--- modules/mqttlllp3/main.sh
+++ modules/mqttlllp3/main.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+#all handled in loadvars.sh & mqttsub.py
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
+MODULEDIR=$(cd `dirname $0` && pwd)
+
+source $OPENWBBASEDIR/helperFunctions.sh
+
+openwbPublishModuleState "LP" 0 "Kein Fehler" 3
