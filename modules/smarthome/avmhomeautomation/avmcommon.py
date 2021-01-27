097abd5f4536bdb76d476e2bf1fb03092d294b9c
Fabian Wickborn <fabian@wickborn.net>
2021-01-27 20:54:13 +0100

    Typo (unintended linebreak)

--- modules/smarthome/avmhomeautomation/avmcommon.py
+++ modules/smarthome/avmhomeautomation/avmcommon.py
@@ -129,8 +129,8 @@ class AVMHomeAutomation:
     # an authentication to obtain a new session ID if neccessary. A session ID
     # is assumed to be recent enough if it has been obtained ramdisk less than
     # 5 minutes ago in which case it is reused.
-    def connect(self): should_authenticate = True
-
+    def connect(self): 
+        should_authenticate = True
         ownInfo = self.cachedOwnInfo()
         if "session_mtime" in ownInfo and "session_id" in ownInfo:
             self.sessionID = ownInfo["session_id"]
