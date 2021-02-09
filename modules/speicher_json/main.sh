6658ee6ddef46ed150cd249244cc65a5d9a5c3d0
DetMoerk <76627235+DetMoerk@users.noreply.github.com>
2021-02-09 18:31:20 +0100

    Speicher_JSON Module BETA!!!!
    New Module to use Home Battery with JSON Output.
    Can use complex jq Filters like PV_JSON and EVU_JSON
    Description in moduleconfigbat.php has to be reworked.

---
+++ modules/speicher_json/main.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
+MODULEDIR=$(cd `dirname $0` && pwd)
+MODULE="BATT"
+#LOGFILE="$RAMDISKDIR/speicher.log"
+LOGFILE="$RAMDISKDIR/openwb.log"
+Debug=$debug
+myPid=$$
+
+#For Development only
+#Debug=1
+
+DebugLog(){
+        if (( Debug > 0 )); then
+                timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+                if (( Debug == 2 )); then
+                      echo "$timestamp: ${MODULE}: PID:$myPid: $@" >> $LOGFILE
+                else
+                      echo "$timestamp: ${MODULE}: $@" >> $LOGFILE
+                fi
+        fi
+}
+
+
+
+answer=$(curl --connect-timeout 5 -s $battjsonurl)
+speicherleistung=$(echo $answer | jq -r "$battjsonwatt" | sed 's/\..*$//')
+	if (( $speicherleistung > 5 )); then
+		speicherleistung=$(echo "$speicherleistung*-1" |bc)
+	fi	
+DebugLog "BattLeistung: ${speicherleistung}"
+echo ${speicherleistung}
+echo $speicherleistung > /var/www/html/openWB/ramdisk/speicherleistung
+battsoc=$(echo $answer | jq -r "$battjsonsoc")
+DebugLog "BattSoC: ${battsoc}"
+echo $battsoc > /var/www/html/openWB/ramdisk/speichersoc
