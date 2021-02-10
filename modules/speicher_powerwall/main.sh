54b87c61d9f9ed42530884645ef961bad9882308
benderl <benderl@users.noreply.github.com>
2021-02-10 10:45:17 +0100

    Powerwall: added logging (#1035)
    * add logging to powerwall modules

--- modules/speicher_powerwall/main.sh
+++ modules/speicher_powerwall/main.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
-MODULEDIR=$(cd `dirname $0` && pwd)
+MODULE="BATT"
+#LOGFILE="$RAMDISKDIR/speicher.log"
+LOGFILE="$RAMDISKDIR/openwb.log"
+Debug=$debug
+myPid=$$
+
+#For Development only
+Debug=1
+
 COOKIEFILE="$RAMDISKDIR/powerwall_cookie.txt"
 cookieOptions=""
 
+DebugLog(){
+	if (( Debug > 0 )); then
+		timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+		if (( Debug == 2 )); then
+			echo "$timestamp: ${MODULE}: PID:$myPid: $@" >> $LOGFILE
+		else
+			echo "$timestamp: ${MODULE}: $@" >> $LOGFILE
+		fi
+	fi
+}
+
 if (( speicherpwloginneeded == 1 )); then
-	# log in and save cookie for later use
-	curl -s -k -i -c $COOKIEFILE -X POST -H "Content-Type: application/json" -d "{\"username\":\"customer\",\"password\":\"$speicherpwpass\", \"email\":\"$speicherpwuser\",\"force_sm_off\":false}" "https://$speicherpwip/api/login/Basic"
+	# delete our login cookie after some time as it may be invalid
+	if [[ $(find "$COOKIEFILE" -cmin +60) ]]; then
+		DebugLog "Deleting saved login cookie after 1 hour as it may not be valid anymore."
+		rm "$COOKIEFILE"
+	fi
+	if [[ ! -f "$COOKIEFILE" ]]; then
+		# log in and save cookie for later use
+		DebugLog "Trying to authenticate..."
+		curl -s -k -i -c $COOKIEFILE --connect-timeout 5 -X POST -H "Content-Type: application/json" -d "{\"username\":\"customer\",\"password\":\"$speicherpwpass\", \"email\":\"$speicherpwuser\",\"force_sm_off\":false}" "https://$speicherpwip/api/login/Basic"
+		exitCode=$?
+		if (( exitCode != 0 )); then
+			DebugLog "Something went wrong. Curl Exit Code: $exitCode"
+			exit
+		else
+			DebugLog "Login successfull."
+		fi
+	else
+		DebugLog "Using saved login cookie."
+	fi
 	# tell next curl calls to use saved cookie
 	cookieOptions="-b $COOKIEFILE -c $COOKIEFILE"
 fi
