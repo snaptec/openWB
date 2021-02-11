a3bb1bafcaf35857bf5826c6e17b65136f030cbf
DetMoerk <76627235+DetMoerk@users.noreply.github.com>
2021-02-11 21:12:06 +0100

    SOC_EQ Fix Mqttcall
    Wrong order of Parameters

--- modules/soc_eq/main.sh
+++ modules/soc_eq/main.sh
@@ -8,7 +8,7 @@ CHARGEPOINT=$1
 myPid=$$
 socDebug=$debug
 # for developement only
-socDebug=1
+#socDebug=1
 
 
 case $CHARGEPOINT in
@@ -69,5 +69,5 @@ else
   $MODULEDIR/soc.py $soc_eq_client_id $soc_eq_client_secret $soc_eq_vin $soc_file $CHARGEPOINT >>$LOGFILE
   ret=$?
   socDebugLog "Py Return: ${ret}"
-  openwbModulePublishState "EVSOC" "$CHARGEPOINT" "Code:" "${ret}"
+  openwbModulePublishState "EVSOC" "${ret}" "Code: ${ret}" "$CHARGEPOINT"
 fi
