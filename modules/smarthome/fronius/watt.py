9a05b7b759532f9d26b687d75fea6ee2feccdc62
Flohlaus <79670120+Flohlaus@users.noreply.github.com>
2021-03-01 14:21:47 +0100

    Rückgabewert "Relais" entfernt

--- modules/smarthome/fronius/watt.py
+++ modules/smarthome/fronius/watt.py
@@ -16,13 +16,7 @@ answer = json.loads(str(urllib.request.urlopen("http://"+str(ipadr)+"/solar_api/
 aktpower = answer['Body']['Data']['PowerReal_P_Sum']*-1
 powerc = answer['Body']['Data']['EnergyReal_WAC_Sum_Consumed']
 
-#if aktpower > 50:
-#    relais = 1
-#else:
-#    relais = 0
-
 answer = '{"power":' + str(aktpower) + ',"powerc":' + str(powerc)  + '} '
-#answer = '{"power":' + str(aktpower) + ',"powerc":' + str(powerc) + ',"on":' + str(relais) + '} '
 f1 = open('/var/www/html/openWB/ramdisk/smarthome_device_ret' + str(devicenumber), 'w')
 json.dump(answer,f1)
 f1.close()
