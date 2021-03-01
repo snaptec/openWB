a0e6732dbdb4b2f49f25539faf227d89b2e8e97c
Flohlaus <79670120+Flohlaus@users.noreply.github.com>
2021-03-01 13:17:13 +0100

    Kommentare hinzu

--- modules/smarthome/fronius/watt.py
+++ modules/smarthome/fronius/watt.py
@@ -9,8 +9,8 @@ named_tuple = time.localtime() # getstruct_time
 time_string = time.strftime("%m/%d/%Y, %H:%M:%S fronius watty.py", named_tuple)
 
 devicenumber=str(sys.argv[1])
-ipadr=str(sys.argv[2])
-smid=int(sys.argv[3])
+ipadr=str(sys.argv[2])        #IP-ADresse des Fronius Wechselrichters, mit dem der Zähler kommuniziert
+smid=int(sys.argv[3])         #ID des Zählers im Wechselrichter (Hauptzähler 0, weitere fortlaufend)
 
 answer = json.loads(str(urllib.request.urlopen("http://"+str(ipadr)+"/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceID="+str(smid), timeout=3).read().decode("utf-8")))
 aktpower = answer['Body']['Data']['PowerReal_P_Sum']*-1
