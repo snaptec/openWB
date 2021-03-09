cd2e5f51f3458c1e6a94956bea4a8778993cdfa7
okaegi <okaegi@pinball4you.ch>
2021-03-09 12:23:49 +0100

    Ersatz monthly v2
    Ersatz monthly v2

--- runs/initRamdisk.sh
+++ runs/initRamdisk.sh
@@ -590,5 +590,6 @@ initRamdisk(){
 	echo $importtemp > $RamdiskPath/smarthomehandlermaxbatterypower
 
 	sudo chmod 777 $RamdiskPath/*
+	#python3 /var/www/html/openWB/runs/csvcalc.py /var/www/html/openWB/web/logging/data/daily/ /var/www/html/openWB/web/logging/data/v001/ /var/www/html/openWB/ramdisk/ M >> /var/www/html/openWB/ramdisk/csvcalc.log 2>&1 &
 	echo "Ramdisk init done."
 }
