bd2161f54bc33bdc6efd2e508fdb338369b822d5
jss-niro <jan@js-schwarz.de>
2021-03-13 11:55:00 +0100

    Add stamps to kia soc module to fix problem with new API

--- modules/soc_kia/kiasoc.py
+++ modules/soc_kia/kiasoc.py
@@ -2,14 +2,19 @@ import sys
 import requests
 import uuid
 import json
+import random
 import urllib.parse as urlparse
 from urllib.parse import parse_qs
+import stamps
 
 email = str(sys.argv[1])
 password = str(sys.argv[2])
 pin = str(sys.argv[3])
 vin = str(sys.argv[4])
 socfile = str(sys.argv[5])
+
+def get_stamp(): 
+    return stamps.stamps[random.randint(0,len(stamps.stamps))]
 
 def main():
     #diviceID
@@ -21,7 +26,8 @@ def main():
         'Host': 'prd.eu-ccapi.kia.com:8080',
         'Connection': 'close',
         'Accept-Encoding': 'gzip, deflate',
-        'User-Agent': 'okhttp/3.10.0'}
+        'User-Agent': 'okhttp/3.10.0',
+        'Stamp': get_stamp()}
     data = {"pushRegId":"1","pushType":"GCM","uuid": str(uuid.uuid1())}
     response = requests.post(url, json = data, headers = headers)
     if response.status_code == 200:
@@ -93,7 +99,8 @@ def main():
         'Host': 'prd.eu-ccapi.kia.com:8080',
         'Connection': 'close',
         'Accept-Encoding': 'gzip, deflate',
-        'User-Agent': 'okhttp/3.10.0'}
+        'User-Agent': 'okhttp/3.10.0',
+        'Stamp': get_stamp()}
     response = requests.get(url, headers = headers)
     if response.status_code == 200:
         response = json.loads(response.text)
@@ -117,7 +124,8 @@ def main():
         'Host': 'prd.eu-ccapi.kia.com:8080',
         'Connection': 'close',
         'Accept-Encoding': 'gzip, deflate',
-        'User-Agent': 'okhttp/3.10.0'}
+        'User-Agent': 'okhttp/3.10.0',
+        'Stamp': get_stamp()}
     data = {"action":"prewakeup","deviceId": deviceId}
     response = requests.post(url, json = data, headers = headers)
     if response.status_code == 200:
@@ -152,7 +160,8 @@ def main():
     headers = {
         'Authorization': controlToken,
         'ccsp-device-id': deviceId,
-        'Content-Type': 'application/json'}
+        'Content-Type': 'application/json',
+        'Stamp': get_stamp()}
     response = requests.get(url, headers = headers)
     if response.status_code == 200:
        statusresponse = json.loads(response.text)
