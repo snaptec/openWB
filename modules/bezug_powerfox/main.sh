2d2258aac0be24e0f09d8d3af73626a416f04900
mutsch75 <80944912+mutsch75@users.noreply.github.com>
2021-03-19 17:02:53 +0100

    Update main.sh
    War natürlich falsches file

--- modules/bezug_powerfox/main.sh
+++ modules/bezug_powerfox/main.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
-
-OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
-RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
-MODULEDIR=$(cd `dirname $0` && pwd)
-#DMOD="EVU"
-DMOD="MAIN"
-Debug=$debug
+output=$(curl --connect-timeout 3 -s -u $powerfoxuser:"$powerfoxpass" "https://backend.powerfox.energy/api/2.0/my/$powerfoxid/current")
 
 
-#For development only
-#Debug=1
+einspeisungwh=$(echo $output | jq '.A_Minus')
+echo $einspeisungwh > /var/www/html/openWB/ramdisk/einspeisungkwh
 
-openwbDebugLog ${DMOD} 2 "${bezugjsonwatt}"
-openwbDebugLog ${DMOD} 2 "${bezugjsonkwh}"
-openwbDebugLog ${DMOD} 2 "${einspeisungjsonkwh}"
+bezugwh=$(echo $output | jq '.A_Plus')
+echo $bezugwh > /var/www/html/openWB/ramdisk/bezugkwh
 
-answer=$(curl --connect-timeout 5 -s $bezugjsonurl)
-evuwatt=$(echo $answer | jq -r "$bezugjsonwatt" | sed 's/\..*$//')
-echo ${evuwatt}
-openwbDebugLog  ${DMOD} 1 "Watt: ${evuwatt}"
-echo $evuwatt > /var/www/html/openWB/ramdisk/wattbezug
 
-if [ ! -z "${bezugjsonkwh}" ]; then
-    evuikwh=$(echo $answer | jq -r "$bezugjsonkwh")
-else
-    evuikwh=0
-fi
+watt=$(echo $output | jq '.Watt')
+echo $watt > /var/www/html/openWB/ramdisk/wattbezug
 
-openwbDebugLog ${DMOD} 1 "BezugkWh: ${evuikwh}"
-echo $evuikwh > /var/www/html/openWB/ramdisk/bezugkwh
 
-if [ ! -z "${einspeisungjsonkwh}" ]; then
-    evuekwh=$(echo $answer | jq -r "$einspeisungjsonkwh")
-else
-    evuekwh=0
-fi
-openwbDebugLog ${DMOD} 1 "EinspeiskWh: ${evuekwh}"
-echo $evuekwh > /var/www/html/openWB/ramdisk/einspeisungkwh
-
+echo $watt
