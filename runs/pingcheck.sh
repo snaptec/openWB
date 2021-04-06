45aa5cda083b30b166e1804811d7214bf66a8617
DetMoerk <76627235+DetMoerk@users.noreply.github.com>
2021-04-06 14:43:52 +0200

    Pingcheck enhancement
    print Load into openwb.conf if Debug >=1

--- runs/pingcheck.sh
+++ runs/pingcheck.sh
@@ -3,6 +3,7 @@
 OPENWBBASEDIR=$(cd `dirname $0`/../ && pwd)
 RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 MYLOGFILE="$RAMDISKDIR/openWB.log"
+DEBUG=$debug
 
 DebugLog(){
 	timestamp=`date +"%Y-%m-%d %H:%M:%S"`
@@ -11,7 +12,10 @@ DebugLog(){
 
 #get Gateway for Connection
 gateway=$(ip route get 1 | awk '{print $3;exit}')
-
+if (( $DEBUG >= 1 )); then
+	load=$(uptime |cut -d ' ' -f 13-15)
+	DebugLog "Load: ${load}"
+fi
 #ping the gateway to see if the connection is OK
 ping -c1 $gateway >/dev/null
 ret=$?
