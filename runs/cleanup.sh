f49ed7984c42e8ecd02d05ad8b0272657c3acbfe
Lutz Bender <github@lutz-bender.de>
2021-04-07 16:58:36 +0200

    fix backup and debug.log

--- runs/cleanup.sh
+++ runs/cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-find /var/www/html/openWB/ramdisk/* -name "*.log" -type f -exec /var/www/html/openWB/runs/cleanupf.sh {} \;
-
+if [ ! -f /var/www/html/openWB/ramdisk/debuguser ]; then
+	find /var/www/html/openWB/ramdisk/* -name "*.log" -type f -exec /var/www/html/openWB/runs/cleanupf.sh {} \;
+else
+	echo "Skipping logfile cleanup as senddebug.sh is collecting data." >> /var/www/html/openWB/ramdisk/openwb.log
+fi
