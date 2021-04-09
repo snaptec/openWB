9961ca876d303615c2d474aa82f496302cf628ce
Bucky2k <67497826+Bucky2k@users.noreply.github.com>
2021-04-09 19:45:10 +0200

    - typo

--- modules/soc_evcclp2/main.sh
+++ modules/soc_evcclp2/main.sh
@@ -3,6 +3,6 @@
 OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 
 # for backward compatibility only
-# functionality is in soc_http
+# functionality is in soc_evcc
 $OPENWBBASEDIR/modules/soc_evcc/main.sh 2
 exit 0
