7d518b0169bdb70b9f3c9236c9a0c19ef91ee14d
snaptec <info@snaptec.org>
2021-04-16 07:30:20 +0200

    Bugfix tronity Modul Lp2

--- modules/soc_tronitylp2/main.sh
+++ modules/soc_tronitylp2/main.sh
@@ -4,5 +4,5 @@ OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 
 # for backward compatibility only
 # functionality is in soc_tronity
-$OPENWBBASEDIR/modules/soc_tronitylp2/main.sh 2
+$OPENWBBASEDIR/modules/soc_tronity/main.sh 2
 exit 0
