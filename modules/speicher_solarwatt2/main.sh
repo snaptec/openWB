2f29c10514813e1a4fde64b309a5494640a26ae5
Flock82 <81387035+Flock82@users.noreply.github.com>
2021-04-20 21:38:36 +0200

    Solarwatt My Reserve Batterie+EVU via Gateway-Device

--- modules/speicher_solarwatt2/main.sh
+++ modules/speicher_solarwatt2/main.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
+MODULE="Speicher"
+LOGFILE="$RAMDISKDIR/openWB.log"
+Debug=$debug
+myPid=$$
+
+DebugLog(){
+	if (( Debug > 0 )); then
+		timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+		echo "$timestamp: ${MODULE}: $@" >> $LOGFILE
+	fi
+}
+
+
+sresponse=$(curl --connect-timeout 3 -s "http://${speicher1_ip}:8080/")
+
+ibat=$(echo $sresponse | jq '.FData.IBat') 
+vbat=$(echo $sresponse | jq '.FData.VBat') 
+speicherleistung=$(echo "($ibat * $vbat)" | bc) 
+speicherleistung=$(echo "scale=0; ($speicherleistung) / (-1)" | bc) 
+
+DebugLog "Speicherleistung: ${speicherleistung} W"
+echo $speicherleistung > /var/www/html/openWB/ramdisk/speicherleistung 
+
+
+speichersoc=$(echo $sresponse | jq '.SData.SoC' | sed 's/\..*$//') 
+
+DebugLog "SpeicherSoC: ${speichersoc} %"
+echo $speichersoc > /var/www/html/openWB/ramdisk/speichersoc 
