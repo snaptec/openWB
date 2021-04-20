3b62ee76219c6929359a5be549fda0a6ddb49d5f
Flock82 <81387035+Flock82@users.noreply.github.com>
2021-04-20 21:38:01 +0200

    Solarwatt My Reserve Batterie+EVU via Gateway-Device

--- modules/bezug_solarwatt2/main.sh
+++ modules/bezug_solarwatt2/main.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
+MODULE="EVU"
+LOGFILE="$RAMDISKDIR/openWB.log"
+Debug=$debug
+myPid=$$
 
+DebugLog(){
+	if (( Debug > 0 )); then
+		timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+		echo "$timestamp: ${MODULE}: $@" >> $LOGFILE
+	fi
+}
+
+sresponse=$(curl --connect-timeout 3 -s "http://${speicher1_ip}:8080/")
+
+bezugw=$(echo $sresponse | jq '.FData.PGrid' | sed 's/\..*$//')
+bezugwatt=$(echo "scale=0; $bezugw / 1" | bc)
+
+echo $bezugwatt > /var/www/html/openWB/ramdisk/wattbezug
+echo $bezugwatt
+DebugLog "Netzbezug: $bezugwatt W"
