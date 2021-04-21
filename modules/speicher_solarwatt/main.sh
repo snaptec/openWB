ca3d78d0c447af74e1484e32214e4fa2f2b60549
Flock82 <81387035+Flock82@users.noreply.github.com>
2021-04-21 11:56:41 +0200

    Alternative EVU+Batterie-Module für Solarwatt/MyReserve-Geräte mit Gateway

--- modules/speicher_solarwatt/main.sh
+++ modules/speicher_solarwatt/main.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-DMOD="MAIN"
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
+MODULE="Speicher"
+LOGFILE="$RAMDISKDIR/openWB.log"
 Debug=$debug
 
-openwbDebugLog ${DMOD} 1 "URL: ${speicher1_ip}/rest/kiwigrid/wizard/devices"
-sresponse=$(curl --connect-timeout 5 -s "http://${speicher1_ip}/rest/kiwigrid/wizard/devices")
-#sresponse=$(curl --connect-timeout 5 -s "http://192.168.178.64/rest/kiwigrid/wizard/devices")
-openwbDebugLog ${DMOD} 1 "Resp: $?"
-#pvwh=$(echo $sresponse | jq '.result.items | .[0].tagValues.WorkProduced.value')
-#echo "PV erzeugt $pvwh"
-#pvwatt=$(echo $sresponse | jq '.result.items | .[0].tagValues.PowerProduced.value')
-#echo "PV Leistung aktuellt $pvwatt"
-#bezugwh=$(echo $sresponse | jq '.result.items | .[0].tagValues.WorkConsumedFromGrid.value')
-#echo "Vom Netz bezogen Gesamt $bezugwh"
-#bezugw=$(echo $sresponse | jq '.result.items | .[0].tagValues.PowerConsumedFromGrid.value')
-#einspeisungw=$(echo $sresponse | jq '.result.items | .[0].tagValues.PowerOut.value')
-#bezugwatt=$(echo "scale=0; $bezugw - $einspeisungw /1" |bc)
-#echo "Bezug/Einspeisung am Übergabepunkt $bezugwatt"
-#einspeisungwh=$(echo $sresponse | jq '.result.items | .[0].tagValues.WorkOut.value')
-#echo "Ins Netz eingespeist Gesamt $einspeisungwh" 
+DebugLog(){
+	if (( Debug > 0 )); then
+		timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+		echo "$timestamp: ${MODULE}: $@" >> $LOGFILE
+	fi
+}
 
-speichere=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerConsumedFromStorage.value != null) | .tagValues.PowerConsumedFromStorage.value' | sed 's/\..*$//') 
-openwbDebugLog ${DMOD} 1 "Speichere: ${speichere}"
-speicherein=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerOutFromStorage.value != null) | .tagValues.PowerOutFromStorage.value' | sed 's/\..*$//') 
-openwbDebugLog ${DMOD} 1 "Speicherein: ${speicherein}"
-speicheri=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerBuffered.value != null) | .tagValues.PowerBuffered.value' | sed 's/\..*$//') 
-openwbDebugLog ${DMOD} 1 "Speicheri: ${speicheri}"
-#speicherleistung=$((speichere + speicherin - speicheri)) 
-speicherleistung=$(echo "scale=0; ($speichere + $speicherin - $speicheri) *-1" | bc) 
-openwbDebugLog ${DMOD} 1 "Speicherleistung: ${speicherleistung}"
+if (( $solarwattmethod == 0 )); then 	#Abruf über Energy Manager
+	sresponse=$(curl --connect-timeout 5 -s "http://${speicher1_ip}/rest/kiwigrid/wizard/devices")
+
+	speichere=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerConsumedFromStorage.value != null) | .tagValues.PowerConsumedFromStorage.value' | sed 's/\..*$//')
+	speicherein=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerOutFromStorage.value != null) | .tagValues.PowerOutFromStorage.value' | sed 's/\..*$//') 
+	speicheri=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerBuffered.value != null) | .tagValues.PowerBuffered.value' | sed 's/\..*$//') 
+
+	speicherleistung=$(echo "scale=0; ($speichere + $speicherin - $speicheri) *-1" | bc) 
+	speichersoc=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.StateOfCharge.value != null) | .tagValues.StateOfCharge.value' | sed 's/\..*$//') 
+fi
+
+if (( $solarwattmethod == 1 )); then 	#Abruf über Gateway
+	sresponse=$(curl --connect-timeout 3 -s "http://${speicher1_ip}:8080/")
+
+	ibat=$(echo $sresponse | jq '.FData.IBat') 
+	vbat=$(echo $sresponse | jq '.FData.VBat') 
+	speicherleistung=$(echo "($ibat * $vbat)" | bc) 
+	speicherleistung=$(echo "scale=0; ($speicherleistung) / (-1)" | bc)
+	speichersoc=$(echo $sresponse | jq '.SData.SoC' | sed 's/\..*$//')
+fi
+
+DebugLog "Speicherleistung: ${speicherleistung} W"
 echo $speicherleistung > /var/www/html/openWB/ramdisk/speicherleistung 
-#echo "Batterieladung/entladung $speicherleistung" 
-speichersoc=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.StateOfCharge.value != null) | .tagValues.StateOfCharge.value' | sed 's/\..*$//') 
-openwbDebugLog ${DMOD} 1 "SpeicherSoC: ${speichersoc}"
-#echo "Batterieladezustand $speichersoc" 
+
+DebugLog "SpeicherSoC: ${speichersoc} %"
 echo $speichersoc > /var/www/html/openWB/ramdisk/speichersoc 
