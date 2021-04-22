6afd9143400b57043750bbf89d149e7c66779559
Flock82 <81387035+Flock82@users.noreply.github.com>
2021-04-22 22:38:09 +0200

    Ignore empty readouts

--- modules/bezug_solarwatt/main.sh
+++ modules/bezug_solarwatt/main.sh
@@ -5,6 +5,7 @@ RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 MODULE="EVU"
 LOGFILE="$RAMDISKDIR/openWB.log"
 Debug=$debug
+BEZUGFILE="$RAMDISKDIR/wattbezug"
 
 DebugLog(){
 	if (( Debug > 0 )); then
@@ -18,24 +19,24 @@ if (( $solarwattmethod == 0 )); then 	#Abruf über Energy Manager
 	sresponse=$(curl --connect-timeout 3 -s "http://${speicher1_ip}/rest/kiwigrid/wizard/devices")
 
 	if ((${#sresponse}  < 10)); then
-		exit 1
+		bezugwatt=$(<$BEZUGFILE)
+	else
+		bezugw=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerConsumedFromGrid.value != null) | .tagValues.PowerConsumedFromGrid.value' | sed 's/\..*$//')
+		einspeisungw=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerOut.value != null) | .tagValues.PowerOut.value' | head -n 1 | sed 's/\..*$//')
+		bezugwatt=$(echo "scale=0; $bezugw - $einspeisungw /1" |bc)
 	fi
-	
-	bezugw=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerConsumedFromGrid.value != null) | .tagValues.PowerConsumedFromGrid.value' | sed 's/\..*$//')
-	einspeisungw=$(echo $sresponse | jq '.result.items | .[] | select(.tagValues.PowerOut.value != null) | .tagValues.PowerOut.value' | head -n 1 | sed 's/\..*$//')
-	bezugwatt=$(echo "scale=0; $bezugw - $einspeisungw /1" |bc)
 fi
 if (( $solarwattmethod == 1 )); then 	#Abruf über Gateway
 	sresponse=$(curl --connect-timeout 3 -s "http://${speicher1_ip}:8080/")
 
 	if ((${#sresponse}  < 10)); then
-		exit 1
+		bezugwatt=$(<$BEZUGFILE)
+	else
+		bezugw=$(echo $sresponse | jq '.FData.PGrid' | sed 's/\..*$//')
+		bezugwatt=$(echo "scale=0; $bezugw / 1" | bc)
 	fi
-
-	bezugw=$(echo $sresponse | jq '.FData.PGrid' | sed 's/\..*$//')
-	bezugwatt=$(echo "scale=0; $bezugw / 1" | bc)
 fi
 
 DebugLog "Netzbezug: $bezugwatt W"
-echo $bezugwatt > /var/www/html/openWB/ramdisk/wattbezug
+echo $bezugwatt > $BEZUGFILE
 echo $bezugwatt
