037d9b2a1e13358a7e7a7a298b41f7a29671f87c
eisenkoch <andreaskoch1@gmail.com>
2021-04-28 13:49:15 +0200

    added return for pvwatt

--- modules/wr_studer/main.sh
+++ modules/wr_studer/main.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo python /var/www/html/openWB/modules/wr_studer/studer_wr.py $studer_ip $studer_xt $studer_vc $studer_vc_type
+#sudo python /var/www/html/openWB/modules/wr_studer/studer_wr.py $studer_ip $studer_xt $studer_vc $studer_vc_type
+sudo python /var/www/html/openWB/modules/wr_studer/studer_wr.py 192.168.178.155 2 2 VS
 
-
+# Rückgabe des Wertes Gesamt-PV-Leistung
+pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt)
+echo $pvwatt
 
 
 
