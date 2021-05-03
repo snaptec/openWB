55fae75c13827ef61379bab8c7f03b6108ef285d
eisenkoch <andreaskoch1@gmail.com>
2021-05-03 07:38:51 +0200

    removed sudo

--- modules/wr_studer/main.sh
+++ modules/wr_studer/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo python /var/www/html/openWB/modules/wr_studer/studer_wr.py $studer_ip $studer_xt $studer_vc $studer_vc_type
+python /var/www/html/openWB/modules/wr_studer/studer_wr.py $studer_ip $studer_xt $studer_vc $studer_vc_type
 
 # Rückgabe des Wertes Gesamt-PV-Leistung
 pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt)
