81ce7bd264a5b7c913cff7c61b9a0a5b13e6977d
manuel <manuel.cargnel@rwth-aachen.de>
2021-05-15 16:18:55 +0200

    total power added for wr2_huawei module

--- modules/wr2_huawei/huawei.py
+++ modules/wr2_huawei/huawei.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 import sys
 import os
 import time
@@ -11,18 +12,26 @@ import binascii
 ipaddress = str(sys.argv[1])
 modbusID = str(sys.argv[2])
 
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-
-active_power = 1
+active_power = 0
+total_power = 0
 
 client = ModbusClient(ipaddress, port=502, unit_id=modbusID)
+
 client.connect()
 if client.connect():
     time.sleep(1)
     rr = client.read_holding_registers(0x7D50, 0x02)
+    time.sleep(5)
+    rw = client.read_holding_registers(0x7D6A, 0x02)
 
     active_power = rr.registers[1]
+    total_power = rw.registers[1]
+
 
 f = open('/var/www/html/openWB/ramdisk/pv2watt', 'w')
 f.write(str(active_power*-1))
+f.close()
+
+f = open('/var/www/html/openWB/ramdisk/pv2kwh', 'w')
+f.write(str(total_power*10.0))
 f.close()
--- modules/wr2_huawei/main.sh
+++ modules/wr2_huawei/main.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 python /var/www/html/openWB/modules/wr2_huawei/huawei.py $pv2ip $pv2id
-pvwatt=$(</var/www/html/openWB/ramdisk/pv2watt)
-echo $pvwatt
 
