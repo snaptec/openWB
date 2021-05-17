75d978c0c0ffdb1dbd21f964cca09a0b48816592
Lutz Bender <github@lutz-bender.de>
2021-05-17 12:35:58 +0200

    forced soc update if not plugged

--- loadvars.sh
+++ loadvars.sh
@@ -922,7 +922,9 @@ loadvars(){
 		socvorhanden=1
 		echo 1 > /var/www/html/openWB/ramdisk/socvorhanden
 		if (( stopsocnotpluggedlp1 == 1 )); then
-			if (( plugstat == 1 )); then
+			soctimer=$(</var/www/html/openWB/ramdisk/soctimer)
+			# if (( plugstat == 1 )); then
+			if [ $plugstat -eq 1 -o $soctimer -eq 20005 ]; then # force soc update button sends 20005
 				modules/$socmodul/main.sh &
 				soc=$(</var/www/html/openWB/ramdisk/soc)
 				tmpsoc=$(</var/www/html/openWB/ramdisk/tmpsoc)
