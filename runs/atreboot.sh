21e5121ea901f1f8d444578ad192772572401a15
tgreve85 <tgreve@stakotec.de>
2021-06-25 16:54:06 +0200

    Virtuelle IP-Adresse konfigurierbar
    Damit mehrere Boxen im Standalone-Betrieb auf das selbe EVU-Modul zugreifen können. Ansonsten gibt es einen IP-Adress-Konflikt

--- runs/atreboot.sh
+++ runs/atreboot.sh
@@ -156,9 +156,9 @@ fi
 echo "LAN/WLAN..."
 ethstate=$(</sys/class/net/eth0/carrier)
 if (( ethstate == 1 )); then
-	sudo ifconfig eth0:0 192.168.193.5 netmask 255.255.255.0 up
+	sudo ifconfig eth0:0 $virtual_ip_eth0_evu netmask 255.255.255.0 up
 else
-	sudo ifconfig wlan0:0 192.168.193.6 netmask 255.255.255.0 up
+	sudo ifconfig wlan0:0 $virtual_ip_wlan0_evu netmask 255.255.255.0 up
 fi
 
 # check for apache configuration
