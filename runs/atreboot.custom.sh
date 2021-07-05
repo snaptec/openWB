92829ad57bab1406dba1364accdac963fadc92fe
tgreve85 <tgreve@stakotec.de>
2021-07-05 09:05:35 +0200

    Create atreboot.custom.sh

---
+++ runs/atreboot.custom.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Hier können eigene Anweisungen eingefügt werden, welche nach einem Neustart ausgeführt werden sollen.
+# Beispiel: Ladepunkt 1 beim reboot sperren:
+# echo 0 > /var/www/html/openWB/ramdisk/lp1enabled
