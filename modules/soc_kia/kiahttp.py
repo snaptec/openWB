068e794d225f9ca397638e7e2f8f09bd0ea3676e
Flock82 <81387035+Flock82@users.noreply.github.com>
2021-07-10 20:06:24 +0200

    Add files via upload
    Update des Loginprozesses auf die neuen Server
    Modularisierung zwecks Übersichtlichkeit
    Reduktion der Ramdisk-Dateien

---
+++ modules/soc_kia/kiahttp.py
@@ -0,0 +1,117 @@
+import json
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
+import requests
+import soclogging
+
+lastCookies = {}
+lastUrl = ""
+
+def getHTTP(url = '', headers = '', cookies = '', timeout = 30):
+    global lastCookies
+    global lastUrl
+    
+    try:
+        response = requests.get(url, headers = headers, cookies = cookies, timeout = timeout)        
+    except requests.Timeout as err:
+        soclogging.logDebug(1, "Connection Timeout")
+        raise
+    except:
+        soclogging.logDebug(1, "HTTP Error")
+        raise
+    
+    if response.status_code == 200 or response.status_code == 204:
+        lastCookies = response.cookies.get_dict()
+        return response.text
+    else:
+        try:
+            responseDict = json.loads(response.text)
+            if response.status_code == 400 or response.status_code == 408 or response.status_code == 503:
+                errorString = "[" + responseDict['resCode'] + "] " + responseDict['resMsg']
+            else:
+                errorString = "[" + responseDict['errCode'] + "] " + responseDict['errMsg']
+        except:
+            errorString = "[XXXX] Unidentified Error" + " " + response.text
+        soclogging.logDebug(1, 'Request failed, StatusCode: ' + str(response.status_code) + ', Error: ' + errorString)
+        raise RuntimeError
+        
+    return
+
+def putHTTP(url = '', data = '', headers = '', cookies = '', timeout = 30):
+    try:
+        if isinstance(data, dict):
+            response = requests.put(url, json = data, headers = headers, cookies = cookies, timeout = timeout)
+        else:
+            response = requests.put(url, data = data, headers = headers, cookies = cookies, timeout = timeout)        
+    except requests.Timeout as err:
+        soclogging.logDebug(1, "Connection Timeout")
+        raise
+    except:
+        soclogging.logDebug(1, "HTTP Error")
+        raise
+
+    if response.status_code == 200 or response.status_code == 204:
+        return response.text
+    else:
+        try:
+            responseDict = json.loads(response.text)
+            if response.status_code == 408:
+                errorString = "[" + responseDict['resCode'] + "] " + responseDict['resMsg']
+            else:
+                errorString = "[" + responseDict['errCode'] + "] " + responseDict['errMsg']
+        except:
+            errorString = "[XXXX] Unidentified Error"
+        soclogging.logDebug(1, 'Request failed, StatusCode: ' + str(response.status_code) + ', Error: ' + errorString)
+        raise RuntimeError
+        
+    return
+    
+def postHTTP(url = '', data = '', headers = '', cookies = '', timeout = 30, allow_redirects = True):
+    global lastCookies
+    global lastUrl
+    
+    try:
+        if isinstance(data, dict):
+            response = requests.post(url, json = data, headers = headers, cookies = cookies, timeout = timeout, allow_redirects = allow_redirects)
+        else:
+            response = requests.post(url, data = data, headers = headers, cookies = cookies, timeout = timeout, allow_redirects = allow_redirects)        
+    except requests.Timeout as err:
+        soclogging.logDebug(1, "Connection Timeout")
+        raise
+    except:
+        soclogging.logDebug(1, "HTTP Error")
+        raise
+    
+    if response.status_code == 200 or response.status_code == 204:
+        lastUrl = response.url
+        return response.text
+    else:
+        try:
+            responseDict = json.loads(response.text)
+            if response.status_code == 408:
+                errorString = "[" + responseDict['resCode'] + "] " + responseDict['resMsg']
+            else:
+                errorString = "[" + responseDict['errCode'] + "] " + responseDict['errMsg']
+        except:
+            errorString = "[XXXX] Unidentified Error"
+        soclogging.logDebug(1, 'Request failed, StatusCode: ' + str(response.status_code) + ', Error: ' + errorString)
+        raise RuntimeError
+        
+    return
+
+def getHTTPCookies(url):
+    try:
+        session = requests.Session()
+        response = session.get(url)
+    except requests.Timeout as err:
+        soclogging.logDebug(1, "Connection Timeout")
+        raise    
+    
+    if response.status_code == 200:
+        cookies = session.cookies.get_dict()
+    else:
+        soclogging.logDebug(1, "Receiving cookies failed, StatusCode: " + str(response.status_code))
+        raise
+        
+    return cookies
---
+++ modules/soc_kia/main.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SOCMODULE="kia"
+
+MODULEDIR=$(cd `dirname $0` && pwd)
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
+LOGFILE="$RAMDISKDIR/soc.log"
+CHARGEPOINT=$1
+DEBUGLEVEL=$debug
+
+
+case $CHARGEPOINT in
+	2)
+		# second charge point
+		kia_email=$soc2user
+		kia_password=$soc2pass
+		kia_pin=$soc2pin
+		kia_vin=$soc2vin
+		kia_intervall=$soc2intervall
+		soccalc=$kia_soccalclp2
+		akkug=$akkuglp2
+		efficiency=$wirkungsgradlp2
+		;;
+	*)
+		# defaults to first charge point for backward compatibility
+		# set CHARGEPOINT in case it is empty (needed for soclogging)
+		CHARGEPOINT=1
+		kia_email=$soc_bluelink_email
+		kia_password=$soc_bluelink_password
+		kia_pin=$soc_bluelink_pin
+		kia_vin=$soc_vin
+		kia_intervall=$soc_bluelink_interval
+		soccalc=$kia_soccalclp1
+		akkug=$akkuglp1
+		efficiency=$wirkungsgradlp1
+		;;
+esac
+
+ARGSFILE="$RAMDISKDIR/soc_${SOCMODULE}_lp${CHARGEPOINT}_args"
+
+ARGS='{'
+ARGS+='"moduleName": "'"$SOCMODULE"'", '
+ARGS+='"chargePoint": "'"$CHARGEPOINT"'", '
+ARGS+='"accountName": "'"$kia_email"'", '
+ARGS+='"accountPassword": "'"$kia_password"'", '
+ARGS+='"accountPin": "'"$kia_pin"'", '
+ARGS+='"vehicleVin": "'"$kia_vin"'", '
+ARGS+='"timerInterval": "'"$kia_intervall"'", '
+ARGS+='"manualCalc": "'"$soccalc"'", '
+ARGS+='"batterySize": "'"$akkug"'", '
+ARGS+='"efficiency": "'"$efficiency"'", '
+ARGS+='"ramDiskDir": "'"$RAMDISKDIR"'", '
+ARGS+='"debugLevel": "'"$DEBUGLEVEL"'"'
+ARGS+='}'
+
+echo $ARGS > $ARGSFILE
+
+sudo python3 $MODULEDIR/main.py $ARGSFILE &>> $LOGFILE &
