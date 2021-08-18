fb358ae0852f714bb5fea2890520c3efbbaf2007
LKuemmel <lena.kuemmel@openwb.de>
2021-08-18 08:50:48 +0200

    undo go e changes

--- modules/goelp3/main.sh
+++ modules/goelp3/main.sh
@@ -1,60 +1,59 @@
-#!/bin/bash		 #!/bin/bash
- re='^-?[0-9]+$'		
- rekwh='^[-+]?[0-9]+\.?[0-9]*$'		
+#!/bin/bash
+re='^-?[0-9]+$'
+rekwh='^[-+]?[0-9]+\.?[0-9]*$'
 
- 
-  output=$(curl --connect-timeout $goetimeoutlp3 -s http://$goeiplp3/status)		 python3 /var/www/html/openWB/modules/bezug_goelp1/go_e.py 3 $goeiplp3 $goetimeoutlp3 
- if [[ $? == "0" ]] ; then		
- 	watt=$(echo $output | jq -r '.nrg[11]')		
- 	watt=$(echo "scale=0;$watt * 10 /1" |bc)		
- 	if [[ $watt =~ $re ]] ; then		
- 		echo $watt > /var/www/html/openWB/ramdisk/llaktuells2		
- 	fi		
- 	lla1=$(echo $output | jq -r '.nrg[4]')		
- 	lla1=$(echo "scale=0;$lla1 / 10" |bc)		
- 	if [[ $lla1 =~ $re ]] ; then		
- 		echo $lla1 > /var/www/html/openWB/ramdisk/llas21		
- 	fi		
- 	lla2=$(echo $output | jq -r '.nrg[5]')		
- 	lla2=$(echo "scale=0;$lla2 / 10" |bc)		
- 	if [[ $lla2 =~ $re ]] ; then		
- 		echo $lla2 > /var/www/html/openWB/ramdisk/llas22		
- 	fi		
- 	lla3=$(echo $output | jq -r '.nrg[6]')		
- 	lla3=$(echo "scale=0;$lla3 / 10" |bc)		
- 	if [[ $lla3 =~ $re ]] ; then		
- 		echo $lla3 > /var/www/html/openWB/ramdisk/llas23		
- 	fi		
- 	llv1=$(echo $output | jq -r '.nrg[0]')		
- 	if [[ $llv1 =~ $re ]] ; then		
- 		echo $llv1 > /var/www/html/openWB/ramdisk/llvs21		
- 	fi		
- 	llv2=$(echo $output | jq -r '.nrg[1]')		
- 	if [[ $llv2 =~ $re ]] ; then		
- 		echo $llv2 > /var/www/html/openWB/ramdisk/llvs22		
- 	fi		
- 	llv3=$(echo $output | jq -r '.nrg[2]')		
- 	if [[ $llv3 =~ $re ]] ; then		
- 		echo $llv3 > /var/www/html/openWB/ramdisk/llvs23		
- 	fi		
- 	llkwh=$(echo $output | jq -r '.eto')		
- 	llkwh=$(echo "scale=3;$llkwh / 10" |bc)		
- 	if [[ $llkwh =~ $rekwh ]] ; then		
- 		echo $llkwh > /var/www/html/openWB/ramdisk/llkwhs2		
- 	fi		
- 	#car status 1 Ladestation bereit, kein Auto		
- 	#car status 2 Auto lädt		
- 	#car status 3 Warte auf Fahrzeug		
- 	#car status 4 Ladung beendet, Fahrzeug verbunden		
- 	car=$(echo $output | jq -r '.car')		
- 	if [[ $car == "1" ]] ; then		
- 		echo 0 > /var/www/html/openWB/ramdisk/plugstatlp3		
- 	else		
- 		echo 1 > /var/www/html/openWB/ramdisk/plugstatlp3		
- 	fi		
- 	if [[ $car == "2" ]] ; then		
- 		echo 1 > /var/www/html/openWB/ramdisk/chargestatlp3		
- 	else		
- 		echo 0 > /var/www/html/openWB/ramdisk/chargestatlp3		
- 	fi		
- fi		
+output=$(curl --connect-timeout $goetimeoutlp3 -s http://$goeiplp3/status)
+if [[ $? == "0" ]] ; then
+	watt=$(echo $output | jq -r '.nrg[11]')
+	watt=$(echo "scale=0;$watt * 10 /1" |bc)
+	if [[ $watt =~ $re ]] ; then
+		echo $watt > /var/www/html/openWB/ramdisk/llaktuells2
+	fi
+	lla1=$(echo $output | jq -r '.nrg[4]')
+	lla1=$(echo "scale=0;$lla1 / 10" |bc)
+	if [[ $lla1 =~ $re ]] ; then
+		echo $lla1 > /var/www/html/openWB/ramdisk/llas21
+	fi
+	lla2=$(echo $output | jq -r '.nrg[5]')
+	lla2=$(echo "scale=0;$lla2 / 10" |bc)
+	if [[ $lla2 =~ $re ]] ; then
+		echo $lla2 > /var/www/html/openWB/ramdisk/llas22
+	fi
+	lla3=$(echo $output | jq -r '.nrg[6]')
+	lla3=$(echo "scale=0;$lla3 / 10" |bc)
+	if [[ $lla3 =~ $re ]] ; then
+		echo $lla3 > /var/www/html/openWB/ramdisk/llas23
+	fi
+	llv1=$(echo $output | jq -r '.nrg[0]')
+	if [[ $llv1 =~ $re ]] ; then
+		echo $llv1 > /var/www/html/openWB/ramdisk/llvs21
+	fi
+	llv2=$(echo $output | jq -r '.nrg[1]')
+	if [[ $llv2 =~ $re ]] ; then
+		echo $llv2 > /var/www/html/openWB/ramdisk/llvs22
+	fi
+	llv3=$(echo $output | jq -r '.nrg[2]')
+	if [[ $llv3 =~ $re ]] ; then
+		echo $llv3 > /var/www/html/openWB/ramdisk/llvs23
+	fi
+	llkwh=$(echo $output | jq -r '.eto')
+	llkwh=$(echo "scale=3;$llkwh / 10" |bc)
+	if [[ $llkwh =~ $rekwh ]] ; then
+		echo $llkwh > /var/www/html/openWB/ramdisk/llkwhs2
+	fi
+	#car status 1 Ladestation bereit, kein Auto
+	#car status 2 Auto lädt
+	#car status 3 Warte auf Fahrzeug
+	#car status 4 Ladung beendet, Fahrzeug verbunden
+	car=$(echo $output | jq -r '.car')
+	if [[ $car == "1" ]] ; then
+		echo 0 > /var/www/html/openWB/ramdisk/plugstatlp3
+	else
+		echo 1 > /var/www/html/openWB/ramdisk/plugstatlp3
+	fi
+	if [[ $car == "2" ]] ; then
+		echo 1 > /var/www/html/openWB/ramdisk/chargestatlp3
+	else
+		echo 0 > /var/www/html/openWB/ramdisk/chargestatlp3
+	fi
+fi
