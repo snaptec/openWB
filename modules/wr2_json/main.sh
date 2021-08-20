45484fb6d427c84b8d40a8c251ad2d82f63bd75c
DetMoerk <76627235+DetMoerk@users.noreply.github.com>
2021-08-20 16:22:27 +0200

    Generic JSON Modules in Python
    From Shell -- Python3.
    
    Requires python jq for python3. Should be present, as it is still used
    Be careful with URLS, that don't contain a http://. Worked in curl but doesn't work in Python request.

--- modules/wr2_json/main.sh
+++ modules/wr2_json/main.sh
@@ -5,39 +5,75 @@ RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 MODULEDIR=$(cd `dirname $0` && pwd)
 DMOD="PV"
 #DMOD="MAIN"
-#LOGFILE="$RAMDISKDIR/pv_wr.log"
-#LOGFILE="$RAMDISKDIR/openWB.log"
 Debug=$debug
 
 #For Development only
 #Debug=1
 
-re='^[-+]?[0-9]+\.?[0-9]*$'
-answer=$(curl --connect-timeout 5 -s $wr2jsonurl)
-pvwatt=$(echo $answer | jq -r "$wr2jsonwatt" | sed 's/\..*$//')
-# Wenn WR aus bzw. im Standby (keine Antwort), ersetze leeren Wert durch eine 0
-if ! [[ $pvwatt =~ $re ]] ; then
-	openwbDebugLog ${DMOD} 1 "PV2Watt Not Numeric: $pvwatt . Check if Filter is correct or WR is in standby"
-	pvwatt=0
-fi
-
-if (( $pvwatt > 5 )); then
-	pvwatt=$(echo "$pvwatt*-1" |bc)
-fi
-openwbDebugLog ${DMOD} 1 "PV2Watt: ${pvwatt}"
-echo ${pvwatt}
-echo $pvwatt > /var/www/html/openWB/ramdisk/pv2watt
-
-if [ ! -z "$wr2jsonkwh" ]; then
-	pvkwh=$(echo $answer | jq -r "$wr2jsonkwh")
-	if ! [[ $pvkwh =~ $re ]] ; then
-		openwbDebugLog ${DMOD} 1 "PV2kWh Not Numeric: $pvkwh . Check if Filter is correct or WR is in standby"
-		pvkwh=$(</var/www/html/openWB/ramdisk/pv2kwh)
-	fi
+if [ $DMOD == "MAIN" ]; then
+	MYLOGFILE="$RAMDISKDIR/openWB.log"
 else
-	openwbDebugLog ${DMOD} 2 "PV2kWh NoFilter is set"
-	pvkwh=0
+	MYLOGFILE="$RAMDISKDIR/nurpv.log"
 fi
 
-openwbDebugLog ${DMOD} 1 "PV2kWh: ${pvkwh}"
-echo $pvkwh > /var/www/html/openWB/ramdisk/pv2kwh
+
+
+
+openwbDebugLog ${DMOD} 2 "PV URL : ${wr2jsonurl}"
+openwbDebugLog ${DMOD} 2 "PV Watt: ${wr2jsonwatt}"
+openwbDebugLog ${DMOD} 2 "PV kWh : ${wr2jsonkwh}"
+
+python3 $OPENWBBASEDIR/modules/wr_json/read_json.py "${wr2jsonurl}" "${wr2jsonwatt}" "${wr2jsonkwh}" "2" >>$MYLOGFILE 2>&1
+ret=$?
+
+openwbDebugLog ${DMOD} 2 "RET: ${ret}"
+
+watt=$(<$RAMDISKDIR/pvwatt2)
+echo ${watt}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#re='^[-+]?[0-9]+\.?[0-9]*$'
+#answer=$(curl --connect-timeout 5 -s $wrjsonurl)
+#pvwatt=$(echo $answer | jq -r "$wrjsonwatt" | sed 's/\..*$//')
+## Wenn WR aus bzw. im Standby (keine Antwort), ersetze leeren Wert durch eine 0
+#if ! [[ $pvwatt =~ $re ]] ; then
+#	openwbDebugLog ${DMOD} 1 "PVWatt Not Numeric: $pvwatt . Check if Filter is correct or WR is in standby"
+#	pvwatt=0
+#fi
+#
+#if (( $pvwatt > 5 )); then
+#	pvwatt=$(echo "$pvwatt*-1" |bc)
+#fi	
+#openwbDebugLog ${DMOD} 1 "PVWatt: ${pvwatt}"
+#echo ${pvwatt}
+#echo $pvwatt > /var/www/html/openWB/ramdisk/pvwatt
+#
+#if [ ! -z "$wrjsonkwh" ]; then
+#	pvkwh=$(echo $answer | jq -r "$wrjsonkwh")
+#	if ! [[ $pvkwh =~ $re ]] ; then
+#		openwbDebugLog ${DMOD} 1 "PVkWh Not Numeric: $pvkwh . Check if Filter is correct or WR is in standby"
+#		pvkwh=$(</var/www/html/openWB/ramdisk/pvkwh)
+#	fi
+#else
+#	openwbDebugLog ${DMOD} 2 "PVkWh NoFilter is set"
+#	pvkwh=0
+#fi
+#
+#openwbDebugLog ${DMOD} 1 "PVkWh: ${pvkwh}"
+#echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwh
