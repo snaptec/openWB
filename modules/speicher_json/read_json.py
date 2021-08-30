5adbd4ca11fc85307d75f84880ccb4765e1cec65
Det <76627235+DetMoerk@users.noreply.github.com>
2021-08-30 17:03:56 +0200

    Py jq fix for speicher_json (#1546)
    * fix isnumeric
    
    as isnumeric is not working for negative numbers, I changed to regexp matching.
    
    Hope that works now

--- modules/speicher_json/read_json.py
+++ modules/speicher_json/read_json.py
@@ -4,11 +4,14 @@ import requests
 import sys
 import traceback
 import jq
+import re
 from datetime import datetime, timezone
 import os
 
 Debug         = int(os.environ.get('debug'))
 myPid         = str(os.getpid())
+
+renumeric ='^-?[0-9]+$'
 
 battjsonurl = str(sys.argv[1])
 battjsonwatt = str(sys.argv[2])
@@ -19,6 +22,9 @@ def DebugLog(message):
 	print(local_time.strftime(format = "%Y-%m-%d %H:%M:%S") + ": PID: "+ myPid +": " + message)
 
 
+numcheck = re.compile(renumeric)
+
+
 response = requests.get(battjsonurl, timeout=5).json()
 if Debug>=2:
 	DebugLog(str(response))
@@ -26,7 +32,7 @@ try:
 	speicherleistung = jq.compile(battjsonwatt).input(response).first()
 	if Debug>=1:
 		DebugLog('Speicherleistung: ' + str(speicherleistung))
-	if not str(speicherleistung).isnumeric():
+	if not numcheck.match(str(speicherleistung)):
 		DebugLog('Speicherleistung nicht numerisch. Bitte Filterausdruck ueberpruefen -->0')
 		speicherleistung=0
 	speicherleistung = int(speicherleistung)
@@ -40,7 +46,7 @@ try:
 		battsoc = jq.compile(battjsonsoc).input(response).first()
 		if Debug>=1:
 			DebugLog('SpeicherSoC: ' + str(battsoc))
-		if not str(battsoc).isnumeric():
+		if not numcheck.match(str(battsoc)):
 			DebugLog('SpeicherSoc nicht numerisch. Bitte Filterausdruck ueberpruefen -->0')
 			battsoc=0
 		battsoc = int(battsoc)
