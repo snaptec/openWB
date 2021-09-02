93e5f0ced63f89cdd5742a3355270bbb97f9036a
skl77 <skl@velmede-bestwig.de>
2021-09-02 19:08:27 +0200

    nächste Version der Überarbeitung (#1554)
    * alpha version - Übarbeitung, auslagerung in Klassen, Remote Debugging
    
    * überarbeitete read_kostalplenticore.py
    
    * update plenticore module, klassenbasierte Version
    
    * weitere Klassen abstraktion
    
    * weitere Klassen abstraktion
    
    * Erweiterung um 3. Umrichter
    
    * nächste Version Plenticor 3 Umrichter und ausbaufähig
    
    * nächste Version Plenticor 3 Umrichter und ausbaufähig
    
    * 5 Wechselrichter moeglich
    
    * ipparser Funktion entfernt
    
    * openwb-install rückgängig machen
    
    * bug fix
    
    * change ramdisk variables
    
    * support von ipparser

--- modules/wr_plenticore/main.sh
+++ modules/wr_plenticore/main.sh
@@ -8,14 +8,16 @@
 #
 # 2019 Michael Ortenstein
 # This file is part of openWB
-#
+# 
+# 01.09.2021	skl	Python3 , Anpassung auf neue .py
 #########################################################
 
-# Aufruf der Leseroutine mit den IP WR 1 und ggf. WR 2
+# Aufruf der Leseroutine mit den IP WR 1 und ggf. WR 2 und WR3
+# WR3 kann auch eine Liste an einanderfolgenden IP haben 
 if [[ $speichermodul == "speicher_kostalplenticore" ]]; then
-	sudo python /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2 1 $kostalplenticoreip3
+	sudo python3 /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2 1 $kostalplenticoreip3
 else
-	sudo python /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2 0 $kostalplenticoreip3
+	sudo python3 /var/www/html/openWB/modules/wr_plenticore/read_kostalplenticore.py $kostalplenticoreip $kostalplenticoreip2 0 $kostalplenticoreip3
 fi
 
 # Rückgabe des Wertes Gesamt-PV-Leistung
