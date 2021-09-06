f7243c1ce07bdc7ea39f0332199db39f079731a3
philipp123ow <89248773+philipp123ow@users.noreply.github.com>
2021-09-06 14:20:34 +0200

    Korrekturfaktor und Simulation Parametrisiert
    Implementierung Korrekturfaktor und Simulation nur wenn Parametrisiert

--- modules/goelp1/main.sh
+++ modules/goelp1/main.sh
@@ -4,10 +4,14 @@ rekwh='^[-+]?[0-9]+\.?[0-9]*$'
 
 output=$(curl --connect-timeout $goetimeoutlp1 -s http://$goeiplp1/status)
 if [[ $? == "0" ]] ; then
+	goecorrectionfactor=$goecorrectionfactorlp1
+	goecorrectionfactor=$(echo "scale=0;$goecorrectionfactor * 100000 /1" |bc)
 	watt=$(echo $output | jq -r '.nrg[11]')
 	watt=$(echo "scale=0;$watt * 10 /1" |bc)
 	if [[ $watt =~ $re ]] ; then
-		echo $watt > /var/www/html/openWB/ramdisk/llaktuell
+		wattc=$((watt*$goecorrectionfactor/100000))
+		wattc=$(echo "scale=0;$wattc" |bc)
+		echo $wattc > /var/www/html/openWB/ramdisk/llaktuell
 	fi
 	lla1=$(echo $output | jq -r '.nrg[4]')
 	lla1=$(echo "scale=0;$lla1 / 10" |bc)
@@ -38,49 +42,44 @@ if [[ $? == "0" ]] ; then
 	fi
 	llkwh=$(echo $output | jq -r '.eto')
 	llkwh=$(echo "scale=3;$llkwh / 10" |bc)
-	#if [[ $llkwh =~ $rekwh ]] ; then
-	#	echo $llkwh > /var/www/html/openWB/ramdisk/llkwh
-	#fi
 	rfid=$(echo $output | jq -r '.uby')
 	oldrfid=$(</var/www/html/openWB/ramdisk/tmpgoelp1rfid)
 	if [[ $rfid != $oldrfid ]] ; then
 		echo $rfid > /var/www/html/openWB/ramdisk/readtag
 		echo $rfid > /var/www/html/openWB/ramdisk/tmpgoelp1rfid
 	fi
-
 	#simulation der Energiemenge während des ladens
 	#wenn die Dateien noch nicht da sind, werden sie angelegt sobald das Auto nicht angesteckt ist.
 	if [ -f "/var/www/html/openWB/ramdisk/goewatt0neg" ]; then
 		if [ -f "/var/www/html/openWB/ramdisk/goewatt0pos" ]; then
-			python /var/www/html/openWB/runs/simcount.py $watt goe goeposkwh goenegkwh
+			python /var/www/html/openWB/runs/simcount.py $wattc goe goeposkwh goenegkwh
 		fi
 	fi
-
 	#car status 1 Ladestation bereit, kein Auto
 	#car status 2 Auto lädt
 	#car status 3 Warte auf Fahrzeug
 	#car status 4 Ladung beendet, Fahrzeug verbunden
 	car=$(echo $output | jq -r '.car')
-	if [[ $car == "1" ]] ; then
-		echo 0 > /var/www/html/openWB/ramdisk/plugstat
-		#wenn das Auto nicht angesteckt ist, wird der simulierte Zählerstand mit dem ausgelesenen Zählerstand überschrieben
-		#Damit wird die Simulation wieder mit dem Zähler des Go-E abgeglichen, die Nachkommestellen gehen allerdings verloren.
-		echo 0 > /var/www/html/openWB/ramdisk/goewatt0neg
+	if [[ $goesimulationlp1 == "0" ]] ; then
 		if [[ $llkwh =~ $rekwh ]] ; then
 			echo $llkwh > /var/www/html/openWB/ramdisk/llkwh
-			simenergy=$(echo "scale=0; $(</var/www/html/openWB/ramdisk/llkwh)*3600000/1" | bc)
-			echo $simenergy > /var/www/html/openWB/ramdisk/goewatt0pos
 		fi
 	else
+		if [[ $car != "1" ]] ; then
+			#der ausgelesene Zählerstand wird ignoriert und stattdessen die Leistung aufintegriert
+			#Grund: der ausgelesene Zählerstand hat eine Auflösung von 1kWh -> zu ungenau in der Darstellung
+			if [ -f "/var/www/html/openWB/ramdisk/goeposkwh" ]; then
+				simenergy=$(echo "scale=3; $(</var/www/html/openWB/ramdisk/goeposkwh)/1000" | bc)
+				echo $simenergy > /var/www/html/openWB/ramdisk/llkwh
+			else
+				echo $llkwh > /var/www/html/openWB/ramdisk/llkwh
+			fi
+		fi
+	fi
+	if [[ $car == "1" ]] ; then
+		echo 0 > /var/www/html/openWB/ramdisk/plugstat
+	else
 		echo 1 > /var/www/html/openWB/ramdisk/plugstat
-		#wenn das Auto angesteckt ist, wird der ausgelesene Zählerstand ignoriert und stattdessen die Leistung aufintegriert
-		#Grund: der ausgelesene Zählerstand hat eine Auflösung von 1kWh -> zu ungenau in der Darstellung
-        if [ -f "/var/www/html/openWB/ramdisk/goeposkwh" ]; then
-		    simenergy=$(echo "scale=3; $(</var/www/html/openWB/ramdisk/goeposkwh)/1000" | bc)
-		    echo $simenergy > /var/www/html/openWB/ramdisk/llkwh
-        else
-            echo $llkwh > /var/www/html/openWB/ramdisk/llkwh
-        fi
 	fi
 	if [[ $car == "2" ]] ; then
 		echo 1 > /var/www/html/openWB/ramdisk/chargestat
