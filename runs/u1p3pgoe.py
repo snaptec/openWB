10053d90764e4ab3e84c87b95fdf5a37658e5500
Matthias Moßner <matze.mpc@gmx.de>
2021-11-06 12:49:44 +0100

    u1p3pgoe.py - PEP8 corrections

--- runs/u1p3pgoe.py
+++ runs/u1p3pgoe.py
@@ -14,39 +14,39 @@ parser.add_argument("-v", "--verbose", required=False, action="store_true", help
 args = parser.parse_args()
 
 if(args.verbose):
-    print("Go-e mit IP %s Umschaltung auf %d Phasen"%(args.address, args.phases))
-	
-status_goe = requests.get('http://'+args.address+'/status', timeout = 5).json()	
+    print("Go-e mit IP %s Umschaltung auf %d Phasen" % (args.address, args.phases))
+
+status_goe = requests.get('http://'+args.address+'/status', timeout=5).json()
 if(args.verbose):
-	try:
-		print  ("go-e serial number: %s"%(status_goe['sse']))
-	except:
-		traceback.print_exc()
-		exit(1)	
+    try:
+        print("go-e serial number: %s" % (status_goe['sse']))
+    except:
+        traceback.print_exc()
+        exit(1)
 
 # check whether fsp param exists => go-e charger has HW V3 and therefore 1to3 phase switch capability
-if ( "fsp" in status_goe):
-	try:
-		if(args.verbose):
-			print  ("Phaseneinstellung fsp vorher: %d"%(int(status_goe['fsp'])))
-		if ( args.phases == 1 and int(status_goe['fsp']) != 1):
-			set_fsp_goe = requests.get('http://'+args.address+'/mqtt?payload=fsp=1', timeout = 5).json()
-			#Using API V2 the respective call would be /api/set?psm=1
-			if (int(set_fsp_goe['fsp']) == 1 and args.verbose):
-				print ("Umschaltung auf 1 Phase erfolgreich: fsp=%d"%(int(set_fsp_goe['fsp'])))
-		if ( args.phases == 3 and int(status_goe['fsp']) != 0 ):
-			if (args.minampere and args.minampere >= 5 and args.minampere <= 32):
-				set_amx_goe = requests.get('http://'+args.address+'/mqtt?payload=amx='+str(args.minampere), timeout = 5).json()
-				#Using API V2 the respective call would be /api/set?ama=args.minampere
-				if (int(set_amx_goe['amx']) == args.minampere and args.verbose):
-					print ("Setzen von MinAmpere erfolgreich: amx=%d"%(int(set_amx_goe['amx'])))
-			set_fsp_goe = requests.get('http://'+args.address+'/mqtt?payload=fsp=0', timeout = 5).json()
-			#Using API V2 the respective call would be /api/set?psm=2
-			if (int(set_fsp_goe['fsp']) == 0 and args.verbose):
-				print ("Umschaltung auf 3 Phasen erfolgreich: fsp=%d"%(int(set_fsp_goe['fsp'])))
-	except:
-		traceback.print_exc()
-		exit(1)
-else :
-	if(args.verbose):
-		print ("Phasenumschaltung von go-e Charger nicht unterstuetzt (V2 HW ?!)")
+if ("fsp" in status_goe):
+    try:
+        if(args.verbose):
+            print("Phaseneinstellung fsp vorher: %d" % (int(status_goe['fsp'])))
+        if (args.phases == 1 and int(status_goe['fsp']) != 1):
+            set_fsp_goe = requests.get('http://'+args.address+'/mqtt?payload=fsp=1', timeout=5).json()
+            # Using API V2 the respective call would be /api/set?psm=1
+            if (int(set_fsp_goe['fsp']) == 1 and args.verbose):
+                print("Umschaltung auf 1 Phase erfolgreich: fsp=%d" % (int(set_fsp_goe['fsp'])))
+        if (args.phases == 3 and int(status_goe['fsp']) != 0):
+            if (args.minampere and args.minampere >= 5 and args.minampere <= 32):
+                set_amx_goe = requests.get('http://'+args.address+'/mqtt?payload=amx='+str(args.minampere), timeout=5).json()
+                # Using API V2 the respective call would be /api/set?ama=args.minampere
+                if (int(set_amx_goe['amx']) == args.minampere and args.verbose):
+                    print("Setzen von MinAmpere erfolgreich: amx=%d" % (int(set_amx_goe['amx'])))
+            set_fsp_goe = requests.get('http://'+args.address+'/mqtt?payload=fsp=0', timeout=5).json()
+            # Using API V2 the respective call would be /api/set?psm=2
+            if (int(set_fsp_goe['fsp']) == 0 and args.verbose):
+                print("Umschaltung auf 3 Phasen erfolgreich: fsp=%d" % (int(set_fsp_goe['fsp'])))
+    except:
+        traceback.print_exc()
+        exit(1)
+else:
+    if(args.verbose):
+        print("Phasenumschaltung von go-e Charger nicht unterstuetzt (V2 HW ?!)")
