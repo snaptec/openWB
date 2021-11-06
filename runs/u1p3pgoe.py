77d48d094e5ea87159469722b8db7115c698f69f
Matthias Moßner <matze.mpc@gmx.de>
2021-11-06 11:51:57 +0100

    goe u1p3p: set minampere BEFORE fsp cause switching phases takes some seconds and inbetween no amx setting is allowed

--- runs/u1p3pgoe.py
+++ runs/u1p3pgoe.py
@@ -35,15 +35,15 @@ if ( "fsp" in status_goe):
 			if (int(set_fsp_goe['fsp']) == 1 and args.verbose):
 				print ("Umschaltung auf 1 Phase erfolgreich: fsp=%d"%(int(set_fsp_goe['fsp'])))
 		if ( args.phases == 3 and int(status_goe['fsp']) != 0 ):
-			set_fsp_goe = requests.get('http://'+args.address+'/mqtt?payload=fsp=0', timeout = 5).json()
-			#Using API V2 the respective call would be /api/set?psm=2
-			if (int(set_fsp_goe['fsp']) == 0 and args.verbose):
-				print ("Umschaltung auf 3 Phasen erfolgreich: fsp=%d"%(int(set_fsp_goe['fsp'])))
 			if (args.minampere and args.minampere >= 5 and args.minampere <= 32):
 				set_amx_goe = requests.get('http://'+args.address+'/mqtt?payload=amx='+str(args.minampere), timeout = 5).json()
 				#Using API V2 the respective call would be /api/set?ama=args.minampere
 				if (int(set_amx_goe['amx']) == args.minampere and args.verbose):
 					print ("Setzen von MinAmpere erfolgreich: amx=%d"%(int(set_amx_goe['amx'])))
+			set_fsp_goe = requests.get('http://'+args.address+'/mqtt?payload=fsp=0', timeout = 5).json()
+			#Using API V2 the respective call would be /api/set?psm=2
+			if (int(set_fsp_goe['fsp']) == 0 and args.verbose):
+				print ("Umschaltung auf 3 Phasen erfolgreich: fsp=%d"%(int(set_fsp_goe['fsp'])))
 	except:
 		traceback.print_exc()
 		exit(1)
