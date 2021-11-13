fc59fd1c58692711acc1d58915eba587a0c4aa0b
Matthias Moßner <matze.mpc@gmx.de>
2021-11-13 16:27:15 +0100

    u1p3pgoe: fix change to APIv2 *argl* III

--- runs/u1p3pgoe.py
+++ runs/u1p3pgoe.py
@@ -32,15 +32,15 @@ if ("fsp" in status_goe):
         if (args.phases == 1 and int(status_goe['fsp']) != 1):
             set_fsp_goe = requests.get('http://'+args.address+'/api/set?psm=1', timeout=5).json()
             if (int(set_fsp_goe['psm']) == 1 and args.verbose):
-                print("Umschaltung auf 1 Phase erfolgreich: fsp=%d" % (int(set_fsp_goe['fsp'])))
+                print("Umschaltung auf 1 Phase erfolgreich: psm=%d" % (int(set_fsp_goe['fsp'])))
         if (args.phases == 3 and int(status_goe['fsp']) != 0):
             if (args.minampere and args.minampere >= 5 and args.minampere <= 32):
                 set_amx_goe = requests.get('http://'+args.address+'/api/set?amp='+str(args.minampere), timeout=5).json()
                 if (int(set_amx_goe['amp']) == args.minampere and args.verbose):
-                    print("Setzen von MinAmpere erfolgreich: amx=%d" % (int(set_amx_goe['amx'])))
+                    print("Setzen von MinAmpere erfolgreich: amp=%d" % (int(set_amx_goe['amx'])))
             set_fsp_goe = requests.get('http://'+args.address+'/api/set?psm=2', timeout=5).json()
             if (int(set_fsp_goe['psm']) == 0 and args.verbose):
-                print("Umschaltung auf 3 Phasen erfolgreich: fsp=%d" % (int(set_fsp_goe['fsp'])))
+                print("Umschaltung auf 3 Phasen erfolgreich: psm=%d" % (int(set_fsp_goe['fsp'])))
     except:
         traceback.print_exc()
         exit(1)
