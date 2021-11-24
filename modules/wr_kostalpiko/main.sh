d60af5b248e8918e25e349717f64a62e5a4c0466
Frank <frankortmann80@gmail.com>
2021-11-24 16:38:28 +0100

    Anzeige des Gesamtwerte von Kostal Piko

--- modules/wr_kostalpiko/main.sh
+++ modules/wr_kostalpiko/main.sh
@@ -6,20 +6,18 @@ if [[ "$speichermodul" != "none" ]]; then
 else
 	pvwatttmp=$(curl --connect-timeout 5 -s $wrkostalpikoip/api/dxs.json?dxsEntries=67109120'&'dxsEntries=251658753)
 fi
-
 # aktuelle Ausgangsleistung am WR [W]
 pvwatt=$(echo $pvwatttmp | jq '.dxsEntries[0].value' | sed 's/\..*$//')
 if [ $pvwatt > 5 ]
 	then
 	pvwatt=$(echo "$pvwatt*-1" |bc)
 fi
-
 echo $pvwatt
 # zur weiteren verwendung im webinterface
 echo $pvwatt > /var/www/html/openWB/ramdisk/pvwatt
 # Gesamtzählerstand am WR [kWh]
-# pvkwh=$(echo $pvwatttmp | jq '.dxsEntries[1].value' | sed 's/\..*$//')
-# echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwhk
-# pvkwh=$(echo "$pvkwh*1000" |bc)
+pvkwh=$(echo $pvwatttmp | jq '.dxsEntries[1].value' | sed 's/\..*$//')
+echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwhk
+pvkwh=$(echo "$pvkwh*1000" |bc)
 # zur weiteren verwendung im webinterface
-# echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwh
+echo $pvkwh > /var/www/html/openWB/ramdisk/pvkwh
