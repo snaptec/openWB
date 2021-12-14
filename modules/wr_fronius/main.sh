8e46097a122689129923f5a3bdfe0ea618eaf370
LKuemmel <lena.kuemmel@openwb.de>
2021-12-14 11:41:29 +0100

    add fronius

--- modules/wr_fronius/main.sh
+++ modules/wr_fronius/main.sh
@@ -2,24 +2,24 @@
 OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
 MODULEDIR=$(cd `dirname $0` && pwd)
-#DMOD="EVU"
-DMOD="MAIN"
+DMOD="PV"
+#DMOD="MAIN"
 Debug=$debug
 
 #For development only
 #Debug=1
 
 if [ ${DMOD} == "MAIN" ]; then
-	MYLOGFILE="${RAMDISKDIR}/openWB.log"
+        MYLOGFILE="${RAMDISKDIR}/openWB.log"
 else
-	MYLOGFILE="${RAMDISKDIR}/wr_fronius.log"
+        MYLOGFILE="${RAMDISKDIR}/nurpv.log"
 fi
 
 openwbDebugLog ${DMOD} 2 "WR IP: ${wrfroniusip}"
 openwbDebugLog ${DMOD} 2 "WR Gen 24: ${wrfroniusisgen24}"
 openwbDebugLog ${DMOD} 2 "WR IP2: ${wrfronius2ip}"
 
-python3 /var/www/html/openWB/modules/wr_fronius/fronius.py "${wrfroniusip}" "${wrfroniusisgen24}" "${wrfronius2ip}" &>>$MYLOGFILE
+python3 /var/www/html/openWB/packages/modules/fronius/device.py "inverter" "${wrfroniusip}" "0" "${wrfroniusisgen24}" "0" "0" "0" "${wrfronius2ip}" "${speichermodul}" "1" &>>$MYLOGFILE
 ret=$?
 
 openwbDebugLog ${DMOD} 2 "RET: ${ret}"
