84334e0f756086efaf9bff4ac947029b78412f21
Yannik Hampe <yzeugs@cipher-code.de>
2021-12-15 23:06:16 +0100

    fix SMA

--- modules/wr_tripower9000/main.sh
+++ modules/wr_tripower9000/main.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 python3 /var/www/html/openWB/modules/wr_tripower9000/tripower.py "${wrsmawebbox}" "${tri9000ip}" "${wrsma2ip}" "${wrsma3ip}" "${wrsma4ip}"
-
-pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt)
-echo $pvwatt
-ekwh=$(</var/www/html/openWB/ramdisk/pvkwh)
-pvkwhk=$(echo "scale=3;$ekwh / 1000" |bc)
-echo $pvkwhk > /var/www/html/openWB/ramdisk/pvkwhk
+cat /var/www/html/openWB/ramdisk/pvwatt
