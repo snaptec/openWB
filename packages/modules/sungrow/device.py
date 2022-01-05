d84fa5d9f5a5c0baf8cfe7a9ace913bdfc7df12d
Yannik Hampe <yzeugs@cipher-code.de>
2022-01-05 20:44:06 +0100

    use legacy run for sungrow

--- packages/modules/sungrow/device.py
+++ packages/modules/sungrow/device.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-""" Modul zum Auslesen von Alpha Ess Speichern, Zählern und Wechselrichtern.
-"""
-import sys
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
 
 from helpermodules import log
+from helpermodules.cli import run_using_positional_cli_args
 from modules.common import modbus
 from modules.common.abstract_device import AbstractDevice
 from modules.common.component_context import SingleComponentUpdateContext
@@ -69,45 +67,29 @@ class Device(AbstractDevice):
             )
 
 
-def read_legacy(argv: List[str]) -> None:
-    COMPONENT_TYPE_TO_MODULE = {
-        "bat": bat,
-        "counter": counter,
-        "inverter": inverter
-    }
-    component_type = argv[1]
-    ip_address = argv[2]
-
+def read_legacy(ip_address: str, component_config: dict, id: Optional[int], **kwargs):
+    component_config["id"] = id
+    component_config["configuration"].update(kwargs)
     device_config = get_default_config()
     device_config["configuration"]["ip_address"] = ip_address
     dev = Device(device_config)
-    if component_type in COMPONENT_TYPE_TO_MODULE:
-        component_config = COMPONENT_TYPE_TO_MODULE[component_type].get_default_config()
-        if component_type == "counter":
-            version = int(argv[3])
-            component_config["configuration"]["version"] = version
-            log.MainLogger().debug('Sungrow Version: ' + str(version))
-            num = None
-        else:
-            version = None
-            try:
-                num = int(argv[3])
-            except IndexError:
-                num = None
-    else:
-        raise Exception(
-            "illegal component type " + component_type + ". Allowed values: " +
-            ','.join(COMPONENT_TYPE_TO_MODULE.keys())
-        )
-    component_config["id"] = num
     dev.add_component(component_config)
-
-    log.MainLogger().debug('Sungrow IP-Adresse: ' + str(ip_address))
     dev.update()
 
 
-if __name__ == "__main__":
-    try:
-        read_legacy(sys.argv)
-    except Exception:
-        log.MainLogger().exception("Fehler im Sungrow Skript")
+def read_legacy_bat(ip_address: str, num: Optional[int]):
+    read_legacy(ip_address, bat.get_default_config(), num)
+
+
+def read_legacy_counter(ip_address: str, version: int):
+    read_legacy(ip_address, counter.get_default_config(), version=version)
+
+
+def read_legacy_inverter(ip_address: str, num: Optional[int]):
+    read_legacy(ip_address, inverter.get_default_config(), num)
+
+
+def main(argv: List[str]):
+    run_using_positional_cli_args(
+        {"bat": read_legacy_bat, "counter": read_legacy_counter, "inverter": read_legacy_inverter}, argv
+    )
