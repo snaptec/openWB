c5fe87cd5980864a45d0f211970889e863235a86
Lutz Bender <github@lutz-bender.de>
2022-01-06 09:25:11 +0100

    unify powerwall modules

--- modules/wr_powerwall/main.sh
+++ modules/wr_powerwall/main.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+OPENWBBASEDIR=$(cd "$(dirname $0)/../../" && pwd)
 RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
-#MODULEDIR=$(cd `dirname $0` && pwd)
+MODULEDIR=$(cd "$(dirname $0)" && pwd)
 #DMOD="PV"
 DMOD="MAIN"
-Debug=$debug
-
-#For Development only
-#Debug=1
 
 if [ $DMOD == "MAIN" ]; then
 	MYLOGFILE="${RAMDISKDIR}/openWB.log"
@@ -16,15 +12,7 @@ else
 	MYLOGFILE="${RAMDISKDIR}/wr_powerwall.log"
 fi
 
-openwbDebugLog ${DMOD} 2 "PV Login erforderlich: ${speicherpwloginneeded}"
-openwbDebugLog ${DMOD} 2 "PV User: ${speicherpwuser}"
-openwbDebugLog ${DMOD} 2 "PV Passwort: ${speicherpwpass}"
-openwbDebugLog ${DMOD} 2 "PV IP: ${speicherpwip}"
+python3 "${MODULEDIR}/powerwall.py" "${speicherpwip}" "${speicherpwuser}" "${speicherpwpass}" >>$MYLOGFILE 2>&1
 
-python3 /var/www/html/openWB/modules/wr_powerwall/powerwall.py "${OPENWBBASEDIR}" "${speicherpwloginneeded}" "${speicherpwuser}" "${speicherpwpass}" "${speicherpwip}" >>$MYLOGFILE 2>&1
-ret=$?
-
-openwbDebugLog ${DMOD} 2 "RET: ${ret}"
-
-pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt) 
+pvwatt=$(<${RAMDISKDIR}/pvwatt) 
 echo $pvwatt
