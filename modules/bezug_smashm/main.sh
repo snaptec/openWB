fc099c1939051b0e43f89dbc2b854610306188f3
Yannik Hampe <yzeugs@cipher-code.de>
2022-01-17 21:56:22 +0100

    log it if fetching Date from SMA HomeManager/EnergyMeter times out

--- modules/bezug_smashm/main.sh
+++ modules/bezug_smashm/main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 timeout 3 python3 "$OPENWBBASEDIR/modules/bezug_smashm/sma_em_measurement.py" "$smashmbezugid" >> "$OPENWBBASEDIR/ramdisk/openWB.log" 2>&1
+(($? == 124)) && openwbDebugLog "MAIN" 0 "Fetching SMA counter data timed out"
 cat /var/www/html/openWB/ramdisk/wattbezug
