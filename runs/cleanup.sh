9c47abbb5ba78d8a7e45d0e1318b5025692610b1
hhoefling <penny@gmx.eu>
2022-01-18 18:53:59 +0100

    Logfile cleaning optimiert.
    - openwb.log gibt es nicht. (soll wohl openWB.log sein)
    - Die Logfiles werden alle 5 Minuten geprüft, aber nicht jedes mal einfach  überge"tailed" sondern nur wenn sie größer als 256KB sind werden sie gevierteilt.

--- runs/cleanup.sh
+++ runs/cleanup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#
+# Running with sudo
+#
+timestamp=`date +"%Y-%m-%d %H:%M:%S"`
 if [ -f /var/www/html/openWB/ramdisk/debuguser ]; then
-	timestamp=`date +"%Y-%m-%d %H:%M:%S"`
-	echo "$timestamp cleanup.sh: Skipping logfile cleanup as senddebug.sh is collecting data." >> /var/www/html/openWB/ramdisk/openwb.log
+	echo "$timestamp cleanup.sh: Skipping logfile cleanup as senddebug.sh is collecting data." >> /var/www/html/openWB/ramdisk/openWB.log
 else
+	echo "$timestamp cleanup.sh: checking logfiles." >> /var/www/html/openWB/ramdisk/openWB.log
 	find /var/www/html/openWB/ramdisk/ -name "*.log" -type f -exec /var/www/html/openWB/runs/cleanupf.sh {} \;
 fi
--- runs/cleanupf.sh
+++ runs/cleanupf.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+# Truncate Logfile to max 256KB each
+# Truncate only if bigger, let the file untouched if less.
+
+f=$1
+
+logfilesize=$(stat --format=%s "$f")
+if  (( $logfilesize > (256*1024) )) ; then
+    timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+    lines=$(wc -l <"$f" )
+    lines=$(( $lines / 4 ))   # truncate to 1/4 size
+    echo "$(tail -$lines $f)" > $f
+    echo "$timestamp cleanupf.sh truncate tp $lines" >>$f
+    echo "$timestamp cleanupf.sh Logfile $f bigger than 256Kb, truncate it to $lines lines"
+    chown pi:pi $f
+    chmod a+rw $f
+    #ls -l $f
+fi
+
+
+
 #echo "tail $1"
-ls $1 -l
-echo "$(tail -2000 $1)" > $1
+#ls $1 -l
+#echo "$(tail -2000 $1)" > $1
