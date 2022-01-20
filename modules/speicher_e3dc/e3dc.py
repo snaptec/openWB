90e28069e970dab14ddef108d8c9f43d7abd9077
okaegi <okaegi@pinball4you.ch>
2022-01-20 18:09:10 +0100

    E3DC Speicher und WR Anpassung V2
    Der E3DC Speicher und E3DC WR wurde an V2 Standards angepasst.
    Im E3DC Speicherdriver ist ebenso der E3DC Wr Anschluss realisiert.
    Der E3DC Speicher kennt heute folgende Übergabeparameter
    - IP Adresse erster Speicher (muss)
    - IP Adresse zweiter Speicher (optional)
    - Externe Produktion vom E3DC  berücksichtigen (wenn ja wird die im Modbusprotokoll als externe PV Produktion mit ausgelesen).
    
    Die Pv Produktion vom E3DC wird immer ausgelesen.
    
    Folgende Parametrisierungen gibt es:
    1)
    PV Modul: None
    Speicher: E3DC Externe Produktion = JA/NEIN
    Hier liefert der driver sowohl Speicher Leistung als auch PV Leistung (intern und extern wenn JA) und  wird als WR1 im Loadvars berücksichtigt)
    2)
    PV Modul: Z.B. Solaredge
    Speicher: E3DC Externe Produktion = Nein/Ja
    Hier liefert der driver sowohl Speicher Leistung als auch PV Leistung (intern und extern wenn ja)
    wenn tatsächlich eine PV Leistung anfällt. Diese Leistung wird zu der Solaredge Leistung hinzuaddiert.
    
    3)
    edc3 Adresse 1 und e3dc Adresse 2 gefüllt (Farm)
    Beide Speichersystem werden hintereinander abgefragt, der SOC wird als Durchschnitt geliefert, die PV Leistung wird als ein Wert an openwb übergeben. Im Gegensatz zur V1 Implementierung wird auch das externe Produktion Flag korrekt interpretiert.
    Testen konnte ich in meiner Config
    Solaredge RW
    (E3DC nur mit Batterie, Solaredge WR als externe Produktion angeschlossen) Fall 1 und 2

--- modules/speicher_e3dc/e3dc.py
+++ modules/speicher_e3dc/e3dc.py
@@ -1,56 +1,69 @@
-#!/usr/bin/python
-import sys
-# import os
-# import time
-# import getopt
-# import socket
-# import ConfigParser
-import struct
-# import binascii
-from pymodbus.client.sync import ModbusTcpClient
+#!/usr/bin/python3
+import logging
+from statistics import mean
+from typing import Iterable
 
-ipaddress = str(sys.argv[1])
-addext = int(sys.argv[2])
+from pymodbus.constants import Endian
 
-client = ModbusTcpClient(ipaddress, port=502)
+from helpermodules.cli import run_using_positional_cli_args
+from helpermodules.log import setup_logging_stdout
+from modules.common.component_state import InverterState, BatState
+from modules.common.modbus import ModbusClient, ModbusDataType
+from modules.common.store import get_inverter_value_store, get_bat_value_store
+from modules.common.simcount import SimCountFactory
 
-# battsoc
-resp= client.read_holding_registers(40082,1,unit=1)
-value1 = resp.registers[0]
-all = format(value1, '04x')
-final = int(struct.unpack('>h', all.decode('hex'))[0]) 
-f = open('/var/www/html/openWB/ramdisk/speichersoc', 'w')
-f.write(str(final))
-f.close()
-# print "hausverbrauch"
-# resp= client.read_holding_registers(40071,2,unit=1)
-# value1 = resp.registers[0]
-# value2 = resp.registers[1]
-# all = format(value2, '04x') + format(value1, '04x')
-# final = int(struct.unpack('>i', all.decode('hex'))[0])
-# print final
-# pv punkt
-ext = 0
-if addext == 1:
-    resp= client.read_holding_registers(40075,2,unit=1)
-    value1 = resp.registers[0]
-    value2 = resp.registers[1]
-    all = format(value2, '04x') + format(value1, '04x')
-    ext = int(struct.unpack('>i', all.decode('hex'))[0])
-resp= client.read_holding_registers(40067,2,unit=1)
-value1 = resp.registers[0]
-value2 = resp.registers[1]
-all = format(value2, '04x') + format(value1, '04x')
-final = int(struct.unpack('>i', all.decode('hex'))[0]) * -1
-f = open('/var/www/html/openWB/ramdisk/pvwatt', 'w')
-f.write(str(final+ext))
-f.close()
-#battleistung
-resp= client.read_holding_registers(40069,2,unit=1)
-value1 = resp.registers[0]
-value2 = resp.registers[1]
-all = format(value2, '04x') + format(value1, '04x')
-final = int(struct.unpack('>i', all.decode('hex'))[0])
-f = open('/var/www/html/openWB/ramdisk/speicherleistung', 'w')
-f.write(str(final))
-f.close()
+log = logging.getLogger("E3DC Battery")
+
+def update_e3dc_battery(address: Iterable[str],external: int,pvother: int,pvwattin: int):
+    soc = 0
+    count = 0
+    speicher = 0
+    # extpv - > pv Leistung die als externe Produktion an e3dc angeschlossen ist
+    # nur auslesen wenn als relevant parametrisiert  (external = 1) , sonst doppelte Auslesung
+    extpv = 0
+    # pv -> pv Leistung die direkt an e3dc angeschlossen ist
+    pv = 0
+    for addr in address:
+        log.debug("Battery Ip: %s, external %d pvother %d pvwatt (input) %d", addr,external,pvother,pvwattin)
+        if addr != "none":
+            count=count+1
+            client = ModbusClient(addr, port=502)
+            #40082 soc
+            soc = soc + client.read_holding_registers(40082,ModbusDataType.INT_16,unit=1)
+            #40069 speicherleistung
+            speicher = speicher + client.read_holding_registers(40069, ModbusDataType.INT_32, wordorder=Endian.Little,unit=1)
+            #40067 pv Leistung
+            pv = pv + (client.read_holding_registers(40067, ModbusDataType.INT_32, wordorder=Endian.Little,unit=1) * -1)
+            if external == 1:
+                #40075 externe pv Leistung
+                extpv = extpv + client.read_holding_registers(40075, ModbusDataType.INT_32, wordorder=Endian.Little,unit=1)
+    soc = soc / count
+    log.debug("Battery soc %d speicherleistung %d pv %d extpv %d anzahl ip %d", soc,speicher,pv,extpv,count)
+    cnt= SimCountFactory().get_sim_counter()().sim_count(speicher, prefix="speicher")
+    get_bat_value_store(1).set(BatState(power=speicher, soc=soc, imported= cnt[0], exported= cnt[1]))
+    # pvother sagt aus, ob wr definiert ist, und dessen pv Leistungs auch gilt
+    # wenn 0 gilt nur pv und extpv aus e3dc
+    pvtotal = pv + extpv
+    if (pvother == 0) or (pvtotal != 0):
+        if pvother == 1:
+            pvtotal = pvtotal + pvwattin
+        log.debug(" wr update pvother %d pvtotal %d", pvother,pvtotal)
+        cntpv= SimCountFactory().get_sim_counter()().sim_count(pvtotal, prefix="pv")
+        get_inverter_value_store(1).set(InverterState(counter=cntpv[1], power=pvtotal))
+
+def main(address1: str,address2: str, external: int,pvmodul: str,pvwattin: int):
+    # external is 0 or 1
+    log.debug("Beginning update")
+    address = ['none']*2
+    address[0]=address1
+    address[1]=address2
+    if pvmodul == "none":
+        pvother = 0
+    else:
+        pvother = 1
+    update_e3dc_battery(address,external,pvother,pvwattin)
+    log.debug("Update completed successfully")
+
+if __name__ == '__main__':
+    setup_logging_stdout()
+    run_using_positional_cli_args(main)
