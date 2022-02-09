eb379841c68fccb09e66f399d981370f326116df
Lutz Bender <github@lutz-bender.de>
2022-02-09 09:25:58 +0100

    fix indentation in updateConfig.sh

--- runs/updateConfig.sh
+++ runs/updateConfig.sh
@@ -2170,15 +2170,15 @@ updateConfig(){
 		echo "pv2flexversion=1" >> $ConfigFile
 	fi
 	if ! grep -Fq "soc_aiways_user=" $ConfigFile; then
-        echo "soc_aiways_user=''" >> $ConfigFile
-        echo "soc_aiways_pass=''" >> $ConfigFile
-        echo "soc_aiways_vin=''" >> $ConfigFile
-        echo "soc_aiways_intervall=''" >> $ConfigFile
-        echo "soc_aiwayslp2_user=''" >> $ConfigFile
-        echo "soc_aiwayslp2_pass=''" >> $ConfigFile
-        echo "soc_aiwayslp2_vin=''" >> $ConfigFile
-        echo "soc_aiwayslp2_intervall=''" >> $ConfigFile
-    fi
+		echo "soc_aiways_user=''" >> $ConfigFile
+		echo "soc_aiways_pass=''" >> $ConfigFile
+		echo "soc_aiways_vin=''" >> $ConfigFile
+		echo "soc_aiways_intervall=''" >> $ConfigFile
+		echo "soc_aiwayslp2_user=''" >> $ConfigFile
+		echo "soc_aiwayslp2_pass=''" >> $ConfigFile
+		echo "soc_aiwayslp2_vin=''" >> $ConfigFile
+		echo "soc_aiwayslp2_intervall=''" >> $ConfigFile
+	fi
 
 	echo "Config file Update done."
 }
