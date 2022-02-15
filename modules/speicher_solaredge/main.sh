e77ed684968f169b4794b253c099c993f1290a6a
LKuemmel <lena.kuemmel@openwb.de>
2022-02-15 20:12:58 +0100

    legacy run server for wr2 kostal piko var 2

--- modules/speicher_solaredge/main.sh
+++ modules/speicher_solaredge/main.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-if [[ $solaredgespeicherip == $solaredgepvip ]]  ; then
-	echo "value read at pv modul" > /dev/null
-else
-	OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
-	python3 "$OPENWBBASEDIR/modules/speicher_solaredge/solaredge.py" "$solaredgespeicherip" "$solaredgezweiterspeicher" >> "${OPENWBBASEDIR}/ramdisk/openWB.log" 2>&1
-fi
-
-#!/bin/bash
 OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
 MODULEDIR=$(cd `dirname $0` && pwd)
