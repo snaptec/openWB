3a6822ae9751fc2fdd75954f783edf925033432e
LKuemmel <lena.kuemmel@openwb.de>
2022-02-21 15:46:22 +0100

    add to packages

--- modules/bezug_lgessv1/main.sh
+++ modules/bezug_lgessv1/main.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd "$(dirname "$0")/../../" && pwd)
-RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
-MODULEDIR=$(cd "$(dirname "$0")" && pwd)
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
+MODULEDIR=$(cd `dirname $0` && pwd)
 #DMOD="EVU"
 DMOD="MAIN"
+Debug=$debug
 
-if [ $DMOD == "MAIN" ]; then
-	MYLOGFILE="$RAMDISKDIR/openWB.log"
+#For development only
+#Debug=1
+
+if [ ${DMOD} == "MAIN" ]; then
+        MYLOGFILE="${RAMDISKDIR}/openWB.log"
 else
-	MYLOGFILE="$RAMDISKDIR/evu_json.log"
+        MYLOGFILE="${RAMDISKDIR}/evu.log"
 fi
 
-bash "$OPENWBBASEDIR/packages/legacy_run.sh" "bezug_lgessv1.lgessv1" "${lgessv1ip}" "${lgessv1pass}" "${ess_api_ver}" >>${MYLOGFILE} 2>&1
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "modules.lg.device" "counter" "${lgessv1ip}" "${lgessv1pass}" "${ess_api_ver}">>${MYLOGFILE} 2>&1
 ret=$?
 
-openwbDebugLog ${DMOD} 2 "RET: ${ret}"
-
-wattbezug=$(</var/www/html/openWB/ramdisk/wattbezug)
-echo "$wattbezug"
+openwbDebugLog ${DMOD} 2 "EVU RET: ${ret}"
+wattbezug=$(<${RAMDISKDIR}/wattbezug)
+echo $wattbezug
