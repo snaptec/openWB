75c19e2c7a43c51eac2c07ec356326b7eee0e330
Lutz Bender <github@lutz-bender.de>
2022-02-23 11:57:32 +0100

    kostal piko

--- modules/wr_kostalpikovar2/main.sh
+++ modules/wr_kostalpikovar2/main.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+OPENWBBASEDIR=$(cd "$(dirname "$0")/../../" && pwd)
 RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
-MODULEDIR=$(cd `dirname $0` && pwd)
 #DMOD="EVU"
 DMOD="MAIN"
-Debug=$debug
-
-#For development only
-#Debug=1
 
 if [ ${DMOD} == "MAIN" ]; then
 	MYLOGFILE="${RAMDISKDIR}/openWB.log"
@@ -20,10 +15,10 @@ openwbDebugLog ${DMOD} 2 "WR User: ${wr_piko2_user}"
 openwbDebugLog ${DMOD} 2 "WR Passwort: ${wr_piko2_pass}"
 openwbDebugLog ${DMOD} 2 "WR URL: ${wr_piko2_url}"
 
-bash "$OPENWBBASEDIR/packages/legacy_run.sh" "wr_kostalpikovar2.kostal_piko_var2" 1 "${wr_piko2_url}" "${wr_piko2_user}" "${wr_piko2_pass}" >>$MYLOGFILE 2>&1
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "wr_kostalpikovar2.kostal_piko_var2" 1 "${wr_piko2_url}" "${wr_piko2_user}" "${wr_piko2_pass}" >>"$MYLOGFILE" 2>&1
 ret=$?
 
 openwbDebugLog ${DMOD} 2 "RET: ${ret}"
 
 pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt) 
-echo $pvwatt
+echo "$pvwatt"
