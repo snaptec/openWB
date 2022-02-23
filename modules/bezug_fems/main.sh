43e19070e1207c57aba189673e47ccdb010767d5
Lutz Bender <github@lutz-bender.de>
2022-02-23 11:36:14 +0100

    fems

--- modules/bezug_fems/main.sh
+++ modules/bezug_fems/main.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+OPENWBBASEDIR=$(cd "$(dirname "$0")/../../" && pwd)
+RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
+#DMOD="EVU"
+DMOD="MAIN"
 
-sudo python3 /var/www/html/openWB/modules/bezug_fems/fems.py $femskacopw $femsip
-wattbezug=$(</var/www/html/openWB/ramdisk/wattbezug)
-echo $wattbezug
+if [ ${DMOD} == "MAIN" ]; then
+	MYLOGFILE="${RAMDISKDIR}/openWB.log"
+else
+	MYLOGFILE="${RAMDISKDIR}/evu.log"
+fi
+
+openwbDebugLog ${DMOD} 2 "FEMS IP: ${femsip}"
+openwbDebugLog ${DMOD} 2 "FEMS Passwort: ${femskacopw}"
+
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "bezug_fems.fems" "$femskacopw" "$femsip" >>"${MYLOGFILE}" 2>&1
+ret=$?
+
+openwbDebugLog ${DMOD} 2 "EVU RET: ${ret}"
+wattbezug=$(<"${RAMDISKDIR}/wattbezug")
+echo "$wattbezug"
