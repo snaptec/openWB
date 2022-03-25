9c1b5ebacecf221849460954b6047e75f8c92798
Lutz Bender <github@lutz-bender.de>
2022-03-25 08:55:37 +0100

    fix lovato counter

--- modules/wr_ethmpm3pmaevu/main.sh
+++ modules/wr_ethmpm3pmaevu/main.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+OPENWBBASEDIR=$(cd "$(dirname "$0")/../../" && pwd)
 RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
-MODULEDIR=$(cd `dirname $0` && pwd)
 DMOD="PV"
 #DMOD="MAIN"
-Debug=$debug
-
-#For development only
-#Debug=1
 
 if [ ${DMOD} == "MAIN" ]; then
-        MYLOGFILE="${RAMDISKDIR}/openWB.log"
+	MYLOGFILE="${RAMDISKDIR}/openWB.log"
 else
-        MYLOGFILE="${RAMDISKDIR}/nurpv.log"
+	MYLOGFILE="${RAMDISKDIR}/nurpv.log"
 fi
 
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "modules.openwb_pv_evu.device" "${pvkitversion}" "1">>"$MYLOGFILE" 2>&1
 
-bash "$OPENWBBASEDIR/packages/legacy_run.sh" "modules.openwb_pv_evu.device" "${pvkitversion}" "1">>${MYLOGFILE} 2>&1
-
-pvwatt=$(<${RAMDISKDIR}/pvwatt)
-echo $pvwatt
+cat "$RAMDISKDIR/pvwatt"
