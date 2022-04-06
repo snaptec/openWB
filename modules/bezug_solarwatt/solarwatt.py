3d85de0974aa1c539ff1aabf49224dcba9221b8d
Lutz Bender <github@lutz-bender.de>
2022-04-06 10:35:37 +0200

    fix solarwatt battery module

--- modules/bezug_solarwatt/solarwatt.py
+++ modules/bezug_solarwatt/solarwatt.py
@@ -2,7 +2,6 @@
 from typing import List
 import logging
 import requests
-import sys
 
 from helpermodules.cli import run_using_positional_cli_args
 
@@ -15,20 +14,20 @@ def update(solarwattmethod: int, speicher1_ip: str, speicher1_ip2: str):
     log.debug('Solarwatt IP2: ' + speicher1_ip2)
 
     if solarwattmethod == 0:  # Abruf über Energy Manager
-        sresponse = requests.get('http://'+speicher1_ip+'/rest/kiwigrid/wizard/devices', timeout=3).json()
-        if len(str(sresponse)) < 10:
+        json_response = requests.get('http://'+speicher1_ip+'/rest/kiwigrid/wizard/devices', timeout=3).json()
+        if len(str(json_response)) < 10:
             with open("/var/www/html/openWB/ramdisk/wattbezug", "r") as f:
                 bezugwatt = f.read()
         else:
-            for item in sresponse["result"]["items"].values():
+            for item in json_response["result"]["items"].values():
                 bezugw = int(item["tagValues"]["PowerConsumedFromGrid"]["value"])
 
-            for item in sresponse["result"]["items"].values():
+            for item in json_response["result"]["items"].values():
                 einspeisungw = int(item["tagValues"]["PowerOut"]["value"])
             bezugwatt = bezugw - einspeisungw
     if solarwattmethod == 1:  # Abruf über Gateway
-        sresponse = requests.get('http://'+speicher1_ip2+':8080/', timeout=3).json()
-        bezugwatt = int(sresponse["FData"]["PGrid"])
+        json_response = requests.get('http://'+speicher1_ip2+':8080/', timeout=3).json()
+        bezugwatt = int(json_response["FData"]["PGrid"])
 
     log.debug("Netzbezug: "+str(bezugwatt)+" W")
     with open("/var/www/html/openWB/ramdisk/wattbezug", "w") as f:
