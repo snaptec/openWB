4f54366be54248470218b9da45c6192506a932e2
Lutz Bender <github@lutz-bender.de>
2022-05-04 14:17:32 +0200

    rework rfid handling

--- runs/readrfid2.py
+++ runs/readrfid2.py
@@ -1,7 +1,6 @@
 import evdev
 import sys
-import os
-from evdev import InputDevice, categorize 
+from evdev import InputDevice, categorize
 devicen = str(sys.argv[1])
 dev = InputDevice('/dev/input/event1')
 
@@ -12,17 +11,16 @@ scancodes = {
     30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u';',
     40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
     50: u'M', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
-    }
+}
 s = ""
 for event in dev.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
-        data = evdev.categorize(event) 
-        if data.keystate == 1:  
+        data = categorize(event)
+        if data.keystate == 1:
             key_lookup = scancodes.get(data.scancode) or u'UNKNOWN:{}'.format(data.scancode)
-            s+=str(format(key_lookup))
+            s += str(format(key_lookup))
             if "CRLF" in s:
                 s = s[:-4]
-                f = open('/var/www/html/openWB/ramdisk/readtag', 'w')
-                f.write(str(s))
-                f.close()
+                with open('/var/www/html/openWB/ramdisk/readtag', 'w') as f:
+                    f.write(str(s))
                 s = ""
