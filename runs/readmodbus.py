92bf3b2eac5fad78fac5397fc7ed99aa62812f9d
Lutz Bender <github@lutz-bender.de>
2022-06-28 14:40:56 +0200

    fix python warnings and format

--- runs/readmodbus.py
+++ runs/readmodbus.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 import sys
-import os
-import time
-import getopt
-import socket
-import struct
-import binascii
 from pymodbus.client.sync import ModbusSerialClient
 
 seradd = str(sys.argv[1])
@@ -13,8 +7,8 @@ modbusid = int(sys.argv[2])
 readreg = int(sys.argv[3])
 reganzahl = int(sys.argv[4])
 
-client = ModbusSerialClient(method = "rtu", port=seradd, baudrate=9600, stopbits=1, bytesize=8, timeout=1)
-request = client.read_holding_registers(readreg,reganzahl,unit=modbusid)
+client = ModbusSerialClient(method="rtu", port=seradd, baudrate=9600, stopbits=1, bytesize=8, timeout=1)
+request = client.read_holding_registers(readreg, reganzahl, unit=modbusid)
 if request.isError():
     # handle error, log?
     print('Modbus Error:', request)
