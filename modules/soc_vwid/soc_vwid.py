6c76f7ef26e548ceeb3448e5212812375a14f159
rleidner <rleidner@gmx.net>
2022-07-18 11:17:18 +0200

    fix missing quotes, add logging

--- modules/soc_vwid/soc_vwid.py
+++ modules/soc_vwid/soc_vwid.py
@@ -10,6 +10,17 @@ import time
 import json
 import os
 import pickle
+from datetime import datetime
+
+def logDebug(msg):
+    socLogFile= '/var/www/html/openWB/ramdisk/soc.log'
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    line = timestamp + ": " + msg + "\n"
+    f = open(socLogFile, 'a')
+    f.write(line)
+    f.close()
+    return
 
 async def main():
 #    logging.basicConfig(level=logging.DEBUG)
@@ -42,9 +53,11 @@ async def main():
             tf = open(tokensFile, "rb")     # try to open tokens file
             w.tokens = pickle.load(tf)      # initialize tokens in vwid
             tokens_old = pickle.dumps(w.tokens) # remember current tokens
-            w.headers[Authorization] = Bearer %s % w.tokens["accessToken"]
+            w.headers['Authorization'] = 'Bearer %s' % w.tokens["accessToken"]
             tf.close()
         except Exception as e:
+            logDebug("tokens initialization exception: e="+str(e))
+            logDebug("tokens initialization exception: set tokens_old to initial value")
             tokens_old = bytearray(1)   # if no old token found set tokens_old to dummy value
 
         data = await w.get_status()
@@ -52,18 +65,21 @@ async def main():
             print (data['data']['batteryStatus']['currentSOC_pct'])
             try:
                 f = open(replyFile, 'w', encoding='utf-8')
-            except:
+            except Exception as e:
+                logDebug("replyFile open exception: e="+str(e))
+                logDebug("replyFile open Exception, remove existing file")
                 os.system("sudo rm "+replyFile)
                 f = open(replyFile, 'w', encoding='utf-8')
             json.dump(data, f, ensure_ascii=False, indent=4)
             f.close()
+            os.chmod(replyFile, 0o777)
             tokens_new = pickle.dumps(w.tokens)
             if ( tokens_new != tokens_old ):    # check for modified tokens
+                logDebug("tokens_new != tokens_old, rewrite tokens file")
                 tf = open(tokensFile, "wb") 
                 pickle.dump(w.tokens, tf) # write tokens file
                 tf.close()
-                os.system("sudo chown pi "+tokensFile)
-                os.system("sudo chgrp pi "+tokensFile)
+                os.chmod(tokensFile, 0o777)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
