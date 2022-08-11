6e19b64d6d77495103908cf48fae08287b19746c
Lutz Bender <lutz@lutz-bender.de>
2022-08-11 15:44:03 +0200

    formatting

--- runs/pushButtons/pushButtonsHelper.sh
+++ runs/pushButtons/pushButtonsHelper.sh
@@ -4,41 +4,39 @@ if [[ -z "$OPENWBBASEDIR" ]]; then
 	RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
 fi
 
-declare -F openwbDebugLog &> /dev/null || {
+declare -F openwbDebugLog &>/dev/null || {
 	. "$OPENWBBASEDIR/helperFunctions.sh"
 }
 
-pushButtonsStart(){
-	if pgrep -f '^python.*/pushButtonsDaemon.py' > /dev/null
-	then
+pushButtonsStart() {
+	if pgrep -f '^python.*/pushButtonsDaemon.py' >/dev/null; then
 		openwbDebugLog "MAIN" 0 "push buttons handler already running"
 	else
 		openwbDebugLog "MAIN" 0 "push buttons handler not running! starting process"
-		python3 "$OPENWBBASEDIR/runs/pushButtons/pushButtonsDaemon.py" >> "$RAMDISKDIR/openWB.log" 2>&1 &
+		python3 "$OPENWBBASEDIR/runs/pushButtons/pushButtonsDaemon.py" >>"$RAMDISKDIR/openWB.log" 2>&1 &
 	fi
 }
 
-pushButtonsStop(){
-	if pgrep -f '^python.*/pushButtonsDaemon.py' > /dev/null
-	then
-        openwbDebugLog "MAIN" 0 "push buttons handler running but not configured. killing handler"
+pushButtonsStop() {
+	if pgrep -f '^python.*/pushButtonsDaemon.py' >/dev/null; then
+		openwbDebugLog "MAIN" 0 "push buttons handler running but not configured. killing handler"
 		sudo pkill -f '^python.*/pushButtonsDaemon.py'
 	fi
 }
 
-pushButtonsSetup(){
+pushButtonsSetup() {
 	local enabled=$1
 	local forceRestart=$2
 
-	if (( forceRestart == 1 )); then
+	if ((forceRestart == 1)); then
 		openwbDebugLog "MAIN" 0 "push buttons handler restart forced! killing handler"
 		pushButtonsStop
 	fi
-	if (( enabled == 1 )); then
-        openwbDebugLog "MAIN" 0 "push buttons enabled"
+	if ((enabled == 1)); then
+		openwbDebugLog "MAIN" 0 "push buttons enabled"
 		pushButtonsStart
 	else
-        openwbDebugLog "MAIN" 0 "push buttons disabled"
+		openwbDebugLog "MAIN" 0 "push buttons disabled"
 		pushButtonsStop
 	fi
 }
