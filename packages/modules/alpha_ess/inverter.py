45136d67f2978b735f52b94999fbc77cbf4ca128
Lutz Bender <lutz@lutz-bender.de>
2022-08-18 12:08:59 +0200

    fix alpha ess modbus connections

--- packages/modules/alpha_ess/inverter.py
+++ packages/modules/alpha_ess/inverter.py
@@ -50,11 +50,10 @@ class AlphaEssInverter:
             return 0x00A1
 
     def __get_power(self, unit: int, reg_p: int) -> Number:
-        with self.__tcp_client:
-            powers = [
-                self.__tcp_client.read_holding_registers(address, ModbusDataType.INT_32, unit=unit)
-                for address in [reg_p, 0x041F, 0x0423, 0x0427]
-            ]
+        powers = [
+            self.__tcp_client.read_holding_registers(address, ModbusDataType.INT_32, unit=unit)
+            for address in [reg_p, 0x041F, 0x0423, 0x0427]
+        ]
         powers[0] = abs(powers[0])
         power = sum(powers) * -1
         return power
