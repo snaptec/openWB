c67df21a9058faee2b853a01fce4d3510914d1a0
Lutz Bender <lutz@lutz-bender.de>
2022-08-25 07:52:15 +0200

    silence daemons

--- runs/rse/rseHelper.sh
+++ runs/rse/rseHelper.sh
@@ -10,16 +10,16 @@ declare -F openwbDebugLog &>/dev/null || {
 
 rseStart() {
 	if pgrep -f '^python.*/rseDaemon.py' >/dev/null; then
-		openwbDebugLog "MAIN" 0 "rse handler already running"
+		openwbDebugLog "MAIN" 2 "rse handler already running"
 	else
-		openwbDebugLog "MAIN" 0 "rse handler not running! starting process"
+		openwbDebugLog "MAIN" 1 "rse handler not running! starting process"
 		python3 "$OPENWBBASEDIR/runs/rse/rseDaemon.py" >>"$RAMDISKDIR/openWB.log" 2>&1 &
 	fi
 }
 
 rseStop() {
 	if pgrep -f '^python.*/rseDaemon.py' >/dev/null; then
-		openwbDebugLog "MAIN" 0 "rse handler running but not configured. killing handler"
+		openwbDebugLog "MAIN" 1 "rse handler running but not configured. killing handler"
 		sudo pkill -f '^python.*/rseDaemon.py'
 	fi
 }
@@ -29,14 +29,14 @@ rseSetup() {
 	local forceRestart=$2
 
 	if ((forceRestart == 1)); then
-		openwbDebugLog "MAIN" 0 "rse handler restart forced! killing handler"
+		openwbDebugLog "MAIN" 2 "rse handler restart forced! killing handler"
 		rseStop
 	fi
 	if ((enabled == 1)); then
-		openwbDebugLog "MAIN" 0 "rse enabled"
+		openwbDebugLog "MAIN" 1 "rse enabled"
 		rseStart
 	else
-		openwbDebugLog "MAIN" 0 "rse disabled"
+		openwbDebugLog "MAIN" 1 "rse disabled"
 		rseStop
 	fi
 }
