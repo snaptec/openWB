c9b5d0a543cc6a1145076a1c30c7deda8eced007
Lutz Bender <lutz@lutz-bender.de>
2022-11-18 09:27:42 +0100

    fix indentation

--- runs/cron5min.sh
+++ runs/cron5min.sh
@@ -8,10 +8,10 @@ RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 if [ -e "$OPENWBBASEDIR/ramdisk/updateinprogress" ] && [ -e "$OPENWBBASEDIR/ramdisk/bootinprogress" ]; then
 	updateinprogress=$(<"$OPENWBBASEDIR/ramdisk/updateinprogress")
 	bootinprogress=$(<"$OPENWBBASEDIR/ramdisk/bootinprogress")
-	if (( updateinprogress == "1" )); then
+	if ((updateinprogress == "1")); then
 		openwbDebugLog "MAIN" 0 "##### cron5min.sh Update in progress"
 		exit 0
-	elif (( bootinprogress == "1" )); then
+	elif ((bootinprogress == "1")); then
 		openwbDebugLog "MAIN" 0 "##### cron5min.sh Boot in progress"
 		exit 0
 	fi
@@ -28,17 +28,17 @@ monthlyladelogfile="$OPENWBBASEDIR/web/logging/data/ladelog/$(date +%Y%m).csv"
 # check if a monthly logfile exists and create a new one if not
 if [[ ! -f "$monthlyladelogfile" ]]; then
 	openwbDebugLog "MAIN" 1 "creating new monthly chargelog: $monthlyladelogfile"
-	echo > "$monthlyladelogfile"
+	echo >"$monthlyladelogfile"
 fi
 
-ll1=$(<"$RAMDISKDIR/llkwh")     # Zählerstand LP1
-ll2=$(<"$RAMDISKDIR/llkwhs1")   # Zählerstand LP2
-ll3=$(<"$RAMDISKDIR/llkwhs2")   # Zählerstand LP3
-ll4=$(<"$RAMDISKDIR/llkwhlp4")  # Zählerstand LP4
-ll5=$(<"$RAMDISKDIR/llkwhlp5")  # Zählerstand LP5
-ll6=$(<"$RAMDISKDIR/llkwhlp6")  # Zählerstand LP6
-ll7=$(<"$RAMDISKDIR/llkwhlp7")  # Zählerstand LP7
-ll8=$(<"$RAMDISKDIR/llkwhlp8")  # Zählerstand LP8
+ll1=$(<"$RAMDISKDIR/llkwh")    # Zählerstand LP1
+ll2=$(<"$RAMDISKDIR/llkwhs1")  # Zählerstand LP2
+ll3=$(<"$RAMDISKDIR/llkwhs2")  # Zählerstand LP3
+ll4=$(<"$RAMDISKDIR/llkwhlp4") # Zählerstand LP4
+ll5=$(<"$RAMDISKDIR/llkwhlp5") # Zählerstand LP5
+ll6=$(<"$RAMDISKDIR/llkwhlp6") # Zählerstand LP6
+ll7=$(<"$RAMDISKDIR/llkwhlp7") # Zählerstand LP7
+ll8=$(<"$RAMDISKDIR/llkwhlp8") # Zählerstand LP8
 llg=$(<"$RAMDISKDIR/llkwhges")
 
 # ins Log als Wh
@@ -97,63 +97,63 @@ d8haus=$(<"$RAMDISKDIR/smarthome_device_minhaus_8")
 d9haus=$(<"$RAMDISKDIR/smarthome_device_minhaus_9")
 
 # now add a line to our daily csv
-echo "$(date +%H%M),$bezug,$einspeisung,$pv,$ll1,$ll2,$ll3,$llg,$speicheri,$speichere,$verbraucher1,$verbrauchere1,$verbraucher2,$verbrauchere2,$verbraucher3,$ll4,$ll5,$ll6,$ll7,$ll8,$speichersoc,$soc,$soc1,$temp1,$temp2,$temp3,$d1,$d2,$d3,$d4,$d5,$d6,$d7,$d8,$d9,$d10,$temp4,$temp5,$temp6" >> "$dailyfile"
+echo "$(date +%H%M),$bezug,$einspeisung,$pv,$ll1,$ll2,$ll3,$llg,$speicheri,$speichere,$verbraucher1,$verbrauchere1,$verbraucher2,$verbrauchere2,$verbraucher3,$ll4,$ll5,$ll6,$ll7,$ll8,$speichersoc,$soc,$soc1,$temp1,$temp2,$temp3,$d1,$d2,$d3,$d4,$d5,$d6,$d7,$d8,$d9,$d10,$temp4,$temp5,$temp6" >>"$dailyfile"
 openwbDebugLog "MAIN" 1 "daily csv updated: $dailyfile"
 
 # grid protection
 # temporary disabled
 # netzabschaltunghz=0
-if (( netzabschaltunghz == 1 )); then
+if ((netzabschaltunghz == 1)); then
 	hz=$(<"$RAMDISKDIR/llhz")
 	hz=$(echo "$hz * 100" | bc | sed 's/\..*$//')
 	openwbDebugLog "MAIN" 1 "Netzschutz konfiguriert; aktuelle Frequenz: ${hz}"
 	netzschutz=$(<"$RAMDISKDIR/netzschutz")
-	if (( netzschutz == 0 )); then
+	if ((netzschutz == 0)); then
 		# grid protection is not set
-		if (( hz > 4500 )) && (( hz < 5300 )); then
-			if (( hz > 5180 )); then
+		if ((hz > 4500)) && ((hz < 5300)); then
+			if ((hz > 5180)); then
 				# grid power overload detected
 				# store current charge mode
 				lademodus=$(<"$RAMDISKDIR/lademodus")
-				echo "$lademodus" > "$RAMDISKDIR/templademodus"
+				echo "$lademodus" >"$RAMDISKDIR/templademodus"
 				# set charge mode to stop
-				echo 3 > "$RAMDISKDIR/lademodus"
+				echo 3 >"$RAMDISKDIR/lademodus"
 				openwbDebugLog "MAIN" 0 "Netzschutz aktiviert, Frequenz: ${hz}"
 				# set grid protection
-				echo 1 > "$RAMDISKDIR/netzschutz"
-				echo "!!! Netzschutz aktiv !!!" > "$RAMDISKDIR/lastregelungaktiv"
+				echo 1 >"$RAMDISKDIR/netzschutz"
+				echo "!!! Netzschutz aktiv !!!" >"$RAMDISKDIR/lastregelungaktiv"
 			fi
-			if (( hz < 4920 )); then
+			if ((hz < 4920)); then
 				# grid power underload detected
 				# store current charge mode
 				lademodus=$(<"$RAMDISKDIR/lademodus")
-				echo "$lademodus" > "$RAMDISKDIR/templademodus"
+				echo "$lademodus" >"$RAMDISKDIR/templademodus"
 				# set grid protection
-				echo 1 > "$RAMDISKDIR/netzschutz"
-				echo "!!! Netzschutz aktiv !!!" > "$RAMDISKDIR/lastregelungaktiv"
+				echo 1 >"$RAMDISKDIR/netzschutz"
+				echo "!!! Netzschutz aktiv !!!" >"$RAMDISKDIR/lastregelungaktiv"
 				openwbDebugLog "MAIN" 0 "Netzschutz aktiviert, Frequenz: ${hz}"
 
 				# wait a random interval and set charge mode to stop
-				(sleep "$(shuf -i1-90 -n1)" && echo 3 > "$RAMDISKDIR/lademodus") &
+				(sleep "$(shuf -i1-90 -n1)" && echo 3 >"$RAMDISKDIR/lademodus") &
 			fi
 		fi
 	else
 		# grid protection is set
-		if (( hz > 4960 )) && (( hz < 5100 )); then
+		if ((hz > 4960)) && ((hz < 5100)); then
 			# grid is in normal load range
 			# restore last charge mode
 			templademodus=$(<"$RAMDISKDIR/templademodus")
-			echo "$templademodus" > "$RAMDISKDIR/lademodus"
+			echo "$templademodus" >"$RAMDISKDIR/lademodus"
 			# remove grid protection
-			echo 0 > "$RAMDISKDIR/netzschutz"
+			echo 0 >"$RAMDISKDIR/netzschutz"
 			openwbDebugLog "MAIN" 0 "Netzschutz deaktiviert, Frequenz: ${hz}"
-			echo "Netzfrequenz wieder im normalen Bereich." > "$RAMDISKDIR/lastregelungaktiv"
+			echo "Netzfrequenz wieder im normalen Bereich." >"$RAMDISKDIR/lastregelungaktiv"
 		fi
 	fi
 fi
 
 # update electricity provider prices
-if (( etprovideraktiv == 1 )); then
+if ((etprovideraktiv == 1)); then
 	openwbDebugLog "MAIN" 1 "electricity provider configured; trigger price update"
 	"$OPENWBBASEDIR/modules/$etprovider/main.sh" &
 else
@@ -165,147 +165,146 @@ openwbDebugLog "MAIN" 1 "updating daily yield stats"
 pvkwh=$pv
 pvdailyyieldstart=$(head -n 1 "$OPENWBBASEDIR/web/logging/data/daily/$(date +%Y%m%d).csv")
 pvyieldcount=0
-for i in ${pvdailyyieldstart//,/ }
-do
+for i in ${pvdailyyieldstart//,/ }; do
 	# pv
-	pvyieldcount=$((pvyieldcount + 1 ))
-	if (( pvyieldcount == 2 )); then
-		bezugdailyyield=$(echo "scale=2;($bezug - $i) / 1000" |bc)
-		echo "$bezugdailyyield" > "$RAMDISKDIR/daily_bezugkwh"
+	pvyieldcount=$((pvyieldcount + 1))
+	if ((pvyieldcount == 2)); then
+		bezugdailyyield=$(echo "scale=2;($bezug - $i) / 1000" | bc)
+		echo "$bezugdailyyield" >"$RAMDISKDIR/daily_bezugkwh"
 	fi
-	if (( pvyieldcount == 3 )); then
-		einspeisungdailyyield=$(echo "scale=2;($einspeisung - $i) / 1000" |bc)
-		echo "$einspeisungdailyyield" > "$RAMDISKDIR/daily_einspeisungkwh"
+	if ((pvyieldcount == 3)); then
+		einspeisungdailyyield=$(echo "scale=2;($einspeisung - $i) / 1000" | bc)
+		echo "$einspeisungdailyyield" >"$RAMDISKDIR/daily_einspeisungkwh"
 	fi
-	if (( pvyieldcount == 4 )); then
-		pvdailyyield=$(echo "scale=2;($pvkwh - $i) / 1000" |bc)
-		echo "$pvdailyyield" > "$RAMDISKDIR/daily_pvkwhk"
+	if ((pvyieldcount == 4)); then
+		pvdailyyield=$(echo "scale=2;($pvkwh - $i) / 1000" | bc)
+		echo "$pvdailyyield" >"$RAMDISKDIR/daily_pvkwhk"
 	fi
 	# all charge points
-	if (( pvyieldcount == 8 )); then
-		lladailyyield=$(echo "scale=2;($llg - $i) / 1000" |bc)
-		echo "$lladailyyield" > "$RAMDISKDIR/daily_llakwh"
+	if ((pvyieldcount == 8)); then
+		lladailyyield=$(echo "scale=2;($llg - $i) / 1000" | bc)
+		echo "$lladailyyield" >"$RAMDISKDIR/daily_llakwh"
 	fi
 	# house battery
-	if (( pvyieldcount == 9 )); then
-		sidailyyield=$(echo "scale=2;($speicheri - $i) / 1000" |bc)
-		echo "$sidailyyield" > "$RAMDISKDIR/daily_sikwh"
+	if ((pvyieldcount == 9)); then
+		sidailyyield=$(echo "scale=2;($speicheri - $i) / 1000" | bc)
+		echo "$sidailyyield" >"$RAMDISKDIR/daily_sikwh"
 	fi
-	if (( pvyieldcount == 10 )); then
-		sedailyyield=$(echo "scale=2;($speichere - $i) / 1000" |bc)
-		echo "$sedailyyield" > "$RAMDISKDIR/daily_sekwh"
+	if ((pvyieldcount == 10)); then
+		sedailyyield=$(echo "scale=2;($speichere - $i) / 1000" | bc)
+		echo "$sedailyyield" >"$RAMDISKDIR/daily_sekwh"
 	fi
 	# old smarthome devices
-	if (( pvyieldcount == 11 )); then
-		verbraucher1dailyyield=$(echo "scale=2;($verbraucher1 - $i) / 1000" |bc)
-		echo "$verbraucher1dailyyield" > "$RAMDISKDIR/daily_verbraucher1ikwh"
+	if ((pvyieldcount == 11)); then
+		verbraucher1dailyyield=$(echo "scale=2;($verbraucher1 - $i) / 1000" | bc)
+		echo "$verbraucher1dailyyield" >"$RAMDISKDIR/daily_verbraucher1ikwh"
 	fi
-	if (( pvyieldcount == 12 )); then
-		verbrauchere1dailyyield=$(echo "scale=2;($verbrauchere1 - $i) / 1000" |bc)
-		echo "$verbrauchere1dailyyield" > "$RAMDISKDIR/daily_verbraucher1ekwh"
+	if ((pvyieldcount == 12)); then
+		verbrauchere1dailyyield=$(echo "scale=2;($verbrauchere1 - $i) / 1000" | bc)
+		echo "$verbrauchere1dailyyield" >"$RAMDISKDIR/daily_verbraucher1ekwh"
 	fi
-	if (( pvyieldcount == 13 )); then
-		verbraucher2dailyyield=$(echo "scale=2;($verbraucher2 - $i) / 1000" |bc)
-		echo "$verbraucher2dailyyield" > "$RAMDISKDIR/daily_verbraucher2ikwh"
+	if ((pvyieldcount == 13)); then
+		verbraucher2dailyyield=$(echo "scale=2;($verbraucher2 - $i) / 1000" | bc)
+		echo "$verbraucher2dailyyield" >"$RAMDISKDIR/daily_verbraucher2ikwh"
 	fi
-	if (( pvyieldcount == 14 )); then
-		verbrauchere2dailyyield=$(echo "scale=2;($verbrauchere2 - $i) / 1000" |bc)
-		echo "$verbrauchere2dailyyield" > "$RAMDISKDIR/daily_verbraucher2ekwh"
+	if ((pvyieldcount == 14)); then
+		verbrauchere2dailyyield=$(echo "scale=2;($verbrauchere2 - $i) / 1000" | bc)
+		echo "$verbrauchere2dailyyield" >"$RAMDISKDIR/daily_verbraucher2ekwh"
 	fi
-	if (( pvyieldcount == 15 )); then
-		verbraucher3dailyyield=$(echo "scale=2;($verbraucher3 - $i) / 1000" |bc)
-		echo "$verbraucher3dailyyield" > "$RAMDISKDIR/daily_verbraucher3ikwh"
+	if ((pvyieldcount == 15)); then
+		verbraucher3dailyyield=$(echo "scale=2;($verbraucher3 - $i) / 1000" | bc)
+		echo "$verbraucher3dailyyield" >"$RAMDISKDIR/daily_verbraucher3ikwh"
 	fi
 	# smarthome 2.0 devices
-	if (( pvyieldcount == 27 )); then
-		d1dailyyield=$(echo "scale=2;($d1 - $i) / 1000" |bc)
-		echo "$d1dailyyield" > "$RAMDISKDIR/daily_d1kwh"
+	if ((pvyieldcount == 27)); then
+		d1dailyyield=$(echo "scale=2;($d1 - $i) / 1000" | bc)
+		echo "$d1dailyyield" >"$RAMDISKDIR/daily_d1kwh"
 	fi
-	if (( pvyieldcount == 28 )); then
-		d2dailyyield=$(echo "scale=2;($d2 - $i) / 1000" |bc)
-		echo "$d2dailyyield" > "$RAMDISKDIR/daily_d2kwh"
+	if ((pvyieldcount == 28)); then
+		d2dailyyield=$(echo "scale=2;($d2 - $i) / 1000" | bc)
+		echo "$d2dailyyield" >"$RAMDISKDIR/daily_d2kwh"
 	fi
-	if (( pvyieldcount == 29 )); then
-		d3dailyyield=$(echo "scale=2;($d3 - $i) / 1000" |bc)
-		echo "$d3dailyyield" > "$RAMDISKDIR/daily_d3kwh"
+	if ((pvyieldcount == 29)); then
+		d3dailyyield=$(echo "scale=2;($d3 - $i) / 1000" | bc)
+		echo "$d3dailyyield" >"$RAMDISKDIR/daily_d3kwh"
 	fi
-	if (( pvyieldcount == 30 )); then
-		d4dailyyield=$(echo "scale=2;($d4 - $i) / 1000" |bc)
-		echo "$d4dailyyield" > "$RAMDISKDIR/daily_d4kwh"
+	if ((pvyieldcount == 30)); then
+		d4dailyyield=$(echo "scale=2;($d4 - $i) / 1000" | bc)
+		echo "$d4dailyyield" >"$RAMDISKDIR/daily_d4kwh"
 	fi
-	if (( pvyieldcount == 31 )); then
-		d5dailyyield=$(echo "scale=2;($d5 - $i) / 1000" |bc)
-		echo "$d5dailyyield" > "$RAMDISKDIR/daily_d5kwh"
+	if ((pvyieldcount == 31)); then
+		d5dailyyield=$(echo "scale=2;($d5 - $i) / 1000" | bc)
+		echo "$d5dailyyield" >"$RAMDISKDIR/daily_d5kwh"
 	fi
-	if (( pvyieldcount == 32 )); then
-		d6dailyyield=$(echo "scale=2;($d6 - $i) / 1000" |bc)
-		echo "$d6dailyyield" > "$RAMDISKDIR/daily_d6kwh"
+	if ((pvyieldcount == 32)); then
+		d6dailyyield=$(echo "scale=2;($d6 - $i) / 1000" | bc)
+		echo "$d6dailyyield" >"$RAMDISKDIR/daily_d6kwh"
 	fi
-	if (( pvyieldcount == 33 )); then
-		d7dailyyield=$(echo "scale=2;($d7 - $i) / 1000" |bc)
-		echo "$d7dailyyield" > "$RAMDISKDIR/daily_d7kwh"
+	if ((pvyieldcount == 33)); then
+		d7dailyyield=$(echo "scale=2;($d7 - $i) / 1000" | bc)
+		echo "$d7dailyyield" >"$RAMDISKDIR/daily_d7kwh"
 	fi
-	if (( pvyieldcount == 34 )); then
-		d8dailyyield=$(echo "scale=2;($d8 - $i) / 1000" |bc)
-		echo "$d8dailyyield" > "$RAMDISKDIR/daily_d8kwh"
+	if ((pvyieldcount == 34)); then
+		d8dailyyield=$(echo "scale=2;($d8 - $i) / 1000" | bc)
+		echo "$d8dailyyield" >"$RAMDISKDIR/daily_d8kwh"
 	fi
-	if (( pvyieldcount == 35 )); then
-		d9dailyyield=$(echo "scale=2;($d9 - $i) / 1000" |bc)
-		echo "$d9dailyyield" > "$RAMDISKDIR/daily_d9kwh"
+	if ((pvyieldcount == 35)); then
+		d9dailyyield=$(echo "scale=2;($d9 - $i) / 1000" | bc)
+		echo "$d9dailyyield" >"$RAMDISKDIR/daily_d9kwh"
 	fi
 done
 
 #echo $(date +%H%M),$d1haus,$d2haus,$d3haus,$d4haus,$d5haus,$d6haus,$d7haus,$d8haus,$d9haus, $d1dailyyield ,$d2dailyyield , $d3dailyyield , $d4dailyyield , $d5dailyyield , $d6dailyyield , $d7dailyyield , $d8dailyyield , $d9dailyyield  >> $RAMDISKDIR/alog.log
 # zero out devices were kwh should be included in house consumtion
-if (( d1haus == 1 )); then
- d1dailyyield=0
+if ((d1haus == 1)); then
+	d1dailyyield=0
 fi
-if (( d2haus == 1 )); then
- d2dailyyield=0
+if ((d2haus == 1)); then
+	d2dailyyield=0
 fi
-if (( d3haus == 1 )); then
- d3dailyyield=0
+if ((d3haus == 1)); then
+	d3dailyyield=0
 fi
-if (( d4haus == 1 )); then
- d4dailyyield=0
+if ((d4haus == 1)); then
+	d4dailyyield=0
 fi
-if (( d5haus == 1 )); then
- d5dailyyield=0
+if ((d5haus == 1)); then
+	d5dailyyield=0
 fi
-if (( d6haus == 1 )); then
- d6dailyyield=0
+if ((d6haus == 1)); then
+	d6dailyyield=0
 fi
-if (( d7haus == 1 )); then
- d7dailyyield=0
+if ((d7haus == 1)); then
+	d7dailyyield=0
 fi
-if (( d8haus == 1 )); then
- d8dailyyield=0
+if ((d8haus == 1)); then
+	d8dailyyield=0
 fi
-if (( d9haus == 1 )); then
- d9dailyyield=0
+if ((d9haus == 1)); then
+	d9dailyyield=0
 fi
 #echo $(date +%H%M),$d1haus,$d2haus,$d3haus,$d4haus,$d5haus,$d6haus,$d7haus,$d8haus,$d9haus, $d1dailyyield ,$d2dailyyield , $d3dailyyield , $d4dailyyield , $d5dailyyield , $d6dailyyield , $d7dailyyield , $d8dailyyield , $d9dailyyield  >> $RAMDISKDIR/alog.log
 # now calculate the house consumption daily yield as difference of measured input and output
 hausdailyyield=$(echo "scale=2;$bezugdailyyield + $pvdailyyield - $lladailyyield + $sedailyyield - $sidailyyield - $einspeisungdailyyield - $d1dailyyield - $d2dailyyield - $d3dailyyield - $d4dailyyield - $d5dailyyield - $d6dailyyield - $d7dailyyield - $d8dailyyield - $d9dailyyield - $verbraucher1dailyyield + $verbrauchere1dailyyield - $verbraucher2dailyyield + $verbrauchere2dailyyield - $verbraucher3dailyyield" | bc)
-echo "$hausdailyyield" > "$RAMDISKDIR/daily_hausverbrauchkwh"
+echo "$hausdailyyield" >"$RAMDISKDIR/daily_hausverbrauchkwh"
 
 # get our current ip address (prepared for Buster)
-ip route get 1 |  awk '{print $7;exit}' > "$RAMDISKDIR/ipaddress"
+ip route get 1 | awk '{print $7;exit}' >"$RAMDISKDIR/ipaddress"
 openwbDebugLog "MAIN" 1 "current ip: $(<"$RAMDISKDIR/ipaddress")"
 
 # Make sure all services are running (restart crashed services etc.):
 "$OPENWBBASEDIR/runs/services.sh" start
 
 #Pingchecker
-if (( pingcheckactive == 1 )); then
+if ((pingcheckactive == 1)); then
 	openwbDebugLog "MAIN" 1 "pingcheck configured; starting"
 	"$OPENWBBASEDIR/runs/pingcheck.sh" &
 fi
 
 # record the current commit details
 commitId=$(git -C "$OPENWBBASEDIR" log --format="%h" -n 1)
-echo "$commitId" > "$RAMDISKDIR/currentCommitHash"
-git -C "$OPENWBBASEDIR" branch -a --contains "$commitId" | perl -nle 'm|.*origin/(.+).*|; print $1' | uniq | xargs > "$RAMDISKDIR/currentCommitBranches"
+echo "$commitId" >"$RAMDISKDIR/currentCommitHash"
+git -C "$OPENWBBASEDIR" branch -a --contains "$commitId" | perl -nle 'm|.*origin/(.+).*|; print $1' | uniq | xargs >"$RAMDISKDIR/currentCommitBranches"
 
 # EVSE Check
 openwbDebugLog "MAIN" 1 "starting evsecheck"
@@ -313,6 +312,6 @@ openwbDebugLog "MAIN" 1 "starting evsecheck"
 
 # truncate all logs in ramdisk
 openwbDebugLog "MAIN" 1 "logfile cleanup triggered"
-"$OPENWBBASEDIR/runs/cleanup.sh" >> "$RAMDISKDIR/cleanup.log" 2>&1
+"$OPENWBBASEDIR/runs/cleanup.sh" >>"$RAMDISKDIR/cleanup.log" 2>&1
 
 openwbDebugLog "MAIN" 0 "##### cron5min.sh finished #####"
