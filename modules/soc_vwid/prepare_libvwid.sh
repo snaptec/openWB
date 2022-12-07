e1bedc091645ba63fe24d4ef091822707bb60044
rleidner <rleidner@gmx.net>
2022-12-07 14:32:20 +0100

    soc_vwid: fix python2 syntax problem in libvwid.py

--- modules/soc_vwid/prepare_libvwid.sh
+++ modules/soc_vwid/prepare_libvwid.sh
@@ -1,11 +1,14 @@
 echo "downloading libvwid.py  from github to libvwid.org"
 curl -sS -o libvwid.org https://raw.githubusercontent.com/skagmo/ha_vwid/main/custom_components/vwid/libvwid.py
-diff libvwid.py libvwid.org
+sed '
+/status_url =/s/status_url.*$/status_url = API_BASE + "\/vehicles\/" + self.vin + "\/selectivestatus?jobs=" + self.jobs_string/
+' < libvwid.org > libvwid.mod
+diff libvwid.py libvwid.mod
 echo "replace libvwid.py by libvwid.mod?(Y)"
 read a
 if [ "$a" == "Y" ]
 then
-	mv libvwid.org libvwid.py
+	mv libvwid.mod libvwid.py
 	echo "libvwid.py is replaced by version from github/skagmo"
 	chmod +x libvwid.py
 else
