840bb4e7e67e23a5eaa717bd0cbc1fae71fa4087
LKuemmel <lena.kuemmel@openwb.de>
2022-12-15 11:45:18 +0100

    add smartme

--- modules/bezug_smartme/main.sh
+++ modules/bezug_smartme/main.sh
@@ -14,7 +14,7 @@ openwbDebugLog ${DMOD} 2 "Bezug Smartme URL: ${bezug_smartme_url}"
 openwbDebugLog ${DMOD} 2 "Bezug Smartme User : ${bezug_smartme_user}"
 openwbDebugLog ${DMOD} 2 "Bezug Smartme Passwort: ${bezug_smartme_pass}"
 
-bash "$OPENWBBASEDIR/packages/legacy_run.sh" "bezug_smartme.smartme" "${bezug_smartme_url}" "${bezug_smartme_user}" "${bezug_smartme_pass}" >>"$MYLOGFILE" 2>&1
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "modules.devices.smart_me.device" "counter" "${bezug_smartme_user}" "${bezug_smartme_pass}" "${bezug_smartme_url}" >>"$MYLOGFILE" 2>&1
 ret=$?
 
 openwbDebugLog ${DMOD} 2 "RET: ${ret}"
