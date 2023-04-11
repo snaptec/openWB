f6ba9c299bdabbd147b5131aa973b3c345229c0d
Lutz Bender <lutz@lutz-bender.de>
2023-04-11 10:19:14 +0200

    fix duo charge point 2

--- modules/mpm3pmlls1/main.sh
+++ modules/mpm3pmlls1/main.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [[ $evsesources1 = *virtual* ]]
-then
-	if ps ax |grep -v grep |grep "socat pty,link=$evsesources1,raw tcp:$evselanips1:26" > /dev/null
-	then
-		echo "test" > /dev/null
+if [[ $evsesources1 = *virtual* ]]; then
+	if ps ax | grep -v grep | grep "socat pty,link=$evsesources1,raw tcp:$evselanips1:26" >/dev/null; then
+		echo "test" >/dev/null
 	else
 		sudo socat pty,link=$evsesources1,raw tcp:$evselanips1:26 &
 	fi
 else
-	echo "echo" > /dev/null
+	echo "echo" >/dev/null
 fi
-sudo python /var/www/html/openWB/modules/mpm3pmlls1/readmpm3pm.py $mpm3pmlls1source $mpm3pmlls1id
+sudo python /var/www/html/openWB/modules/mpm3pmlls1/readmpm3pm.py "$mpm3pmlls1source" "$mpm3pmlls1id"
