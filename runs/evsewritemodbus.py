18086954eb51ae78712af709f947f2102b16336a
LKuemmel <lena.kuemmel@openwb.de>
2023-05-15 16:46:51 +0200

    fix

--- runs/evsewritemodbus.py
+++ runs/evsewritemodbus.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-from pathlib import Path
+import os
 import sys
-from pymodbus.client.sync import ModbusSerialClient
 
 from modules.common.evse import Evse
 from modules.common.modbus import ModbusSerialClient_
 
-seradd = str(sys.argv[1])
 evseid = int(sys.argv[2])
 lla = int(sys.argv[3])
 
-RAMDSIK_PATH = Path(__file__).resolve().parents[1] / "ramdisk"
-EVSE_CONFIGURED_FILE = RAMDSIK_PATH / ("evse_configured_"+str(seradd[-1:])+"_"+str(evseid))
 
-client = ModbusSerialClient(method="rtu", port=seradd, baudrate=9600, stopbits=1, bytesize=8, timeout=1)
+def detect_modbus_usb_port():
+    """guess USB/modbus device name"""
+    known_devices = ("/dev/ttyUSB0", "/dev/ttyACM0", "/dev/serial0")
+    for device in known_devices:
+        try:
+            with open(device):
+                return device
+        except Exception:
+            pass
+    return known_devices[-1]  # this does not make sense, but is the same behavior as the old code
 
-if Path.is_file(EVSE_CONFIGURED_FILE) is False:
-    Evse(evseid, ModbusSerialClient_(seradd)).deactivate_precise_current()
-    with open(str(EVSE_CONFIGURED_FILE), 'w+') as f:
+
+serial_port = detect_modbus_usb_port()
+
+RAMDSIK_PATH = "/var/www/html/openWB/ramdisk/"
+EVSE_CONFIGURED_FILE = RAMDSIK_PATH+"evse_configured_"+str(serial_port[-4:])+"_"+str(evseid)
+
+evse = Evse(evseid, ModbusSerialClient_(serial_port))
+
+if os.path.isfile(EVSE_CONFIGURED_FILE) is False:
+    evse.deactivate_precise_current()
+    with open(EVSE_CONFIGURED_FILE, 'w+') as f:
         f.write("1")
 
-rq = client.write_registers(1000, lla, unit=evseid)
+rq = evse.set_current(lla)
