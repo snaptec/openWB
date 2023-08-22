1d796f5d39cf494e8610c4ceb9169f59eaad44a3
LKuemmel <lena.kuemmel@openwb.de>
2023-08-22 09:47:38 +0200

    Steca: Fix no production

--- packages/modules/devices/kostal_steca/inverter.py
+++ packages/modules/devices/kostal_steca/inverter.py
@@ -3,7 +3,6 @@ import logging
 from typing import Optional, Tuple
 import xml.etree.ElementTree as ET
 import re
-from math import isnan
 
 from modules.common import req
 from modules.common.component_state import InverterState
@@ -38,9 +37,8 @@ class KostalStecaInverter:
 
         # call for XML file and parse it for current PV power
         measurements = req.get_http_session().get("http://" + self.ip_address + "/measurements.xml", timeout=2).text
-        power = float(ET.fromstring(measurements).find(
-            ".//Measurement[@Type='AC_Power']").get("Value")) * -1
-        power = 0 if isnan(power) else power
+        power_raw = ET.fromstring(measurements).find(".//Measurement[@Type='AC_Power']").get("Value")
+        power = 0 if power_raw is None else float(power_raw) * -1
 
         if self.component_config.configuration.variant_steca:
             # call for XML file and parse it for total produced kwh
