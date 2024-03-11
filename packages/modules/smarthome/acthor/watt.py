5a6a1a6d663fcf15893b1cc6fd025758976c3011
okaegi <72255431+okaegi@users.noreply.github.com>
2024-03-11 14:39:13 +0100

    acthor 27k (#2824)
    * acthor 27k
    
    Unterstützung zwei verbundene Acthors (9000 Watt + 18000 Watt), Gesamthaft 27k Watt. Kann für 1.9 und 2.0 übernommen werden.
    Der Acthor wird als ein Gerät angesteuert und macht Verteilung selber.
    Kann für 1.9 und 2.0 übernommen werden, Gui für 2.0 kommt separat.
    
    * acthor 45K
    
    Neuer acthor type zugelassen 45000 Watt

--- packages/modules/smarthome/acthor/watt.py
+++ packages/modules/smarthome/acthor/watt.py
@@ -42,7 +42,13 @@ neupower = 0
 if instpower == 0:
     instpower = 1000
 cap = 9000
-if atype == "9s18":
+if atype == "9s45":
+    faktor = 45000/instpower
+    cap = 45000
+elif atype == "9s27":
+    faktor = 27000/instpower
+    cap = 27000
+elif atype == "9s18":
     faktor = 18000/instpower
     cap = 18000
 elif atype == "9s":
--- web/settings/smarthomeconfig.php
+++ web/settings/smarthomeconfig.php
@@ -409,6 +409,8 @@ $numDevices = 9;
 												<option value="M3" data-option="M3">Acthor M3</option>
 												<option value="9s" data-option="9s">Acthor 9s</option>
 												<option value="9s18" data-option="9s18">Acthor 9s Dual 18k</option>
+												<option value="9s27" data-option="9s27">Acthor 9s boost 27k</option>
+												<option value="9s45" data-option="9s45">Acthor 9s boost 45k</option>
 											</select>
 											<span class="form-text small">
 												Hier ist das installierte Modell auszuwählen.
@@ -418,7 +420,7 @@ $numDevices = 9;
 									<div class="form-row mb-1">
 										<label for="device_acthorpowerDevices<?php echo $devicenum; ?>" class="col-md-4 col-form-label">Installierte Leistung</label>
 										<div class="col">
-											<input id="device_acthorpowerDevices<?php echo $devicenum; ?>" name="device_acthorpower" class="form-control" type="number" min="0" max="18000" step="100" required="required" data-default="0" value="0" data-topicprefix="openWB/config/get/SmartHome/" data-topicsubgroup="Devices/<?php echo $devicenum; ?>/">
+											<input id="device_acthorpowerDevices<?php echo $devicenum; ?>" name="device_acthorpower" class="form-control" type="number" min="0" max="50000" step="100" required="required" data-default="0" value="0" data-topicprefix="openWB/config/get/SmartHome/" data-topicsubgroup="Devices/<?php echo $devicenum; ?>/">
 											<span class="form-text small">
 												Hier bitte die an den Acthor angeschlossene Leistung in Watt angeben.
 											</span>
