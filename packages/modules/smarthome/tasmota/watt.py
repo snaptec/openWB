78ae2dd64ce53ff91fe5af6c946e10bdff76743d
hawa-lc4 <124808307+hawa-lc4@users.noreply.github.com>
2024-06-06 14:36:42 +0200

    rework commit "adapt watt.py to changed status response starting Tasmota 14.x"
    Regarding concerns of backward compatibility of this script to older versions of Tasmota
    the URL has been reverted to the previously one used and the evaluation of the relais
    status has been adopted to serve Tasmota pre version 14.x as well as newer versions.

--- packages/modules/smarthome/tasmota/watt.py
+++ packages/modules/smarthome/tasmota/watt.py
@@ -10,22 +10,21 @@ devicenumber = str(sys.argv[1])
 ipadr = str(sys.argv[2])
 uberschuss = int(sys.argv[3])
 relais = 0
-#jsonurl0 = "http://"+str(ipadr)+"/cm?cmnd=Status"
-jsonurl1 = "http://"+str(ipadr)+"/cm?cmnd=Status%2011"
+jsonurl1 = "http://"+str(ipadr)+"/cm?cmnd=Status"
 jsonurl2 = "http://"+str(ipadr)+"/cm?cmnd=Status%208"
-jsonpow = ".StatusSTS.POWER"
-jsonpow1 = ".StatusSTS.POWER1"
+jsonpow = ".Status.Power"
 
 try:
     answer = json.loads(str(urllib.request.urlopen(jsonurl1, timeout=3).read().decode("utf-8")))
     p_status = jq.compile(jsonpow).input(answer).first()
-    #p_status = int((answer['Status']['Power']) % 2)
-    if p_status == None:
-        p_status = jq.compile(jsonpow1).input(answer).first()
-    if p_status == None:
-        p_status = "OFF"
+    if type(p_status) is int:
+        relais = p_status % 2
+    elif type(p_status) is str:
+        relais  = int(p_status, 2) % 2
+    else:
+        relais = 0
 except Exception:
-    p_status = "OFF"
+    relais = 0
 
 try:
     answer = json.loads(str(urllib.request.urlopen(jsonurl2, timeout=3).read().decode("utf-8")))
@@ -35,8 +34,6 @@ except Exception:
     aktpower = 0
     powerc = 0
 
-if (aktpower > 50) or (p_status == "ON"):
-    relais = 1
 answer = '{"power":' + str(aktpower) + ',"powerc":' + str(powerc) + ',"on":' + str(relais) + '} '
 f1 = open('/var/www/html/openWB/ramdisk/smarthome_device_ret' + str(devicenumber), 'w')
 json.dump(answer, f1)
